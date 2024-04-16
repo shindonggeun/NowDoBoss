810253b968a1f42bc7f00ecdbfd87d2deb6cd9a9
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-16 17:09:12 +0900

    [BE] feat: 로그인 API 개발

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/controller/MemberController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.ssafy.backend.domain.member.controller;
 
+import com.ssafy.backend.domain.member.dto.MemberLoginRequest;
+import com.ssafy.backend.domain.member.dto.MemberLoginResponse;
 import com.ssafy.backend.domain.member.dto.MemberSignupRequest;
 import com.ssafy.backend.domain.member.service.MemberService;
 import com.ssafy.backend.global.common.dto.Message;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +33,21 @@ public class MemberController {
     public ResponseEntity<Message<Void>> signupMember(@Valid @RequestBody MemberSignupRequest signupRequest) {
         memberService.signupMember(signupRequest);
         return ResponseEntity.ok().body(Message.success());
+    }
+
+    @Operation(
+            summary = "로그인",
+            description = "이메일과 비밀번호를 입력하여 로그인을 하는 기능입니다."
+    )
+    @PostMapping("/login")
+    public ResponseEntity<Message<MemberLoginResponse>> loginMember(@RequestBody MemberLoginRequest loginRequest,
+                                                                    HttpServletResponse response) {
+        MemberLoginResponse loginResponse = memberService.loginMember(loginRequest);
+        // JWT 토큰을 쿠키에 저장
+        Cookie accessTokenCookie = new Cookie("accessToken", loginResponse.tokenInfo().accessToken());
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(25200); // 4200분(25200초)으로 설정 (25200)
+        response.addCookie(accessTokenCookie);
+        return ResponseEntity.ok().body(Message.success(loginResponse));
     }
 }

148b0bea5bc915ab15c46182f8c42d9b83f8ec7a
김광연 <kwang9705@naver.com>
2024-04-16 17:43:17 +0900

    [BE] feat: 유동인구 top 5 조회 쿼리 작성

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/district/dto/DistrictTopFiveResponse.java
@@ -0,0 +1,15 @@
+package com.ssafy.backend.domain.district.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+import java.util.Map;
+
+@Builder
+public record DistrictTopFiveResponse(
+        List<Map<String, Object>> footTrafficTopFiveList,
+        List<Map<String, Object>> salesTopFiveList,
+        List<Map<String, Object>> openedRateTopFiveList,
+        List<Map<String, Object>> closedRateTopFiveList
+) {
+}
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/district/repository/SalesDistrictRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.backend.domain.district.repository;
+
+import com.ssafy.backend.domain.district.entity.SalesDistrict;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalesDistrictRepository extends JpaRepository<SalesDistrict, Long> {
+
+}

517a79d3a4557026058cd09b865377fd7d8dbe97
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-16 15:24:41 +0900

    [BE] feat: 회원 엔티티 생성

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
@@ -1,4 +1,40 @@
 package com.ssafy.backend.domain.member.entity;
 
-public class Member {
+import com.ssafy.backend.domain.member.entity.enums.MemberRole;
+import com.ssafy.backend.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @Comment("회원 아이디")
+    @Column(columnDefinition = "INT UNSIGNED")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Comment("이메일")
+    @Column(nullable = false)
+    private String email;
+
+    @Comment("비밀번호")
+    private String password;
+
+    @Comment("닉네임")
+    @Column(nullable = false)
+    private String nickname;
+
+    @Comment("프로필 이미지 URL")
+    private String profileImage;
+
+    @Comment("권한")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
 }

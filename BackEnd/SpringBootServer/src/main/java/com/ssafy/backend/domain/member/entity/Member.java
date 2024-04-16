d4146e09c10ea509e71ec433daf647c7bba73204
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-16 15:36:35 +0900

    [BE] fix: 회원 엔티티 수정

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
@@ -24,17 +24,22 @@ public class Member extends BaseEntity {
     private String email;
 
     @Comment("비밀번호")
+    @Column(columnDefinition = "VARCHAR(80)")
     private String password;
 
+    @Comment("이름")
+    @Column(columnDefinition = "VARCHAR(40)")
+    private String name;
+
     @Comment("닉네임")
-    @Column(nullable = false)
+    @Column(columnDefinition = "VARCHAR(60)", nullable = false)
     private String nickname;
 
     @Comment("프로필 이미지 URL")
     private String profileImage;
 
     @Comment("권한")
-    @Column(nullable = false)
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
     @Enumerated(EnumType.STRING)
     private MemberRole role;
 }

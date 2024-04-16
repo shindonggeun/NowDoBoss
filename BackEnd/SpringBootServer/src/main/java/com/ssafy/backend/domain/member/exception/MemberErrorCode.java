bc54c8ed72ae507245514d7bd2d5c6972b3de010
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-16 17:58:06 +0900

    [BE] feat: access 토큰 파싱 기능 개발

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberErrorCode.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum MemberErrorCode {
+
     EXIST_MEMBER_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이미 가입되어 있는 이메일입니다.");
 
     private final HttpStatus httpStatus;

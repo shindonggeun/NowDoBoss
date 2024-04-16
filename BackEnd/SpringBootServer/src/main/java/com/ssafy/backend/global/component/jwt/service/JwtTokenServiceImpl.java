ff537a9d7ff455163373a1af4844f139e9368810
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-16 17:13:54 +0900

    [BE] feat: JWT access 토큰 발급 기능 완성

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ssafy.backend.global.component.jwt.service;
+
+import com.ssafy.backend.domain.member.dto.MemberInfo;
+import com.ssafy.backend.domain.member.dto.MemberLoginResponse;
+import com.ssafy.backend.domain.member.entity.Member;
+import com.ssafy.backend.global.component.jwt.JwtTokenProvider;
+import com.ssafy.backend.global.component.jwt.dto.JwtTokenInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JwtTokenServiceImpl implements JwtTokenService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public MemberLoginResponse issueAndSaveJwtToken(Member member) {
+        String accessToken = jwtTokenProvider.issueAccessToken(member);
+        // TODO: 리프레쉬 토큰 발급
+
+
+        log.info("== {} 회원에 대한 토큰 발급: {}", member.getEmail(), accessToken);
+
+        // TODO: 리프레쉬 토큰 Redis에 저장
+
+        JwtTokenInfo tokenInfo = new JwtTokenInfo(accessToken);
+
+        MemberInfo memberInfo = new MemberInfo(
+                member.getId(),
+                member.getEmail(),
+                member.getName(),
+                member.getNickname(),
+                member.getProfileImage(),
+                member.getRole()
+        );
+
+        return new MemberLoginResponse(tokenInfo, memberInfo);
+    }
+}

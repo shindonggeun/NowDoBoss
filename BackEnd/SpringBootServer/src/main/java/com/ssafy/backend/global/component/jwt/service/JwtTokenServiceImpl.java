049af99f6b379ffddba6ae245cf65afd47b0283b
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-17 09:53:45 +0900

    [BE] feat: 로그인 시 redis에 refresh 토큰 저장하는 기능 개발

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
@@ -5,6 +5,7 @@ import com.ssafy.backend.domain.member.dto.MemberLoginResponse;
 import com.ssafy.backend.domain.member.entity.Member;
 import com.ssafy.backend.global.component.jwt.JwtTokenProvider;
 import com.ssafy.backend.global.component.jwt.dto.JwtTokenInfo;
+import com.ssafy.backend.global.component.jwt.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,21 @@ import org.springframework.stereotype.Service;
 public class JwtTokenServiceImpl implements JwtTokenService {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public MemberLoginResponse issueAndSaveJwtToken(Member member) {
         String accessToken = jwtTokenProvider.issueAccessToken(member);
-        // TODO: 리프레쉬 토큰 발급
-
+        String refreshToken = jwtTokenProvider.issueRefreshToken();
 
         log.info("== {} 회원에 대한 토큰 발급: {}", member.getEmail(), accessToken);
 
-        // TODO: 리프레쉬 토큰 Redis에 저장
+        try {
+            refreshTokenRepository.save(member.getEmail(), refreshToken);
+        } catch (Exception e) {
+            // TODO: 레디스 관련 커스텀 Exception (Global)
+            throw new RuntimeException("레디스 연결에 실패하였습니다.");
+        }
 
         JwtTokenInfo tokenInfo = new JwtTokenInfo(accessToken);
 

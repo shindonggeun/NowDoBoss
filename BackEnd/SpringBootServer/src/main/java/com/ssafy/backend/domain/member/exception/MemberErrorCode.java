4e1000a6835973346402298ac4a1ecc83ba0f6f0
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-17 09:49:57 +0900

    [BE] feat: 비밀번호 암호화된 것 복호화 작업 실시해서 매칭되는지 확인하는 기능 개발

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberErrorCode.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode {
 
     EXIST_MEMBER_EMAIL(HttpStatus.CONFLICT, "이미 가입되어 있는 이메일입니다."),
-    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다.");
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;

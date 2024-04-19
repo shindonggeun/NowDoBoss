5071a3c2adab9d9aeb1efbc553cfacc34191cfc0
이승현 <tmdgus1761@gmail.com>
2024-04-19 16:41:23 +0900

    [FE] feat: 페이지 라우팅 적용

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,5 +1,17 @@
+import { CookiesProvider } from 'react-cookie'
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "@src/pages/MainPage.tsx";
+
 function App() {
-  return <>index</>
+  return (
+      <CookiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </CookiesProvider>
+  )
 }
 
 export default App

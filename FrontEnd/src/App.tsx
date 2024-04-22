c856d22654b3a088a958966ad19348bcd1741012
이승현 <tmdgus1761@gmail.com>
2024-04-22 17:21:01 +0900

    [FE] fix: 컴포넌트 경로 확장자 제거

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,8 +1,8 @@
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from '@src/common/Header.tsx'
-import MainPage from '@src/pages/MainPage.tsx'
-import SignUpPage from '@src/pages/SignUpPage.tsx'
+import Header from '@src/common/Header'
+import MainPage from '@src/pages/MainPage'
+import SignUpPage from '@src/pages/SignUpPage'
 
 function App() {
   return (

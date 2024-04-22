837c30a3b1ae92f96570cf7ce4d0784a643d1670
이승현 <tmdgus1761@gmail.com>
2024-04-22 17:24:07 +0900

    [FE] style: 회원가입 관련 재사용 컴포넌트 스타일 적용

---
+++ FrontEnd/src/components/styles/UserStyle/AskSectionStyle.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin-top: 1%;
+`
+
+export const Title = styled.span`
+  font-size: 0.9rem;
+`
+
+export const Subtitle = styled.span`
+  font-size: 1rem;
+  font-weight: 600;
+  margin-left: 2%;
+`

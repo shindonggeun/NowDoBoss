d1fe4b5f154421d6d8433c2c70da66de664abc4e
choiseongho <chltjdgh3@naver.com>
2024-04-22 17:48:38 +0900

    [FE] feat: 커뮤니티 라우팅 연결

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,8 +1,9 @@
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from '@src/common/Header.tsx'
-import MainPage from '@src/pages/MainPage.tsx'
-import SignUpPage from '@src/pages/SignUpPage.tsx'
+import Header from '@src/common/Header'
+import MainPage from '@src/pages/MainPage'
+import SignUpPage from '@src/pages/SignUpPage'
+import CommunityPage from '@src/pages/CommunityPage'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/register" element={<SignUpPage />} />
+          <Route path="/community" element={<CommunityPage />} />
         </Routes>
       </BrowserRouter>
     </CookiesProvider>

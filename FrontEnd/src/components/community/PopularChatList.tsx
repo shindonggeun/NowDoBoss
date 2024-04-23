7a6e6dbb5df4d018a0902291343ae4b1aaf25c15
choiseongho <chltjdgh3@naver.com>
2024-04-23 09:09:39 +0900

    [FE] feat: 인기 채팅방 목록 map 함수 적용

--- FrontEnd/src/components/community/PopularChatList.tsx
+++ FrontEnd/src/components/community/PopularChatList.tsx
@@ -1,10 +1,11 @@
-import * as p from '@src/components/styles/community/PopularChatListStyle'
+import * as p from '@src/components/styles/community/CommunityStyle'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
 import { useRef } from 'react'
 
 const PopularChatList = () => {
+  // slider 옆으로 넘기기 위한 ref 상태
   const sliderRef = useRef<Slider | null>(null)
 
   const settings = {
@@ -28,6 +29,43 @@ const PopularChatList = () => {
       sliderRef.current.slickPrev()
     }
   }
+
+  const ChatCardDatas = [
+    {
+      id: 1,
+      badge: '채팅방',
+      title: '강창모 (강동구 창업자들의 모임)',
+      content:
+        '강동구 사장님, 예비사장님들의 모임입니다. 부담 가지지 말고 들어오세요 :)',
+      category: '이모저모',
+      subContent: '인원 24 / 50',
+    },
+    {
+      id: 2,
+      badge: '채팅방',
+      title: '잡담수다방',
+      content:
+        '강동구 사장님, 예비사장님들의 모임입니다. 부담 가지지 말고 들어오세요 :)',
+      category: '이모저모',
+      subContent: '인원 24 / 50',
+    },
+    {
+      id: 3,
+      badge: '채팅방',
+      title: '강남모임',
+      content: '강남 사장 모임입니다 ㅎㅎ 누구든 들오세유',
+      category: '이모저모',
+      subContent: '인원 24 / 50',
+    },
+    {
+      id: 4,
+      badge: '채팅방',
+      title: '논현 게주아',
+      content: '논현동 사장님, 예비사장님들의 모임입니다. 일단 드루와!',
+      category: '이모저모',
+      subContent: '인원 8 / 50',
+    },
+  ]
 
   return (
     <p.Container>
@@ -54,66 +92,20 @@ const PopularChatList = () => {
       <p.Slick className="slider-container">
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Slider {...settings} ref={sliderRef}>
-          <p.SlickChild>
-            <p.ChatCard>
-              <p.CategoryBadge>채팅방</p.CategoryBadge>
-              <p.CardTitle>강창모 (강동구 창업자들의 모임)</p.CardTitle>
-              <p.CardContent>
-                강동구 사장님, 예비사장님들의 모임입니다. 부담 가지지 말고
-                들어오세요 :)
-              </p.CardContent>
-              <p.CardCategory>
-                <p.Icon src="src/assets/fire_gray.svg" />
-                이모저모
-              </p.CardCategory>
-              <p.CardSubContent>인원 24 / 50</p.CardSubContent>
-            </p.ChatCard>
-          </p.SlickChild>
-          <p.SlickChild>
-            <p.ChatCard>
-              <p.CategoryBadge>채팅방</p.CategoryBadge>
-              <p.CardTitle>강창모 (강동구 창업자들의 모임)</p.CardTitle>
-              <p.CardContent>
-                강동구 사장님, 예비사장님들의 모임입니다. 부담 가지지 말고
-                들어오세요 :)
-              </p.CardContent>
-              <p.CardCategory>
-                <p.Icon src="src/assets/fire_gray.svg" />
-                이모저모
-              </p.CardCategory>
-              <p.CardSubContent>인원 24 / 50</p.CardSubContent>
-            </p.ChatCard>
-          </p.SlickChild>
-          <p.SlickChild>
-            <p.ChatCard>
-              <p.CategoryBadge>채팅방</p.CategoryBadge>
-              <p.CardTitle>강창모 (강동구 창업자들의 모임)</p.CardTitle>
-              <p.CardContent>
-                강동구 사장님, 예비사장님들의 모임입니다. 부담 가지지 말고
-                들어오세요 :)
-              </p.CardContent>
-              <p.CardCategory>
-                <p.Icon src="src/assets/fire_gray.svg" />
-                이모저모
-              </p.CardCategory>
-              <p.CardSubContent>인원 24 / 50</p.CardSubContent>
-            </p.ChatCard>
-          </p.SlickChild>
-          <p.SlickChild>
-            <p.ChatCard>
-              <p.CategoryBadge>채팅방</p.CategoryBadge>
-              <p.CardTitle>강창모 (강동구 창업자들의 모임)</p.CardTitle>
-              <p.CardContent>
-                강동구 사장님, 예비사장님들의 모임입니다. 부담 가지지 말고
-                들어오세요 :)
-              </p.CardContent>
-              <p.CardCategory>
-                <p.Icon src="src/assets/fire_gray.svg" />
-                이모저모
-              </p.CardCategory>
-              <p.CardSubContent>인원 24 / 50</p.CardSubContent>
-            </p.ChatCard>
-          </p.SlickChild>
+          {ChatCardDatas.map(ChatCardData => (
+            <p.SlickChild key={ChatCardData.id}>
+              <p.ChatCard>
+                <p.CategoryBadge>{ChatCardData.badge}</p.CategoryBadge>
+                <p.CardTitle>{ChatCardData.title}</p.CardTitle>
+                <p.CardContent>{ChatCardData.content}</p.CardContent>
+                <p.CardCategory>
+                  <p.Icon src="src/assets/fire_gray.svg" />
+                  {ChatCardData.category}
+                </p.CardCategory>
+                <p.CardSubContent>{ChatCardData.subContent}</p.CardSubContent>
+              </p.ChatCard>
+            </p.SlickChild>
+          ))}
         </Slider>
       </p.Slick>
     </p.Container>

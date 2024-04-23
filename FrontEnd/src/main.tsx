680eec1a9d29e953e0eb2a8f92d639f5bbff54d6
choiseongho <chltjdgh3@naver.com>
2024-04-23 10:51:09 +0900

    [FE] fix: 렌더링 에러 수정

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+import './index.css'
 
 const queryClient = new QueryClient()
 

3fcfa371731f1e90591b078362bc28d9489c44de
choiseongho <chltjdgh3@naver.com>
2024-04-23 10:52:47 +0900

    [FE] style: 커뮤니티 Navbar 상단에 고정

--- FrontEnd/src/containers/community/CommunityContainerStyle.tsx
+++ FrontEnd/src/containers/community/CommunityContainerStyle.tsx
@@ -4,11 +4,14 @@ export const Container = styled.div`
   display: flex;
 `
 export const NabBar = styled.div`
-  display: grid;
+  position: fixed;
+  z-index: 90;
 `
 
 export const Context = styled.div`
-  margin: 3% 10%;
+  //margin-left: 220px;
+  margin: 3% 10% 3% 25%;
+  //margin: 3% 10%;
   //background-color: blanchedalmond;
   width: 65%;
 `

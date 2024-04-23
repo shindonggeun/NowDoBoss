401a07167c19c35e75ec135e49e9f9ac47065b07
choiseongho <chltjdgh3@naver.com>
2024-04-23 17:27:56 +0900

    [FE] feat: 커뮤니티 상세, 등록, 수정 라우터 생성

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,6 +4,9 @@ import Header from '@src/common/Header'
 import MainPage from '@src/pages/MainPage'
 import SignUpPage from '@src/pages/SignUpPage'
 import CommunityPage from '@src/pages/CommunityPage'
+import CommunityRegisterPage from '@src/pages/CommunityRegisterPage'
+import CommunityEditPage from '@src/pages/CommunityEditPage'
+import CommunityDetailPage from '@src/pages/CommunityDetailPage'
 
 function App() {
   return (
@@ -14,6 +17,15 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/register" element={<SignUpPage />} />
           <Route path="/community" element={<CommunityPage />} />
+          <Route
+            path="/community/register"
+            element={<CommunityRegisterPage />}
+          />
+          <Route path="/community/edit" element={<CommunityEditPage />} />
+          <Route
+            path="/community/:community_id"
+            element={<CommunityDetailPage />}
+          />
         </Routes>
       </BrowserRouter>
     </CookiesProvider>

eccf0fee7b5e124083629fb1f255f574e1896398
choiseongho <chltjdgh3@naver.com>
2024-04-23 11:46:58 +0900

    [FE] feat: 커뮤니티 게시글 목록 map 함수 적용

--- FrontEnd/src/components/community/ArticleList.tsx
+++ FrontEnd/src/components/community/ArticleList.tsx
@@ -1,6 +1,32 @@
 import * as a from '@src/components/styles/community/CommunityStyle'
+import useCommunityStore from '@src/stores/communityStore'
 
 const ArticleList = () => {
+  const categories = useCommunityStore(state => state.categories)
+
+  const ArticleDatas = [
+    {
+      id: 1,
+      // badge: '커뮤니티',
+      title: '강동구에 창업하신 사장님들께 질문드립니다..',
+      content:
+        '사장님들 안녕하신가요, 저는 강동구에 창업하기 위해 준비중인 예비창업자입니다. 요즘 고민이 있는데요, 자금이 넉넉치 못해 월세가 저렴하면서 폐업률이 낮은 상권이 어디가 있을까요..? 그리고...',
+      category: '창업고민',
+      count: 12,
+      comment: 2,
+    },
+    {
+      id: 2,
+      // badge: '커뮤니티',
+      title: '강동구에 창업하신 사장님들께 질문드립니다..',
+      content:
+        '사장님들 안녕하신가요, 저는 강동구에 창업하기 위해 준비중인 예비창업자입니다. 요즘 고민이 있는데요, 자금이 넉넉치 못해 월세가 저렴하면서 폐업률이 낮은 상권이 어디가 있을까요..? 그리고...',
+      category: '창업고민',
+      count: 12,
+      comment: 2,
+    },
+  ]
+
   return (
     <a.Container>
       {/* 상단 */}
@@ -12,34 +38,30 @@ const ArticleList = () => {
         <a.CreateButton>게시글 작성하기</a.CreateButton>
       </a.Context>
       {/*  게시글 목록 */}
-      <a.ArticleContainer>
-        <a.CategoryBadge>커뮤니티</a.CategoryBadge>
-        <a.CardTitle>강동구에 창업하신 사장님들께 질문드립니다..</a.CardTitle>
-        <a.CardContent>
-          사장님들 안녕하신가요, 저는 강동구에 창업하기 위해 준비중인 예비
-          창업자입니다. 요즘 고민이 있는데요, 자금이 넉넉치 못해 월세가
-          저렴하면서 폐업률이 낮은 상권이 어디가 있을까요..? 그리고...
-        </a.CardContent>
-        <a.CardCategory>
-          <a.Icon src="src/assets/three_line.svg" />
-          창업고민
-        </a.CardCategory>
-        <a.CardSubContent>조회수 12 ∙ 댓글 1</a.CardSubContent>
-      </a.ArticleContainer>
-      <a.ArticleContainer>
-        <a.CategoryBadge>커뮤니티</a.CategoryBadge>
-        <a.CardTitle>강동구에 창업하신 사장님들께 질문드립니다..</a.CardTitle>
-        <a.CardContent>
-          사장님들 안녕하신가요, 저는 강동구에 창업하기 위해 준비중인 예비
-          창업자입니다. 요즘 고민이 있는데요, 자금이 넉넉치 못해 월세가
-          저렴하면서 폐업률이 낮은 상권이 어디가 있을까요..? 그리고...
-        </a.CardContent>
-        <a.CardCategory>
-          <a.Icon src="src/assets/three_line.svg" />
-          창업고민
-        </a.CardCategory>
-        <a.CardSubContent>조회수 12 ∙ 댓글 1</a.CardSubContent>
-      </a.ArticleContainer>
+      <a.ArticlesContainer>
+        {ArticleDatas.map(article => {
+          // 카테고리 이미지를 find 함수를 사용해 category name 과 일치하는 이미지 불러오기
+          const matchedCategory = categories.find(
+            category => category.name === article.category,
+          )
+          const iconSrc = matchedCategory ? matchedCategory.iconInactive : ''
+
+          return (
+            <a.ArticleContainer key={article.id}>
+              <a.CategoryBadge>커뮤니티</a.CategoryBadge>
+              <a.CardTitle>{article.title}</a.CardTitle>
+              <a.CardContent>{article.content}</a.CardContent>
+              <a.CardCategory>
+                <a.Icon src={iconSrc} />
+                {article.category}
+              </a.CardCategory>
+              <a.CardSubContent>
+                조회수 {article.count} ∙ 댓글 {article.comment}
+              </a.CardSubContent>
+            </a.ArticleContainer>
+          )
+        })}
+      </a.ArticlesContainer>
     </a.Container>
   )
 }

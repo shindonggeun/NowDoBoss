b6dfe98d275f496815c991c5dfb31a39dce1a52a
choiseongho <chltjdgh3@naver.com>
2024-04-24 16:32:22 +0900

    [FE] fix: svg 파일 적용 경로 수정

--- FrontEnd/src/stores/communityStore.tsx
+++ FrontEnd/src/stores/communityStore.tsx
@@ -1,4 +1,16 @@
 import { create } from 'zustand'
+import three_line from 'src/assets/three_line.svg'
+import three_line_gray from 'src/assets/three_line_gray.svg'
+import fire from 'src/assets/fire.svg'
+import fire_gray from 'src/assets/fire_gray.svg'
+import shop from 'src/assets/shop.svg'
+import shop_gray from 'src/assets/shop_gray.svg'
+import map from 'src/assets/map.svg'
+import map_gray from 'src/assets/map_gray.svg'
+import user_add from 'src/assets/user_add.svg'
+import user_add_gray from 'src/assets/user_add_gray.svg'
+import chat from 'src/assets/chat.svg'
+import chat_gray from 'src/assets/chat_gray.svg'
 
 // 카테고리 타입 정의
 interface Category {
@@ -16,33 +28,33 @@ interface CommunityStoreType {
 const initialCategories: Category[] = [
   {
     name: '전체보기',
-    iconActive: 'src/assets/three_line.svg',
-    iconInactive: 'src/assets/three_line_gray.svg',
+    iconActive: three_line,
+    iconInactive: three_line_gray,
   },
   {
     name: '이모저모',
-    iconActive: 'src/assets/fire.svg',
-    iconInactive: 'src/assets/fire_gray.svg',
+    iconActive: fire,
+    iconInactive: fire_gray,
   },
   {
     name: '인테리어',
-    iconActive: 'src/assets/shop.svg',
-    iconInactive: 'src/assets/shop_gray.svg',
+    iconActive: shop,
+    iconInactive: shop_gray,
   },
   {
     name: '상권공유',
-    iconActive: 'src/assets/map.svg',
-    iconInactive: 'src/assets/map_gray.svg',
+    iconActive: map,
+    iconInactive: map_gray,
   },
   {
     name: '동업제안',
-    iconActive: 'src/assets/user_add.svg',
-    iconInactive: 'src/assets/user_add_gray.svg',
+    iconActive: user_add,
+    iconInactive: user_add_gray,
   },
   {
     name: '창업고민',
-    iconActive: 'src/assets/chat.svg',
-    iconInactive: 'src/assets/chat_gray.svg',
+    iconActive: chat,
+    iconInactive: chat_gray,
   },
 ]
 

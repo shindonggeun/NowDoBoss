7157e1c6a653ac61ee9ed97aa2d72f4cd823ba65
choiseongho <chltjdgh3@naver.com>
2024-04-24 16:33:08 +0900

    [FE] feat: 커뮤니티 글 내용 구현

--- FrontEnd/src/components/community/detail/MainContent.tsx
+++ FrontEnd/src/components/community/detail/MainContent.tsx
@@ -1,46 +1,66 @@
 import * as m from '@src/components/styles/community/CommunityDetailStyle'
-import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import TimeCounting from 'time-counting'
+
+interface TimeCountingOption {
+  objectTime: Date
+  lang: 'ko'
+  calculate?: {
+    justNow?: number
+  }
+}
 
 const MainContent = () => {
-  const [comment, setComment] = useState<string>('')
+  const navigate = useNavigate()
+
+  const detailData = {
+    communityId: 1,
+    category: '상권분석',
+    title: '강동구에 창업하신 사장님들께 질문드립니다..',
+    content:
+      '사장님들 안녕하신가요, 저는 강동구에 창업하기 위해 준비중인 예비 창업자입니다. 요즘 고민이 있는데요, 자금이 넉넉치 못해 월세가 저렴하면서 폐업률이 낮은 상권이 어디가 있을까요..? 그리고 롱런하는 꿀팁 부탁드립니다...!',
+    readCount: 3,
+
+    writerId: '',
+    writerNickname: '',
+    writerProfileImage: '',
+    images: '',
+  }
+
+  // 생성 시간 보여주는 라이브러리 사용
+  const TimeOption: TimeCountingOption = {
+    // 기준이 되는 현재 시간
+    objectTime: new Date(),
+    lang: 'ko',
+    calculate: {
+      justNow: 3601,
+    },
+  }
+
+  // test console
+  // console.log(TimeCounting('2024-04-24 4:00:00', TimeOption))
 
   return (
     <m.Container>
       <m.BackButton>
         <m.BackIcon src="/src/assets/arrow_left.svg" />
-        <m.BackText>목록으로 돌아가기</m.BackText>
-      </m.BackButton>
-      <m.Title>강동구에 창업하신 사장님들께 질문드립니다..</m.Title>
-      <m.Category>창업고민</m.Category>
-      <m.Time>2024년 4월 21일 16시 20분</m.Time>
-      <m.ContentImg />
-      <m.Content>
-        사장님들 안녕하신가요, 저는 강동구에 창업하기 위해 준비중인 예비
-        창업자입니다. 요즘 고민이 있는데요, 자금이 넉넉치 못해 월세가 저렴하면서
-        폐업률이 낮은 상권이 어디가 있을까요..? 그리고 롱런하는 꿀팁
-        부탁드립니다...!
-      </m.Content>
-      <m.CommentTitle>댓글 0</m.CommentTitle>
-      <m.CommentBox>
-        <m.CommentInput
-          placeholder="댓글을 작성하세요."
-          value={comment}
-          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-            setComment(e.target.value)
+        <m.BackText
+          onClick={() => {
+            navigate('/community')
           }}
-        />
-        <m.CommentSubmit>작성</m.CommentSubmit>
-      </m.CommentBox>
-      <m.CommentCard>
-        <m.CommentContainer>
-          <m.CommentProfile />
-          <m.CommentUser>
-            <m.CommentName>최성호</m.CommentName>
-            <m.CommentTime>2024-03-12 16:40</m.CommentTime>
-          </m.CommentUser>
-        </m.CommentContainer>
-        <m.CommentContent>좋은 글 잘 읽었습니다.</m.CommentContent>
-      </m.CommentCard>
+        >
+          목록으로 돌아가기
+        </m.BackText>
+      </m.BackButton>
+      <m.Title>{detailData.title}</m.Title>
+      <m.Category>창업고민</m.Category>
+      <m.TimeAndCounting>
+        {/* 게시글 시간 아래에 넣으면 됩니다~ */}
+        {TimeCounting('2024-04-24 4:00:00', TimeOption)} ∙ 조회수{' '}
+        {detailData.readCount}
+      </m.TimeAndCounting>
+      <m.ContentImg />
+      <m.Content>{detailData.content}</m.Content>
     </m.Container>
   )
 }

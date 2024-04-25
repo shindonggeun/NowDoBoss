e442266fe4226bb4b867720461a5b6306bd0ba41
choiseongho <chltjdgh3@naver.com>
2024-04-26 01:19:04 +0900

    [FE] feat: 커뮤니티 상세 api 연결

---
+++ FrontEnd/src/common/Divider.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Hr = styled.hr`
+  border-top: 10px solid #efefef;
+  width: 80%;
+`
+
+const Divider = () => {
+  return (
+    <div>
+      <Hr />
+    </div>
+  )
+}
+
+export default Divider
--- FrontEnd/src/containers/community/CommunityDetailContainer.tsx
+++ FrontEnd/src/containers/community/CommunityDetailContainer.tsx
@@ -2,18 +2,47 @@ import MainContent from '@src/components/community/detail/MainContent'
 import SubContent from '@src/components/community/detail/SubContent'
 import CommentList from '@src/components/community/detail/CommentList'
 import * as c from '@src/containers/community/CommunityContainerStyle'
+import NavBar from '@src/components/community/list/NavBar'
+import Divider from '@src/common/Divider'
+import useCommunityStore from '@src/stores/communityStore'
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import { fetchCommunityDetail } from '@src/api/communityApi'
 
 const CommunityDetailContainer = () => {
+  const { communityId } = useParams<{ communityId: string }>()
+  const { setCategory } = useCommunityStore(state => ({
+    // category: state.selectedCategory,
+    setCategory: state.setSelectedCategory,
+  }))
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['CommunityList'],
+    queryFn: () => fetchCommunityDetail(Number(communityId)),
+  })
+
   return (
-    <c.DetailContainer>
-      <c.MainContentDiv>
-        <MainContent />
-        <CommentList />
-      </c.MainContentDiv>
-      <c.SubContentDiv>
-        <SubContent />
-      </c.SubContentDiv>
-    </c.DetailContainer>
+    <div>
+      {!isLoading && data ? (
+        <c.Container>
+          <c.NabBar>
+            <NavBar setCategory={setCategory} />
+          </c.NabBar>
+          <c.MarginLeft>
+            <c.Context>
+              <c.MainContentDiv>
+                <MainContent detailData={data.dataBody} />
+                <CommentList />
+                <Divider />
+                <SubContent />
+              </c.MainContentDiv>
+            </c.Context>
+          </c.MarginLeft>
+        </c.Container>
+      ) : (
+        <div>데이터 호출 에러</div>
+      )}
+    </div>
   )
 }
 export default CommunityDetailContainer

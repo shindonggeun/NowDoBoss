27b7ae95271c5ed4792d9e3a337cbff83e7d3272
choiseongho <chltjdgh3@naver.com>
2024-04-26 01:13:03 +0900

    [FE] feat: 커뮤니티 목록 api 호출 로직 추가

--- FrontEnd/src/containers/community/CommunityContainer.tsx
+++ FrontEnd/src/containers/community/CommunityContainer.tsx
@@ -1,21 +1,43 @@
 import NavBar from '@src/components/community/list/NavBar'
 import PopularChatList from '@src/components/community/list/PopularChatList'
 import ArticleList from '@src/components/community/list/ArticleList'
+import { useQuery } from '@tanstack/react-query'
+import { fetchCommunityList } from '@src/api/communityApi'
+import { useEffect } from 'react'
+import useCommunityStore from '@src/stores/communityStore'
 import * as c from './CommunityContainerStyle'
 
 const CommunityContainer = () => {
+  const { category, setCategory } = useCommunityStore(state => ({
+    category: state.selectedCategory,
+    setCategory: state.setSelectedCategory,
+  }))
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['CommunityList'],
+    queryFn: () => fetchCommunityList(category.value),
+  })
+  useEffect(() => {
+    refetch()
+  }, [refetch, category])
   return (
-    <c.Container>
-      <c.NabBar>
-        <NavBar />
-      </c.NabBar>
-      <c.MarginLeft>
-        <c.Context>
-          <PopularChatList />
-          <ArticleList />
-        </c.Context>
-      </c.MarginLeft>
-    </c.Container>
+    <div>
+      {!isLoading && data ? (
+        <c.Container>
+          <c.NabBar>
+            <NavBar setCategory={setCategory} />
+          </c.NabBar>
+          <c.MarginLeft>
+            <c.Context>
+              <PopularChatList />
+              <ArticleList articleList={data.dataBody} />
+            </c.Context>
+          </c.MarginLeft>
+        </c.Container>
+      ) : (
+        <div>데이터 호출 에러</div>
+      )}
+    </div>
   )
 }
 
--- FrontEnd/src/containers/community/CommunityContainerStyle.tsx
+++ FrontEnd/src/containers/community/CommunityContainerStyle.tsx
@@ -12,7 +12,7 @@ export const NabBar = styled.div`
   position: fixed;
   z-index: 90;
   //top: 0; // 상단에 고정
-  width: 20%;
+  width: 16.5vw;
 
   @media only screen and (max-width: 992px) {
     position: static; // 화면 너비가 992px 이하일 때는 고정 위치 해제
@@ -39,27 +39,13 @@ export const Context = styled.div`
 
 // detail style
 
-export const DetailContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100vw;
-  height: 92.5vh;
-  @media only screen and (max-width: 768px) {
-    flex-direction: column;
-  }
-`
-
 export const MainContentDiv = styled.div`
-  width: 66.7vw;
-
-  @media only screen and (max-width: 768px) {
-    width: 100vw;
-  }
+  width: 100%;
 `
-
-export const SubContentDiv = styled.div`
-  width: 33.3vw;
-  @media only screen and (max-width: 768px) {
-    width: 100vw;
-  }
-`
+//
+// export const SubContentDiv = styled.div`
+//   width: 33.3vw;
+//   @media only screen and (max-width: 768px) {
+//     width: 100vw;
+//   }
+// `

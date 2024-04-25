bc7db3b4c49aeffa15c5e539595f2959f101fa2a
choiseongho <chltjdgh3@naver.com>
2024-04-26 01:11:06 +0900

    [FE] style: 커뮤니티 navbar style 수정

--- FrontEnd/src/components/styles/community/NavbarStyle.tsx
+++ FrontEnd/src/components/styles/community/NavbarStyle.tsx
@@ -7,16 +7,17 @@ interface CategoryType {
 export const Container = styled.div`
   background-color: #f1f1f1;
   //  왼쪽에 띄울 크기
-  width: 16.5vw;
+  width: 250px;
   //width: 220px;
   height: 120vh;
-
+  padding: 10px;
   @media only screen and (max-width: 992px) {
     display: flex;
     align-items: center;
     justify-content: space-between;
     width: 100vw;
     height: 8vh;
+    padding: 5px;
   }
 `
 export const Community = styled.div`
@@ -25,7 +26,7 @@ export const Community = styled.div`
   @media only screen and (max-width: 992px) {
     display: flex;
     align-items: center;
-    justify-content: left;
+    justify-content: center;
     width: 95vw;
     height: 10vh;
     margin: 0;
@@ -33,7 +34,7 @@ export const Community = styled.div`
   @media only screen and (max-width: 500px) {
     display: flex;
     align-items: center;
-    justify-content: left;
+    justify-content: center;
     width: 100vw;
     height: 10vh;
     margin: 0;
@@ -49,15 +50,20 @@ export const Chatting = styled.div`
 
 export const ChatButton = styled.img`
   display: none;
+  margin-right: 40px;
 
   @media only screen and (max-width: 992px) {
     display: flow;
-    margin-right: 2vw;
     cursor: pointer;
   }
-  @media only screen and (max-width: 500px) {
+  @media only screen and (max-width: 800px) {
+    display: flow;
+    cursor: pointer;
+    margin-right: 5%;
+  }
+  @media only screen and (max-width: 586px) {
     padding: 10px 5px;
-    margin-right: 1.3vw;
+    margin-right: 6%;
     scale: 0.8;
   }
 `
@@ -71,11 +77,7 @@ export const Title = styled.div`
     padding: 10px 15px;
     font-size: 1.5rem;
   }
-  @media only screen and (max-width: 770px) {
-    padding: 10px 10px;
-    font-size: 1.3rem;
-  }
-  @media only screen and (max-width: 500px) {
+  @media only screen and (max-width: 520px) {
     padding: 10px 5px;
     font-size: 1.2rem;
   }
@@ -84,29 +86,31 @@ export const Title = styled.div`
 // 카테고리 및 채팅 목록으로 재사용
 export const Category = styled.div<CategoryType>`
   font-weight: 700;
-  padding: 5px 15px;
+  padding: 10px 20px;
   display: flex;
   flex-direction: row;
-  font-size: ${props => (props.$isChoice ? '1.05rem' : '1rem')};
+  font-size: ${props => (props.$isChoice ? '1.2rem' : '1.1rem')};
   color: ${props => (props.$isChoice ? 'black' : 'gray')};
   background-color: ${props => (props.$isChoice ? '#D9D9D9' : 'none')};
   border-radius: ${props => (props.$isChoice ? '5px' : 'none')};
   &:hover {
     cursor: pointer;
+    background-color: #d9d9d9;
+    border-radius: 5px;
   }
   @media only screen and (max-width: 992px) {
     padding: 7px 5px;
     margin: 0 5px;
   }
-  @media only screen and (max-width: 768px) {
-    padding: 5px;
-  }
-  @media only screen and (max-width: 500px) {
+  //@media only screen and (max-width: 768px) {
+  //  padding: 5px;
+  //}
+  @media only screen and (max-width: 520px) {
     padding: 3px;
   }
 `
 export const Icon = styled.img`
-  @media only screen and (max-width: 768px) {
+  @media only screen and (max-width: 820px) {
     scale: 1.5;
     padding: 0.25rem 0.5rem;
   }
@@ -126,7 +130,7 @@ export const Text = styled.div`
   @media only screen and (max-width: 992px) {
     margin: 0 0 0 2px;
   }
-  @media only screen and (max-width: 768px) {
+  @media only screen and (max-width: 820px) {
     //font-size: 0.8rem;
     display: none;
   }

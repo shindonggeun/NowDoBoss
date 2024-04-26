39cb61545b6a7368b2f7abd97cea76bc8fe0e709
choiseongho <chltjdgh3@naver.com>
2024-04-26 15:00:31 +0900

    [FE] feat: 커뮤니티 게시글 삭제 로직 구현

--- FrontEnd/src/components/community/detail/MainContent.tsx
+++ FrontEnd/src/components/community/detail/MainContent.tsx
@@ -5,6 +5,11 @@ import send_message from '@src/assets/send_message.svg'
 import { CommunityData } from '@src/types/CommunityType'
 import useCommunityStore from '@src/stores/communityStore'
 import Slider from 'react-slick'
+import { useState } from 'react'
+import Swal from 'sweetalert2'
+import { useMutation } from '@tanstack/react-query'
+import { articleDelete } from '@src/api/communityApi'
+import { useNavigate } from 'react-router-dom'
 
 interface TimeCountingOption {
   objectTime: Date
@@ -20,14 +25,18 @@ export type MainContentPropsType = {
 
 const MainContent = (props: MainContentPropsType) => {
   const { detailData } = props
+  const navigate = useNavigate()
 
-  const categories = useCommunityStore(state => state.categories)
+  const { categories, setModifyCommunityId } = useCommunityStore(state => ({
+    categories: state.categories,
+    setModifyCommunityId: state.setModifyCommunityId,
+  }))
   const matchedCategory = categories.find(
     category => category.value === detailData.category,
   )
   const iconSrc = matchedCategory ? matchedCategory.iconInactive : ''
   const categoryKorean = matchedCategory ? matchedCategory.name : ''
-
+  const [moreModalValue, setMoreModal] = useState<boolean>(false)
   // 생성 시간 보여주는 라이브러리 사용
   const TimeOption: TimeCountingOption = {
     // 기준이 되는 현재 시간
@@ -40,6 +49,7 @@ const MainContent = (props: MainContentPropsType) => {
   // test console
   // console.log(TimeCounting('2024-04-24 4:00:00', TimeOption))
 
+  // 이미지 캐러셀 스타일
   const settings = {
     dots: false,
     infinite: true,
@@ -51,9 +61,85 @@ const MainContent = (props: MainContentPropsType) => {
     centerPadding: '0px',
   }
 
+  // 게시글 삭제
+  const { mutate: mutateDeleteArticle } = useMutation({
+    mutationFn: articleDelete,
+    onSuccess: () => {
+      // 댓글 삭제 성공 시 댓글 목록 재호출
+      navigate('/community')
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: toast => {
+          // eslint-disable-next-line no-param-reassign
+          toast.onmouseenter = Swal.stopTimer
+          // eslint-disable-next-line no-param-reassign
+          toast.onmouseleave = Swal.resumeTimer
+        },
+      })
+      Toast.fire({
+        icon: 'success',
+        title: '게시글이 삭제되었습니다.',
+      })
+    },
+  })
+
+  const onArticleDelete = () => {
+    Swal.fire({
+      title: '게시글을 삭제하시겠습니까?',
+      showDenyButton: true,
+      icon: 'warning',
+      confirmButtonText: '네',
+      denyButtonText: '아니요',
+      confirmButtonColor: '#429f50',
+      cancelButtonColor: '#d33',
+    }).then(result => {
+      if (result.isConfirmed) {
+        mutateDeleteArticle(Number(detailData.communityId))
+      }
+    })
+  }
+
   return (
     <m.Container>
-      <GoBackButton />
+      <m.Header>
+        <GoBackButton />
+        <m.More
+          onClick={() => {
+            setMoreModal(!moreModalValue)
+          }}
+        >
+          ∘∘∘
+        </m.More>
+        {moreModalValue ? (
+          <m.MoreModal>
+            <m.ModalTriangle />
+            <m.ModalDiv>
+              <m.ModalContent
+                onClick={() => {
+                  setModifyCommunityId(detailData.communityId)
+                  navigate('/community/register')
+                }}
+              >
+                수정
+              </m.ModalContent>
+              ∘
+              <m.ModalContent
+                onClick={() => {
+                  onArticleDelete()
+                }}
+              >
+                삭제
+              </m.ModalContent>
+            </m.ModalDiv>
+          </m.MoreModal>
+        ) : (
+          ''
+        )}
+      </m.Header>
 
       <m.Title>{detailData.title}</m.Title>
       <m.Category>

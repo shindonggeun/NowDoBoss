cd47ed2c32a67e0f11e5a1f0590eb619596f8327
이승현 <tmdgus1761@gmail.com>
2024-04-26 14:54:37 +0900

    [FE] feat: 소셜로그인 경우에 따라 다른 UI 구현

--- FrontEnd/src/containers/User/SocialLoginContainer.tsx
+++ FrontEnd/src/containers/User/SocialLoginContainer.tsx
@@ -1,7 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
 import { getSocialAuthUrl } from '@src/api/userApi'
+import * as s from '@src/containers/User/SocialLoginContainerStyle'
 
-const SocialLoginContainer = () => {
+interface Props {
+  state: string
+}
+
+const SocialLoginContainer = (props: Props) => {
+  const { state } = props
+  console.log(state)
+
+  const { data: googleUrl } = useQuery({
+    queryKey: ['googleUrl'],
+    queryFn: () => getSocialAuthUrl('google'),
+  })
+
   const { data: naverUrl } = useQuery({
     queryKey: ['naverUrl'],
     queryFn: () => getSocialAuthUrl('naver'),
@@ -12,29 +25,49 @@ const SocialLoginContainer = () => {
     queryFn: () => getSocialAuthUrl('kakao'),
   })
 
-  const { data: googleUrl } = useQuery({
-    queryKey: ['googleUrl'],
-    queryFn: () => getSocialAuthUrl('google'),
-  })
-
   const handleRedirectUrl = (url: string) => {
     window.location.href = url
   }
 
   return (
-    <div>
-      <div>
-        <div onClick={() => handleRedirectUrl(googleUrl.dataBody)}>
-          구글구글구글
-        </div>
-        <div onClick={() => handleRedirectUrl(naverUrl.dataBody)}>
-          네이버네이버
-        </div>
-        <div onClick={() => handleRedirectUrl(kakaoUrl.dataBody)}>
-          카카오카카오
-        </div>
-      </div>
-    </div>
+    <s.Container>
+      {state === 'login' ? (
+        <>
+          <s.OrTextSmallWrapper>
+            <s.Separator />
+            <s.OrText>또는</s.OrText>
+            <s.Separator />
+          </s.OrTextSmallWrapper>
+          <s.SmallButtonWrapper>
+            <s.GoogleBtnSmall
+              onClick={() => handleRedirectUrl(googleUrl.dataBody)}
+            />
+            <s.NaverBtnSmall
+              onClick={() => handleRedirectUrl(naverUrl.dataBody)}
+            />
+            <s.KakaoBtnSmall
+              onClick={() => handleRedirectUrl(kakaoUrl.dataBody)}
+            />
+          </s.SmallButtonWrapper>
+        </>
+      ) : (
+        <>
+          <s.TopSeparator />
+          <s.ButtonWrapper>
+            <s.GoogleBtn
+              onClick={() => handleRedirectUrl(googleUrl.dataBody)}
+            />
+            <s.NaverBtn onClick={() => handleRedirectUrl(naverUrl.dataBody)} />
+            <s.KakaoBtn onClick={() => handleRedirectUrl(kakaoUrl.dataBody)} />
+          </s.ButtonWrapper>
+          <s.OrTextWrapper>
+            <s.Separator />
+            <s.OrText>또는</s.OrText>
+            <s.Separator />
+          </s.OrTextWrapper>
+        </>
+      )}
+    </s.Container>
   )
 }
 

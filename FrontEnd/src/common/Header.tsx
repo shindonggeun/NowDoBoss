68974007d4d9e8e508a5f16a343b0fef5f6fcc4c
choiseongho <chltjdgh3@naver.com>
2024-04-27 19:27:22 +0900

    [FE] fix: 메인 헤더 style 해당 파일에 병합

--- FrontEnd/src/common/Header.tsx
+++ FrontEnd/src/common/Header.tsx
@@ -1,8 +1,89 @@
-import * as h from '@src/common/style/HeaderStyle'
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { NavigateType } from '@src/types/GlobalType'
 import LogoImg from '@src/assets/logo.svg'
+import styled from 'styled-components'
+
+const Container = styled.header`
+  height: 61px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: #c4c4c4 2px solid;
+  // 상단 고정하기 위한 코드
+  background-color: #fff;
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+`
+
+const MenuListLeft = styled.div<{ isMenuOpen?: boolean }>`
+  display: flex;
+  margin: 0 1rem;
+
+  @media (max-width: 992px) {
+    flex-direction: column;
+    display: ${({ isMenuOpen }) => (isMenuOpen ? 'flex' : 'none')};
+  }
+`
+
+const MenuListRight = styled.div<{ isMenuOpen?: boolean }>`
+  width: 420px;
+  display: flex;
+  justify-content: right;
+  margin: 0 1rem;
+
+  @media (max-width: 992px) {
+    flex-direction: column;
+    display: ${({ isMenuOpen }) => (isMenuOpen ? 'flex' : 'none')};
+  }
+`
+const Menu = styled.div<{ $isActive?: boolean }>`
+  height: 57px;
+  padding: 0 10px;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  font-weight: bold;
+  border-bottom: 4px solid ${props => (props.$isActive ? 'blue' : 'white')};
+  color: ${props => (props.$isActive ? 'blue' : 'black')};
+  &:hover {
+    color: blue;
+    border-bottom: 4px solid blue;
+  }
+
+  @media (max-width: 1200px) {
+    font-size: 14px;
+    padding: 0 10px;
+  }
+`
+
+const LogoDiv = styled.div``
+
+const Logo = styled.img`
+  width: 110px;
+  height: 55px;
+  margin: 7px 0 0 0;
+  cursor: pointer;
+  @media (max-width: 1200px) {
+    scale: 0.9;
+  }
+`
+
+const HamburgerMenu = styled.div`
+  font-size: 60px;
+  display: none;
+
+  @media (max-width: 992px) {
+    display: block; // 화면 너비가 1200px 이하일 경우 햄버거 메뉴 표시
+    justify-content: right;
+    cursor: pointer;
+    margin-right: 2vw;
+  }
+`
 
 const Header = () => {
   const [activeMenu, setActiveMenu] = useState<string | null>(null)
@@ -38,7 +119,7 @@ const Header = () => {
     } else if (menuName === '상권분석') {
       navigate('/')
     } else if (menuName === '상권추천') {
-      navigate('/')
+      navigate('/recommend')
     } else if (menuName === '창업시뮬레이션') {
       navigate('/')
     } else if (menuName === '회원가입') {
@@ -55,36 +136,36 @@ const Header = () => {
   }
 
   return (
-    <h.Container>
-      <h.MenuListLeft>
+    <Container>
+      <MenuListLeft>
         {['상권현황', '상권분석', '상권추천', '창업시뮬레이션', '커뮤니티'].map(
           menuName => (
-            <h.Menu
+            <Menu
               key={menuName}
               $isActive={activeMenu === menuName}
               onClick={() => handleMenuClick(menuName)}
             >
               {menuName}
-            </h.Menu>
+            </Menu>
           ),
         )}
-      </h.MenuListLeft>
-      <h.LogoDiv onClick={() => goNavigate({ url: '/' })}>
-        <h.Logo src={LogoImg} alt="logo" />
-      </h.LogoDiv>
-      <h.MenuListRight>
+      </MenuListLeft>
+      <LogoDiv onClick={() => goNavigate({ url: '/' })}>
+        <Logo src={LogoImg} alt="logo" />
+      </LogoDiv>
+      <MenuListRight>
         {['마이페이지', '로그인', '회원가입'].map(menuName => (
-          <h.Menu
+          <Menu
             key={menuName}
             $isActive={activeMenu === menuName}
             onClick={() => handleMenuClick(menuName)}
           >
             {menuName}
-          </h.Menu>
+          </Menu>
         ))}
-      </h.MenuListRight>
-      <h.HamburgerMenu>≡</h.HamburgerMenu>
-    </h.Container>
+      </MenuListRight>
+      <HamburgerMenu>≡</HamburgerMenu>
+    </Container>
   )
 }
 export default Header

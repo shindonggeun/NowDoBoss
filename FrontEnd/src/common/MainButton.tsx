a5c282f9adf40bf0a98dd168f124eb752d783f07
choiseongho <chltjdgh3@naver.com>
2024-04-27 19:29:07 +0900

    [FE] feat: 메인버튼 공통 컴포넌트로 생성

---
+++ FrontEnd/src/common/MainButton.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+`
+const Content = styled.div`
+  padding: 10px;
+  color: white;
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: 600;
+  border-radius: 10px;
+  margin: 20px;
+  background-color: #236cff;
+
+  &:active {
+    background-color: #0051ff;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`
+type MainButtonPropsType = {
+  buttonContent: string
+}
+
+const MainButton = (props: MainButtonPropsType) => {
+  const { buttonContent } = props
+
+  return (
+    <Container>
+      <Content>{buttonContent}</Content>
+    </Container>
+  )
+}
+
+export default MainButton

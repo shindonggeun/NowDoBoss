008799690e47f5ae94ad82632cb18f646604f0f4
이승현 <tmdgus1761@gmail.com>
2024-04-29 00:40:25 +0900

    [FE] feat: 상권 분석 컴포넌트 기본 style 적용

---
+++ FrontEnd/src/containers/analysis/AnalysisContainerStyle.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const SelectLocationContainer = styled.div`
+  flex: 1;
+  overflow: auto;
+  height: 100vh;
+`

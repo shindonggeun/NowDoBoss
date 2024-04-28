00f9c89ac78f9f5362d95d5bc81ead4c194cda90
이승현 <tmdgus1761@gmail.com>
2024-04-29 00:37:18 +0900

    [FE] feat: 상권 분석 페이지 섹션 분리 및 스크롤 이벤트

--- FrontEnd/src/containers/analysis/AnalysisContainer.tsx
+++ FrontEnd/src/containers/analysis/AnalysisContainer.tsx
@@ -1,5 +1,22 @@
+import ResultSection from '@src/components/analysis/ResultSection'
+import SelectLocation from '@src/components/analysis/SelectLocation'
+import * as a from '@src/containers/analysis/AnalysisContainerStyle'
+import { useRef } from 'react'
+
 const AnalysisContainer = () => {
-  return <>분석 페이지</>
+  const element = useRef<HTMLDivElement>(null)
+  const onMoveBox = () => {
+    element.current?.scrollIntoView({ behavior: 'smooth', block: 'start' })
+  }
+
+  return (
+    <a.Container>
+      <a.SelectLocationContainer onClick={onMoveBox}>
+        <SelectLocation />
+      </a.SelectLocationContainer>
+      <ResultSection />
+    </a.Container>
+  )
 }
 
 export default AnalysisContainer

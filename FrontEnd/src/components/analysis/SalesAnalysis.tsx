466a2cfa967999bb5fb45b1a48bff8206d461b17
이승현 <tmdgus1761@gmail.com>
2024-04-29 17:05:53 +0900

    [FE] feat: 상권분석 매출분석 컴포넌트 작성 및 스타일 추가

--- FrontEnd/src/components/analysis/SalesAnalysis.tsx
+++ FrontEnd/src/components/analysis/SalesAnalysis.tsx
@@ -1,5 +1,24 @@
+import CategoryTitleCard from '@src/components/analysis/CategoryTitleCard'
+import * as s from '@src/components/styles/analysis/SalesAnalysisStyle'
+
 const SalesAnalysis = () => {
-  return <div>매출액</div>
+  return (
+    <>
+      <CategoryTitleCard title="매출분석" />
+      <s.FirstLowContainer>
+        <s.ExpectChart>추정 매출액</s.ExpectChart>
+        <s.AgeChart>연령별 매출액</s.AgeChart>
+      </s.FirstLowContainer>
+      <s.SecondLowContainer>
+        <s.TodaySalesChart>일일 평균 매출액</s.TodaySalesChart>
+        <s.TodayNumberChart>일일 평균 매출 건수</s.TodayNumberChart>
+      </s.SecondLowContainer>
+      <s.ThirdLowContainer>
+        <s.WeekSalesChart>요일별 매출액</s.WeekSalesChart>
+        <s.WeekSalesChart>요일별 매출건수</s.WeekSalesChart>
+      </s.ThirdLowContainer>
+    </>
+  )
 }
 
 export default SalesAnalysis

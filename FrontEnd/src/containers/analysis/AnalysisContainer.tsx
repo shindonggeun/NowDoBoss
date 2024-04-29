55f8bb75e74928a4f59cc8e982b964b81ad02b24
이승현 <tmdgus1761@gmail.com>
2024-04-30 01:51:26 +0900

    [FE] feat: 상권분석 유동인구 api 연결 및 반환데이터 상태관리 추가

--- FrontEnd/src/containers/analysis/AnalysisContainer.tsx
+++ FrontEnd/src/containers/analysis/AnalysisContainer.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import analysisStore from '@src/stores/analysisStore'
+import { getFlowPopulationData } from '@src/api/analysisApi'
 import ResultSection from '@src/components/analysis/ResultSection'
 import * as a from '@src/containers/analysis/AnalysisContainerStyle'
 
 const AnalysisContainer = () => {
+  const { selectedCommercialCode, setFlowPopulationDataBody } = analysisStore()
+
+  const { data: FlowPopulationData, status } = useQuery({
+    queryKey: ['GetFlowPopulationData', selectedCommercialCode],
+    queryFn: () => getFlowPopulationData(selectedCommercialCode),
+  })
+
+  useEffect(() => {
+    if (
+      status === 'success' &&
+      FlowPopulationData?.dataHeader.successCode === 0
+    ) {
+      setFlowPopulationDataBody(FlowPopulationData.dataBody)
+    }
+  }, [status, FlowPopulationData]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <a.Container>
       <a.SelectLocationContainer>

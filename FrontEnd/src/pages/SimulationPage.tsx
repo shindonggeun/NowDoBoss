26f8f6903d5b13ba0bdeb23482b7d53205182057
김온유 <gkc04075@gmail.com>
2024-04-29 16:49:09 +0900

    [FE] feat: 버튼클릭 시 창업 시뮬레이션 모달창 생성

--- FrontEnd/src/pages/SimulationPage.tsx
+++ FrontEnd/src/pages/SimulationPage.tsx
@@ -1,7 +1,20 @@
 import SimulSelction from '@src/components/simulation/SimulSelction'
+import { useState } from 'react'
 
 const StatusPage = () => {
-  return <SimulSelction />
+  const [isOpen, setIsOpen] = useState(true)
+
+  const onClickClose = () => setIsOpen(false)
+
+  return (
+    <>
+      <button type="button" onClick={() => setIsOpen(!isOpen)}>
+        시뮬레이션 버튼
+      </button>
+
+      {isOpen && <SimulSelction open={isOpen} onClose={onClickClose} />}
+    </>
+  )
 }
 
 export default StatusPage

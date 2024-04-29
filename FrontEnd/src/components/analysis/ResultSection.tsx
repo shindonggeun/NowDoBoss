ed011bbc5e32c3043dc68b19de05b87fc7fd3984
이승현 <tmdgus1761@gmail.com>
2024-04-29 17:01:53 +0900

    [FE] feat: 상권 분석 결과 컴포넌트 구조 설정 및 배치 수정

--- FrontEnd/src/components/analysis/ResultSection.tsx
+++ FrontEnd/src/components/analysis/ResultSection.tsx
@@ -5,22 +5,28 @@ import SalesAnalysis from '@src/components/analysis/SalesAnalysis'
 import RentalCostAnalysis from '@src/components/analysis/RentalCostAnalysis'
 import ResidentPopulationAnalysis from '@src/components/analysis/ResidentPopulationAnalysis'
 import ExpenditureAnalysis from '@src/components/analysis/ExpenditureAnalysis'
-import SidebarMenu from '@src/components/analysis/SidebarMenu'
+import SideBarMenu from '@src/components/analysis/SideBarMenu'
+import ResultIntro from '@src/components/analysis/ResultIntro'
 import * as r from '@src/components/styles/analysis/ResultSectionStyle'
 
 const ResultSection = () => {
   return (
     <r.Container>
-      <SidebarMenu />
-      <>
-        <FlowPopulationAnalysis />
-        <FacilitiesAnalysis />
-        <StoreCountAnalysis />
-        <SalesAnalysis />
-        <RentalCostAnalysis />
-        <ResidentPopulationAnalysis />
-        <ExpenditureAnalysis />
-      </>
+      <ResultIntro />
+      <r.ResultContainer>
+        <r.SidebarContainer>
+          <SideBarMenu />
+        </r.SidebarContainer>
+        <r.MainContainer>
+          <FlowPopulationAnalysis />
+          <FacilitiesAnalysis />
+          <StoreCountAnalysis />
+          <SalesAnalysis />
+          <RentalCostAnalysis />
+          <ResidentPopulationAnalysis />
+          <ExpenditureAnalysis />
+        </r.MainContainer>
+      </r.ResultContainer>
     </r.Container>
   )
 }

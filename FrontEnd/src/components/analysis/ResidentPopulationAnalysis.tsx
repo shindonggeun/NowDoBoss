3e1a319d71a3e1106510a0ac296a03a38d41b89d
이승현 <tmdgus1761@gmail.com>
2024-04-29 17:06:35 +0900

    [FE] feat: 상권분석 상주인구 컴포넌트 작성 및 스타일 추가

--- FrontEnd/src/components/analysis/ResidentPopulationAnalysis.tsx
+++ FrontEnd/src/components/analysis/ResidentPopulationAnalysis.tsx
@@ -1,5 +1,16 @@
+import CategoryTitleCard from '@src/components/analysis/CategoryTitleCard'
+import * as r from '@src/components/styles/analysis/ResidentPopulationAnalysisStyle'
+
 const ResidentPopulationAnalysis = () => {
-  return <div>상주인구</div>
+  return (
+    <>
+      <CategoryTitleCard title="상주인구" />
+      <r.FirstLowContainer>
+        <r.TotalChart>고민고민</r.TotalChart>
+        <r.CategoryChart>고민고민</r.CategoryChart>
+      </r.FirstLowContainer>
+    </>
+  )
 }
 
 export default ResidentPopulationAnalysis

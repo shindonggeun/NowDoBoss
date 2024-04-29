078f9917263bf807f7fcb00c7571e41a76e16ce1
김온유 <gkc04075@gmail.com>
2024-04-29 13:39:48 +0900

    [FE] feat: 창업 시뮬레이션 페이지 생성

---
+++ FrontEnd/src/components/simulation/SimulSelction.tsx
@@ -0,0 +1,10 @@
+const SimulSelction = () => {
+  return (
+    <>
+      <div>시뮬레이션 선택페이지</div>
+      <div>시뮬레이션 선택페이지</div>
+    </>
+  )
+}
+
+export default SimulSelction
---
+++ FrontEnd/src/pages/SimulationPage.tsx
@@ -0,0 +1,7 @@
+import SimulSelction from '@src/components/simulation/SimulSelction'
+
+const StatusPage = () => {
+  return <SimulSelction />
+}
+
+export default StatusPage

d2add126d0c1746b8474435528d7c299facc6d80
이승현 <tmdgus1761@gmail.com>
2024-04-29 17:07:01 +0900

    [FE] feat: 상권분석 지출내역 컴포넌트 작성 및 스타일 추가

--- FrontEnd/src/components/analysis/ExpenditureAnalysis.tsx
+++ FrontEnd/src/components/analysis/ExpenditureAnalysis.tsx
@@ -1,5 +1,16 @@
+import CategoryTitleCard from '@src/components/analysis/CategoryTitleCard'
+import * as e from '@src/components/styles/analysis/ExpenditureAnalysisStyle'
+
 const ExpenditureAnalysis = () => {
-  return <div>지출액</div>
+  return (
+    <>
+      <CategoryTitleCard title="지출내역" />
+      <e.FirstLowContainer>
+        <e.TotalChart>총 지출 금액</e.TotalChart>
+        <e.CategoryChart>유형별 지출내역</e.CategoryChart>
+      </e.FirstLowContainer>
+    </>
+  )
 }
 
 export default ExpenditureAnalysis

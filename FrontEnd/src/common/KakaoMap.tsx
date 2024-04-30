8dd01d758149e8f6aa6583412e46979b0975c855
choiseongho <chltjdgh3@naver.com>
2024-04-30 15:47:10 +0900

    [FE] feat: 카카오맵 로직 수정

--- FrontEnd/src/common/KakaoMap.tsx
+++ FrontEnd/src/common/KakaoMap.tsx
@@ -1,8 +1,9 @@
 import useKakaoLoader from '@src/hooks/useKakaoLoader'
-import { Map, Polygon } from 'react-kakao-maps-sdk'
+import { CustomOverlayMap, Map, Polygon } from 'react-kakao-maps-sdk'
 import { useRef, useState } from 'react'
 import {
   DataBodyType,
+  LatLng,
   LatLngDataType,
   PromiseDataType,
 } from '@src/types/MapType'
@@ -12,21 +13,45 @@ import {
   fetchDistrict,
 } from '@src/api/mapApi'
 import { useQuery } from '@tanstack/react-query'
+import styled from 'styled-components'
+
+// 마우스 호버 시 뜨는 지역 명
+const PlaceBox = styled.div`
+  position: absolute;
+  background: #236eff;
+  color: #ffffff;
+  //border: 1px solid #888;
+  border-radius: 3px;
+  font-size: 12px;
+  font-weight: 500;
+  top: -5px;
+  left: 15px;
+  padding: 5px;
+`
 
 const KakaoMap = () => {
   // 카카오 지도 호출 코드
   useKakaoLoader()
+
   const mapRef = useRef<kakao.maps.Map>(null)
 
-  // 마우스 올렸을 때
+  // 마우스 올렸을 때 해당 동네 이름 저장
   const [isMouseOver, setIsMouseOver] = useState<string>('')
-  // const [mouseOverLatLng, setMouseOverLatLng] = useState<LatLng>({
-  //   lat: 0,
-  //   lng: 0,
-  // })
+
+  // 마우스 올렸을 때 해당 동네 좌표 저장
+  const [mouseOverLatLng, setMouseOverLatLng] = useState<LatLng>({
+    lat: 37.57023786206844,
+    lng: 126.94665085220238,
+  })
 
   // 현재 지도 level 저장한 값
-  const [level, setLevel] = useState<number>(9)
+  const [level, setLevel] = useState<number>(8)
+
+  // 지도의 중심 좌표
+  const [centerLatLng, setCenterLatLng] = useState<LatLng>({
+    lat: 37.57023786206844,
+    lng: 126.94665085220238,
+  })
 
   // 현재 화면 우상단, 좌하단 좌표 값
   const [latLngData, setLatLngData] = useState<LatLngDataType>({
@@ -55,8 +80,7 @@ const KakaoMap = () => {
   const { data, isLoading } = useQuery<PromiseDataType>({
     queryKey: ['fetchPoligonPath', latLngData],
     queryFn: async () => {
-      console.log(`level : ${level}`)
-      if (level > 7) {
+      if (level > 6) {
         return fetchDistrict(latLngData)
       }
       if (level > 4) {
@@ -66,26 +90,7 @@ const KakaoMap = () => {
     },
   })
 
-  // 데이터를 폴리곤 path 형식으로 변환하는 함수
-  // const parsePolygonData = (dataBody: DataBodyType) => {
-  //   return Object.keys(dataBody.coords).map(key => ({
-  //     code: key,
-  //     center: dataBody.codes[key]
-  //       ? {
-  //           // 중심 좌표 추가
-  //           lng: dataBody.codes[key][0],
-  //           lat: dataBody.codes[key][1],
-  //         }
-  //       : undefined,
-  //     path: dataBody.coords[key].map(([lng, lat]) => ({
-  //       lng,
-  //       lat,
-  //     })),
-  //   }))
-  // }
-  if (data) {
-    console.log(data.dataBody)
-  }
+  // 호출받은 데이터 재가공
   const parsePolygonData = (dataBody: DataBodyType) => {
     return Object.keys(dataBody.coords)
       .map(key => {
@@ -93,7 +98,7 @@ const KakaoMap = () => {
         const centerCoords = dataBody.names[key]
 
         return {
-          code: key,
+          name: key,
           center: {
             lng: centerCoords[0],
             lat: centerCoords[1],
@@ -107,21 +112,55 @@ const KakaoMap = () => {
       .filter(item => item !== null) // 중심 좌표가 없어서 null로 처리된 항목들을 제거
   }
 
+  // // store에 저장된 구 데이터와 선택한 구, 동, 상권 값 가져올 store
+  // const {
+  //   districtData,
+  //   selectedDistrict,
+  //   setSelectedDistrict,
+  //   selectedAdministration,
+  //   setSelectedAdministration,
+  //   setSelectedCommercial,
+  // } = useSelectPlaceStore(state => ({
+  //   districtData: state.districtData,
+  //   selectedDistrict: state.selectedDistrict,
+  //   setSelectedDistrict: state.setSelectedDistrict,
+  //   selectedAdministration: state.selectedAdministration,
+  //   setSelectedAdministration: state.setSelectedAdministration,
+  //   setSelectedCommercial: state.setSelectedCommercial,
+  // }))
+  //
+  // useEffect(() => {
+  //   if (data) {
+  //     parsePolygonData(data.dataBody).map(code => {
+  //       return <div key={code.name} />
+  //     })
+  //   }
+  // }, [data, selectedDistrict])
+
   return (
     <div>
       <div>
         <Map
-          center={{ lat: 37.57023786206844, lng: 126.94665085220238 }}
+          center={centerLatLng}
           style={{ width: '100%', height: 'calc(100vh - 75px)' }}
           ref={mapRef}
           level={level}
           onDragEnd={map => updateMapBounds(map)}
           onZoomChanged={map => updateMapBounds(map)}
-          // minLevel={9}
+          onClick={map => {
+            updateMapBounds(map)
+          }}
+          minLevel={9}
+          // 마우스가 움직일 때 위치를 위경도로 저장
+          onMouseMove={(_map, mouseEvent) =>
+            setMouseOverLatLng({
+              lat: mouseEvent.latLng.getLat(),
+              lng: mouseEvent.latLng.getLng(),
+            })
+          }
         >
           {!isLoading && data
             ? parsePolygonData(data.dataBody).map((code, index) => {
-                console.log(code)
                 // 색상 배열 정의
                 const colors = [
                   '#FF6347',
@@ -139,44 +178,44 @@ const KakaoMap = () => {
 
                 return (
                   <Polygon
-                    key={index}
+                    key={code.name}
                     path={code.path}
                     strokeWeight={3} // 선의 두께입니다
-                    // strokeColor="#39DE2A" // 선의 색깔입니다
                     strokeColor={fillColor} // 선의 색깔입니다
                     strokeOpacity={0.8} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
                     strokeStyle="longdash" // 선의 스타일입니다
+                    // strokeColor="#39DE2A" // 선의 색깔입니다
                     // fillColor={isMouseOver ? '#70ff5c' : '#A2FF99'} // 채우기 색깔입니다
                     fillColor={fillColor} // 채우기 색깔입니다
-                    fillOpacity={isMouseOver === code.code ? 0.8 : 0.6} // 채우기 불투명도입니다
+                    zIndex={100}
+                    fillOpacity={isMouseOver === code.name ? 0.8 : 0.6} // 채우기 불투명도입니다
                     onMouseover={() => {
-                      // if (code.center) {
-                      //   setMouseOverLatLng(code.center)
-                      // }
-                      return setIsMouseOver(code.code)
+                      // setMouseOverLatLng(code.center)
+                      return setIsMouseOver(code.name)
                     }}
                     onMouseout={() => setIsMouseOver('')}
-                    onMousedown={() => {
-                      console.log(code.code)
+                    onMousedown={() => {}}
+                    onClick={() => {
+                      if (level > 4) {
+                        setLevel(level - 1)
+                      }
+                      setCenterLatLng(code.center)
                     }}
                   />
                 )
               })
             : ''}
-          {/* {mouseOverLatLng ? ( */}
-          {/*  <MapInfoWindow // 인포윈도우를 생성하고 지도에 표시합니다 */}
-          {/*    position={{ */}
-          {/*      // 인포윈도우가 표시될 위치입니다 */}
-          {/*      lat: mouseOverLatLng.lat, */}
-          {/*      lng: mouseOverLatLng.lng, */}
-          {/*    }} */}
-          {/*    removable // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다 */}
-          {/*  > */}
-          {/*    <div style={{ padding: '5px', color: '#000' }}>Hello World!</div> */}
-          {/*  </MapInfoWindow> */}
-          {/* ) : ( */}
-          {/*  '' */}
-          {/* )} */}
+          {isMouseOver && (
+            <CustomOverlayMap
+              position={{
+                // 인포윈도우가 표시될 위치입니다
+                lat: mouseOverLatLng.lat,
+                lng: mouseOverLatLng.lng,
+              }}
+            >
+              <PlaceBox>{isMouseOver}</PlaceBox>
+            </CustomOverlayMap>
+          )}
         </Map>
       </div>
     </div>

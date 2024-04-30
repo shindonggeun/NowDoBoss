a675e4fb0487325d1ff525fea5b11907792c284e
choiseongho <chltjdgh3@naver.com>
2024-04-30 15:06:22 +0900

    [FE] feat: 구, 동 코드로 동, 상권 목록 get api 구현

--- FrontEnd/src/api/mapApi.tsx
+++ FrontEnd/src/api/mapApi.tsx
@@ -1,5 +1,9 @@
 import { customAxios } from '@src/util/auth/customAxios'
-import { LatLngDataType } from '@src/types/MapType'
+import {
+  LatLngDataType,
+  PromiseCommercialDataType,
+  PromiseDongDataType,
+} from '@src/types/MapType'
 
 export const fetchDistrict = async (LatLngData: LatLngDataType) => {
   return customAxios
@@ -24,6 +28,26 @@ export const fetchCommercial = async (LatLngData: LatLngDataType) => {
     .get(
       `/map/commercial?lngNE=${LatLngData.lngNE}&latNE=${LatLngData.latNE}&lngSW=${LatLngData.lngSW}&latSW=${LatLngData.latSW}`,
     )
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 구 코드 보내고 동 목록 받아오는 get api
+export const fetchDongList = async (
+  districtCode: number,
+): Promise<PromiseDongDataType> => {
+  return customAxios
+    .get(`/commercial/administration/district/${districtCode}/areas`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 동 코드 보내고 상권 목록 받아오는 get api
+export const fetchAdministrationList = async (
+  administrationCode: number,
+): Promise<PromiseCommercialDataType> => {
+  return customAxios
+    .get(`/commercial/administration/${administrationCode}/areas`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }

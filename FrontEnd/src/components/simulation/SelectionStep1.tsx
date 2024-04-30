c4e48830bdde1fe900428f3def3d8350ae8e6f10
김온유 <gkc04075@gmail.com>
2024-04-30 14:54:13 +0900

    [FE] feat: 시뮬 분석 step1 입력창

--- FrontEnd/src/components/simulation/SelectionStep1.tsx
+++ FrontEnd/src/components/simulation/SelectionStep1.tsx
@@ -2,29 +2,45 @@ import * as c from '@src/components/styles/simulation/StepStyle'
 
 interface Step1Props {
   nextStep: () => void
-  isFranchise: boolean
-  onSelectedkFranchise: (value: boolean) => void 
+  isFranchise: boolean | null
+  onSelectedkFranchise: (value: boolean) => void
 }
 
-const SelectionStep1 = ({ nextStep, isFranchise, onSelectedkFranchise}: Step1Props) => {
+const SelectionStep1 = ({
+  nextStep,
+  isFranchise,
+  onSelectedkFranchise,
+}: Step1Props) => {
   return (
     <c.Container>
-      <h2>setp1 페이지 입니다</h2>
+      {/* <h2>setp1 페이지 입니다</h2> */}
       <c.Title>
         <c.Emphasis>프렌차이즈</c.Emphasis>창업을 생각하고 계신가요?
       </c.Title>
-      <div>
-        <c.SelectButton size="m" selected={isFranchise} type="button" onClick={() => onSelectedkFranchise(true)}>
+      <c.FranchiseContainer>
+        <c.SelectButton
+          size="m"
+          selected={isFranchise === true}
+          type="button"
+          onClick={() => {
+            onSelectedkFranchise(true)
+            nextStep()
+          }}
+        >
           네!
         </c.SelectButton>
-        <c.SelectButton size="m" selected={!isFranchise} type="button" onClick={() => onSelectedkFranchise(false)}>
+        <c.SelectButton
+          size="m"
+          selected={isFranchise === false}
+          type="button"
+          onClick={() => {
+            onSelectedkFranchise(false)
+            nextStep()
+          }}
+        >
           아니요!
         </c.SelectButton>
-      </div>
-  
-      <button type="button" onClick={nextStep}>
-        다음
-      </button>
+      </c.FranchiseContainer>
     </c.Container>
   )
 }

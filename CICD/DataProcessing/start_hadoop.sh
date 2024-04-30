bf00d448650c829ae233dc1fdeecaa2bcd5d83e1
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-30 09:58:23 +0900

    [BE] feat 하둡 시작 스크립트 파일 수정

--- CICD/DataProcessing/start_hadoop.sh
+++ CICD/DataProcessing/start_hadoop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# HDFS 데몬 시작
+# 하둡이 자바 환경을 찾을 수 있도록 JAVA_HOME 환경변수를 설정합니다.
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# HDFS 데몬 시작합니다.
 $HADOOP_HOME/sbin/start-dfs.sh
-# YARN 데몬 시작
+
+# YARN 데몬 시작합니다.
 $HADOOP_HOME/sbin/start-yarn.sh

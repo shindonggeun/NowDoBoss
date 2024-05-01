f7a9575a600d2632d42610e320c9b856d567f22d
이승현 <tmdgus1761@gmail.com>
2024-05-02 02:27:32 +0900

    [FE] feat: 상권분석 남/녀 상주인구 컴포넌트 작성 및 스타일 적용

--- FrontEnd/src/components/analysis/residentPopulation/GenderChart.tsx
+++ FrontEnd/src/components/analysis/residentPopulation/GenderChart.tsx
@@ -1,9 +1,34 @@
+import analysisStore from '@src/stores/analysisStore'
+import DoughnutChart from '@src/common/DoughnutChart'
 import * as r from '@src/components/styles/analysis/ResidentPopulationAnalysisStyle'
 
 const GenderChart = () => {
+  const { residentPopulationDataBody } = analysisStore()
+
+  const { malePercentage } = residentPopulationDataBody
+  const { femalePercentage } = residentPopulationDataBody
+  const totalResidentPopulation =
+    residentPopulationDataBody.populationInfo.totalPopulation.toLocaleString(
+      'ko-KR',
+    )
+
+  const labels: string[] = ['남성', '여성']
+  const value: number[] = [malePercentage, femalePercentage]
+  const textCenter = `총 상주인구 수 : ${totalResidentPopulation}명`
+
+  const maxValue = Math.max(malePercentage, femalePercentage)
+  const minValue = Math.min(malePercentage, femalePercentage)
+  const ratio = (maxValue / minValue).toFixed(1)
+  const maxLabel = labels[value.indexOf(maxValue)]
+  const minLabel = labels[value.indexOf(minValue)]
+
   return (
     <r.GenderChart>
       <r.ChartTitle>남/녀 상주인구</r.ChartTitle>
+      <r.ChartSubTitle>
+        {minLabel} 보다 {maxLabel}의 상주인구가 약 {ratio}배 더 많아요.
+      </r.ChartSubTitle>
+      <DoughnutChart labels={labels} value={value} textCenter={textCenter} />
     </r.GenderChart>
   )
 }

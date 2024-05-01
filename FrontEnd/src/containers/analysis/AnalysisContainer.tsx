c474c73f2af2c039724f1161083ae9a98126130e
이승현 <tmdgus1761@gmail.com>
2024-05-01 23:27:09 +0900

    [FE] feat: 상권분석 상주인구 api 연결 및 반환데이터 상태관리 로직 추가

--- FrontEnd/src/containers/analysis/AnalysisContainer.tsx
+++ FrontEnd/src/containers/analysis/AnalysisContainer.tsx
@@ -1,26 +1,50 @@
 import { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import analysisStore from '@src/stores/analysisStore'
-import { getFlowPopulationData } from '@src/api/analysisApi'
+import {
+  getFlowPopulationData,
+  getResidentPopulationData,
+} from '@src/api/analysisApi'
 import ResultSection from '@src/components/analysis/ResultSection'
 import * as a from '@src/containers/analysis/AnalysisContainerStyle'
 
 const AnalysisContainer = () => {
-  const { selectedCommercialCode, setFlowPopulationDataBody } = analysisStore()
+  const {
+    selectedCommercialCode,
+    setFlowPopulationDataBody,
+    setResidentPopulationDataBody,
+  } = analysisStore()
 
-  const { data: FlowPopulationData, status } = useQuery({
+  // 유동인구
+  const { data: FlowPopulationData, status: flowPopulationStatus } = useQuery({
     queryKey: ['GetFlowPopulationData', selectedCommercialCode],
     queryFn: () => getFlowPopulationData(selectedCommercialCode),
   })
 
   useEffect(() => {
     if (
-      status === 'success' &&
+      flowPopulationStatus === 'success' &&
       FlowPopulationData?.dataHeader.successCode === 0
     ) {
       setFlowPopulationDataBody(FlowPopulationData.dataBody)
     }
-  }, [status, FlowPopulationData]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [flowPopulationStatus, FlowPopulationData]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  // 상주인구
+  const { data: ResidentPopulationData, status: residentPopulationStatus } =
+    useQuery({
+      queryKey: ['GetResidentPopulationData', selectedCommercialCode],
+      queryFn: () => getResidentPopulationData(selectedCommercialCode),
+    })
+
+  useEffect(() => {
+    if (
+      residentPopulationStatus === 'success' &&
+      ResidentPopulationData?.dataHeader.successCode === 0
+    ) {
+      setResidentPopulationDataBody(ResidentPopulationData.dataBody)
+    }
+  }, [residentPopulationStatus, ResidentPopulationData]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <a.Container>

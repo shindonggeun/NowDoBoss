286f069210fb4f889b503c556b3766e9425dd217
choiseongho <chltjdgh3@naver.com>
2024-05-02 13:38:31 +0900

    [FE] feat: 추천 상자 접기 버튼 구현

---
+++ FrontEnd/src/common/ReduceButton.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import up_arrow from '@src/assets/arrow_up.svg'
+import down_arrow from '@src/assets/arrow_down.svg'
+
+const Container = styled.div`
+  background-color: #f2f2f2;
+  border: 2px solid #d9d9d9;
+  margin: 10px 0;
+  border-radius: 12px;
+  width: 55px;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 10px 10px 15px -5px rgba(0, 0, 0, 0.2);
+`
+const Image = styled.img`
+  scale: 1.3;
+`
+
+type ReduceButtonPropsType = {
+  isOpen: boolean
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ReduceButton = (props: ReduceButtonPropsType) => {
+  const { isOpen, setIsOpen } = props
+
+  return (
+    <Container onClick={() => setIsOpen(!isOpen)}>
+      {isOpen ? <Image src={up_arrow} /> : <Image src={down_arrow} />}
+    </Container>
+  )
+}
+export default ReduceButton

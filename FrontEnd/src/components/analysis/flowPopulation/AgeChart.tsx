7062e36d0d0f94bd84c1f5d4934d91ac013f73b9
이승현 <tmdgus1761@gmail.com>
2024-05-02 19:56:07 +0900

    [FE] feat: 상권분석 연령성별 radar 차트 생성

--- FrontEnd/src/components/analysis/flowPopulation/AgeChart.tsx
+++ FrontEnd/src/components/analysis/flowPopulation/AgeChart.tsx
@@ -1,3 +1,4 @@
+import RadarChart from '@src/common/RadarChart'
 import * as f from '@src/components/styles/analysis/FlowPopulationAnalysisStyle'
 
 const AgeChart = () => {
@@ -5,6 +6,7 @@ const AgeChart = () => {
     <f.AgeChart>
       <f.ChartTitle>연령별 유동인구</f.ChartTitle>
       <f.ChartSubTitle>60대 이상 남성 유동인구가 가장 높아요.</f.ChartSubTitle>
+      <RadarChart />
     </f.AgeChart>
   )
 }

56e63a2688f009a7a0bd941ad095ac761f5b49b6
김온유 <gkc04075@gmail.com>
2024-05-02 12:01:03 +0900

    [FE] feat: 시뮬레이션 레포트 api 수정

--- FrontEnd/src/api/simulationApi.tsx
+++ FrontEnd/src/api/simulationApi.tsx
@@ -9,6 +9,8 @@ import { customAxios } from '@src/util/auth/customAxios'
 
 // 시뮬레이션 업종별 가게 사이즈
 export const fetchStoreSize = async (serviceCode: string) => {
+  console.log(serviceCode)
+  console.log('가게 데이터 가져오는중')
   return customAxios
     .get(`/simulation/store?serviceCode=${serviceCode}`)
     .then(res => res.data)

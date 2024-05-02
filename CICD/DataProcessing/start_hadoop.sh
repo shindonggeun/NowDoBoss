05124b31ef619983752517a373a8051aa1c70bee
tlsehdrms124 <tlsehdrms124@naver.com>
2024-05-02 09:49:56 +0900

    [BE] feat: Spark 젠킨스 파이프라인 설정 및 컨테이너 설정

--- CICD/DataProcessing/start_hadoop.sh
+++ CICD/DataProcessing/start_hadoop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # HDFS 데몬 시작합니다.
-$HADOOP_HOME/sbin/start-dfs.sh
+su hdfs -c "$HADOOP_HOME/sbin/start-dfs.sh"
 
 # YARN 데몬 시작합니다.
-$HADOOP_HOME/sbin/start-yarn.sh
+su yarn -c "$HADOOP_HOME/sbin/start-yarn.sh"

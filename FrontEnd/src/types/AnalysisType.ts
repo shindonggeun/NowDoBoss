9f3cf7175dedc965daf491741492b1de46e453cf
이승현 <tmdgus1761@gmail.com>
2024-05-02 11:56:56 +0900

    [FE] feat: 재사용 차트 컴포넌트 prop type 삭제

--- FrontEnd/src/types/AnalysisType.ts
+++ FrontEnd/src/types/AnalysisType.ts
@@ -70,30 +70,3 @@ export type AnalysisStoreType = {
 export type CategoryTitleCardProps = {
   title: string
 }
-
-export type AreaChartPropsType = {
-  labels: string[]
-  values: number[]
-}
-
-export type BarChartPropsType = {
-  labels: string[]
-  values: number[]
-}
-
-export type ComboChartPropsType = {
-  labels: string[]
-  value1: number[]
-  value2: number[]
-}
-
-export type DoughnutChartPropsType = {
-  labels: string[]
-  value: number[]
-  textCenter: string
-}
-
-export type HalfDoughnutChartPropsType = {
-  labels: string[]
-  values: number[]
-}

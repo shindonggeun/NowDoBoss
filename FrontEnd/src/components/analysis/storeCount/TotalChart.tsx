449f4c768fc3c9facecc8acd0533b49b6c1565a9
이승현 <tmdgus1761@gmail.com>
2024-05-02 11:45:34 +0900

    [FE] feat: 상권분석 선택 상권 총 점포 수 컴포넌트 더미데이터 추가

--- FrontEnd/src/components/analysis/storeCount/TotalChart.tsx
+++ FrontEnd/src/components/analysis/storeCount/TotalChart.tsx
@@ -1,11 +1,15 @@
+import analysisStore from '@src/stores/analysisStore'
 import * as s from '@src/components/styles/analysis/StoreCountAnalysisStyle'
 
 const TotalChart = () => {
+  const selectedService = analysisStore(state => state.selectedService)
+
   return (
     <s.TotalChart>
       <s.ChartTitle>선택 상권 총 점포 수</s.ChartTitle>
       <s.ChartSubTitle>
-        선택하신 업종 00000과 유사한 업종 점포가 00개 있어요.
+        선택하신 업종 {selectedService.serviceCodeName}과 유사한 업종 점포가
+        00개 있어요.
       </s.ChartSubTitle>
     </s.TotalChart>
   )

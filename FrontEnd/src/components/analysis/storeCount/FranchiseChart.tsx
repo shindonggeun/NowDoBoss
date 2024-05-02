72661c08b01cde94535e444b3028976547881599
이승현 <tmdgus1761@gmail.com>
2024-05-02 11:44:22 +0900

    [FE] feat: 상권분석 프랜차이즈 점포 수 컴포넌트 더미 데이터 추가 및 차트 생성

--- FrontEnd/src/components/analysis/storeCount/FranchiseChart.tsx
+++ FrontEnd/src/components/analysis/storeCount/FranchiseChart.tsx
@@ -1,12 +1,22 @@
+import analysisStore from '@src/stores/analysisStore'
+import HalfDoughnutChart from '@src/common/HalfDoughnutChart'
 import * as s from '@src/components/styles/analysis/StoreCountAnalysisStyle'
 
 const FranchiseChart = () => {
+  const selectedService = analysisStore(state => state.selectedService)
+
+  const labels = ['프랜차이즈', '일반']
+  const values = [3, 8]
+
   return (
     <s.FranchiseChart>
       <s.ChartTitle>프랜차이즈 점포 수</s.ChartTitle>
-      <s.ChartSubTitle>
-        선택하신 업종 00000의 프랜차이즈 점포는 00개 있어요.
-      </s.ChartSubTitle>
+      <s.ChartSubTitleWrap>
+        <s.ChartSubTitle>{selectedService.serviceCodeName}의</s.ChartSubTitle>
+        <s.ChartSubTitle>프랜차이즈 점포는</s.ChartSubTitle>
+        <s.ChartSubTitle>00개 있어요.</s.ChartSubTitle>
+      </s.ChartSubTitleWrap>
+      <HalfDoughnutChart labels={labels} values={values} />
     </s.FranchiseChart>
   )
 }

6a19d19052f2288922db2ef939777670a1c0dac3
이승현 <tmdgus1761@gmail.com>
2024-05-04 02:41:41 +0900

    [FE] feat: 상권분석 업종 선택 컴포넌트 style 적용

---
+++ FrontEnd/src/components/styles/analysis/ChoiceServiceStyle.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin: 10px 10px;
+  text-align: left;
+`
+
+export const Title = styled.div`
+  font-weight: 500;
+  font-size: 0.9rem;
+`

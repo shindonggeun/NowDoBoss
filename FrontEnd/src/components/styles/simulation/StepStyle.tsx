4525e590975abf9132e4d237b484cc77adffd880
김온유 <gkc04075@gmail.com>
2024-05-03 16:10:55 +0900

    [FE] feat: 주소검색 페이지 style 수정

--- FrontEnd/src/components/styles/simulation/StepStyle.tsx
+++ FrontEnd/src/components/styles/simulation/StepStyle.tsx
@@ -182,3 +182,72 @@ export const GoReportButton = styled.button`
     transform: scale(1.09);
   }
 `
+
+export const DaumContainer = styled.div`
+  margin-top: 2rem;
+`
+
+export const InfoText = styled.div`
+  font-size: 0.9rem;
+  margin-top: 5px;
+  color: #7777777;
+`
+
+export const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+`
+
+export const SearchIcon = styled.img`
+  position: absolute;
+  left: 0;
+  bottom: 5px;
+  width: 1.8rem;
+  cursor: pointer;
+`
+
+export const StyledInput = styled.input`
+  width: 40rem;
+  border: none;
+  border-bottom: 1px solid #5b5b5b;
+  color: #30353b;
+  padding: 8px 2.5rem;
+  font-size: 1rem;
+
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid #2e83f2;
+    background-color: #fff;
+    color: #333;
+  }
+
+  &::placeholder {
+    color: #4c545e;
+    font-style: italic;
+  }
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 55vh;
+`
+
+export const NextButton = styled.button`
+  transition: background-color 0.2s;
+  border-radius: 8px;
+  background-color: #2e83f2;
+  color: white;
+  border: 2px solid #2e83f2;
+
+  font-size: 14px;
+  font-weight: 600;
+  text-align: center;
+  cursor: pointer;
+  width: 15rem;
+  height: 3rem;
+`

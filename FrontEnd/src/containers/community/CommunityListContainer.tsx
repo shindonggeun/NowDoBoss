133e1ddbf2879df5bf0194598a7f1b478e043639
choiseongho <chltjdgh3@naver.com>
2024-05-03 11:35:42 +0900

    [FE] feat: 커뮤니티 목록 중첩 라우팅 적용 및 style 수정

---
+++ FrontEnd/src/containers/community/CommunityListContainer.tsx
@@ -0,0 +1,35 @@
+import PopularChatList from '@src/components/community/list/PopularChatList'
+import ArticleList from '@src/components/community/list/ArticleList'
+import { useQuery } from '@tanstack/react-query'
+import { fetchCommunityList } from '@src/api/communityApi'
+import { useEffect } from 'react'
+import useCommunityStore from '@src/stores/communityStore'
+import * as c from '@src/containers/community/CommunityContainerStyle'
+
+const CommunityListContainer = () => {
+  const { category } = useCommunityStore(state => ({
+    category: state.selectedCategory,
+  }))
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['CommunityList'],
+    queryFn: () => fetchCommunityList(category.value),
+  })
+  useEffect(() => {
+    refetch()
+  }, [refetch, category])
+  return (
+    <c.Mid>
+      {!isLoading && data ? (
+        <c.Div>
+          <PopularChatList />
+          <ArticleList articleList={data.dataBody} />
+        </c.Div>
+      ) : (
+        <c.Div>데이터 호출 에러</c.Div>
+      )}
+    </c.Mid>
+  )
+}
+
+export default CommunityListContainer

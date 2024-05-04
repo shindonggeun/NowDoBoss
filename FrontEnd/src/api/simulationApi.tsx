cdce03bdc7001c32a64aaaec28d4a777283b9de4
김온유 <gkc04075@gmail.com>
2024-05-05 02:51:38 +0900

    [FE] feat: 프렌차이즈 검색 api 연결

--- FrontEnd/src/api/simulationApi.tsx
+++ FrontEnd/src/api/simulationApi.tsx
@@ -9,10 +9,22 @@ import { customAxios } from '@src/util/auth/customAxios'
 
 // 시뮬레이션 업종별 가게 사이즈
 export const fetchStoreSize = async (serviceCode: string) => {
-  console.log(serviceCode)
-  console.log('가게 데이터 가져오는중')
+  // console.log(serviceCode)
+  // console.log('가게 데이터 가져오는중')
   return customAxios
     .get(`/simulation/store?serviceCode=${serviceCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 시뮬레이션 프렌차이즈 검색
+export const fetchFranchiseList = async (
+  keyword: string | null,
+  lastId: number,
+) => {
+  // console.log('==프렌차이즈 검색중==')
+  return customAxios
+    .get(`/simulation/franchisee?keyword=${keyword}&lastId=${lastId}`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }

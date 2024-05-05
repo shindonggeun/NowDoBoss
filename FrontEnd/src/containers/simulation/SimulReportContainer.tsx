1a64a0514bc408aac480f6a710a28e05c0d2398d
김온유 <gkc04075@gmail.com>
2024-05-06 01:14:45 +0900

    [FE] feat: 닫혔을 때 데이터 초기화 및 시뮬데이터 받아오는중

--- FrontEnd/src/containers/simulation/SimulReportContainer.tsx
+++ FrontEnd/src/containers/simulation/SimulReportContainer.tsx
@@ -1,15 +1,54 @@
 import * as c from '@src/components/styles/simulation/SelectionStyle'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ReportSummary from '@src/components/simulation/report/ReportSummary'
 import ReportHeader from '@src/components/simulation/report/ReportHeader'
 import ReportDetail from '@src/components/simulation/report/ReportDetail'
 import ReportGender from '@src/components/simulation/report/ReportGender'
 import ReportMonthAnalysis from '@src/components/simulation/report/ReportMonthAnalysis'
+import useSimulationStore from '@src/stores/simulationStore'
+import useReportStore from '@src/stores/reportStore'
+import { useLocation } from 'react-router-dom'
 
 const SimulReportContainer = () => {
+  const {
+    setIsFranchise,
+    setBrandName,
+    setCategory,
+    setSubCategoryName,
+    setSubCategoryCode,
+    setBulidingSize,
+    setFloor,
+  } = useSimulationStore()
+  const { setAddress, setQuery, setSido, setSigungu } = useReportStore()
+  const { state } = useLocation()
+
+  useEffect(() => {
+    if (state) {
+      console.log('=============')
+      console.log(state)
+    }
+  }, [state])
+
+  const resetButton = () => {
+    setIsFranchise(null)
+    setBrandName(null)
+    setCategory('')
+    setSubCategoryName('')
+    setSubCategoryCode('')
+    setBulidingSize(0)
+    setFloor('')
+    setAddress('')
+    setQuery('')
+    setSido('')
+    setSigungu('')
+  }
+
   const [isOpen, setIsOpen] = useState(true)
 
-  const onClose = () => setIsOpen(false)
+  const onClose = () => {
+    setIsOpen(false)
+    resetButton()
+  }
 
   return (
     <div>

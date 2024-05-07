23a6fe2a400e3676b9b7fefeac4d17d8cf3ca8c8
choiseongho <chltjdgh3@naver.com>
2024-05-07 17:55:28 +0900

    [FE] fix: stomp 연결 로직 수정

--- FrontEnd/src/util/chat/stompClient.ts
+++ FrontEnd/src/util/chat/stompClient.ts
@@ -11,35 +11,35 @@ let stompClient: StompClient = null
 // STOMP 클라이언트 연결 함수
 export const connectStompClient = (
   serverURL: string,
-  token: string,
-  roomId: string | undefined,
-  category: string | undefined,
+  // token: string,
   onConnected: OnConnectedCallback,
   onError: OnErrorCallback,
 ): void => {
   const socket = new WebSocket(serverURL)
-  const client = over(socket)
+  // 연결이 된 후 메세지를 보내는 로직
+  socket.onopen = () => {
+    const client = over(socket)
 
-  const headers = {
-    roomId: `${roomId}`,
-    category: `${category}`,
-    Authorization: `Bearer ${token}`,
+    client.debug = () => {}
+    const headers = {
+      // Authorization: `Bearer ${token}`,
+    }
+
+    // 수정된 connect 호출 부분
+    client.connect(
+      headers,
+      (frame?: Frame) => {
+        if (frame) {
+          onConnected(client)
+        }
+      },
+      (error: Frame | CloseEvent) => {
+        onError(error instanceof Frame ? error : 'CloseEvent')
+      },
+    )
+
+    stompClient = client
   }
-
-  // 수정된 connect 호출 부분
-  client.connect(
-    headers,
-    (frame?: Frame) => {
-      if (frame) {
-        onConnected(client)
-      }
-    },
-    (error: Frame | CloseEvent) => {
-      onError(error instanceof Frame ? error : 'CloseEvent')
-    },
-  )
-
-  stompClient = client
 }
 
 // STOMP 클라이언트 객체를 가져오는 함수

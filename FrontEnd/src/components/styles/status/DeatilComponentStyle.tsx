1e00fdc007bdd4db589835e93917dce0f709aa08
김온유 <gkc04075@gmail.com>
2024-05-07 11:23:33 +0900

    [FE] feat: 유동인구 상세 그래프 적용 (반응형 수정 필요)

--- FrontEnd/src/components/styles/status/DeatilComponentStyle.tsx
+++ FrontEnd/src/components/styles/status/DeatilComponentStyle.tsx
@@ -1,5 +1,21 @@
 import styled from 'styled-components'
 
+export const MixConatiner = styled.div`
+  //display: flex;
+  //flex-direction: row;
+  //align-items: center;
+  //justify-content: center;
+  //flex-wrap: wrap;
+`
+
+export const MixInnerConatiner = styled.div`
+  //width: 15vw;
+  //padding: 1rem;
+  //@media (max-width: 5rem) {
+  //  width: 50vw;
+  //}
+`
+
 export const AnalysisTitle = styled.div`
   font-size: 20px;
   font-weight: 600;

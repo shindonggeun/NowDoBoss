981fb20152806f5d854995063ef0a2895a4be121
choiseongho <chltjdgh3@naver.com>
2024-05-07 17:57:56 +0900

    [FE] feat: 채팅 목록 불러오는 부분 추가

--- FrontEnd/src/components/chatting/ChattingBody.tsx
+++ FrontEnd/src/components/chatting/ChattingBody.tsx
@@ -1,6 +1,13 @@
 import * as c from '@src/components/styles/chatting/ChattingBodyStyle'
+import { MessageType } from '@src/types/ChattingType'
 
-const ChattingBody = () => {
+export type ChattingBodyPropsType = {
+  messages: MessageType[]
+}
+
+const ChattingBody = (props: ChattingBodyPropsType) => {
+  const { messages } = props
+  console.log(messages)
   return <c.Container>바디</c.Container>
 }
 

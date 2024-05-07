0e3f4e4b9e36de4b9cbeb30a8cae86b9acb5f283
tlsehdrms124 <tlsehdrms124@naver.com>
2024-05-08 01:37:39 +0900

    [BE] feat: 점포_상권 레포지토리 생성

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/repository/StoreCommercialRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/repository/StoreCommercialRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface StoreCommercialRepository extends JpaRepository<SalesCommercial, Long>, StoreCommercialCustomRepository {
     @Query(value = "SELECT SUM(total_store) FROM store_commercial WHERE commercial_code = :commercialCode AND period_code = :periodCode", nativeQuery = true)
     Long findByCommercialCode(String commercialCode, String periodCode);
+
+    SalesCommercial findByPeriodCodeAndCommercialCodeAndServiceCode(String periodCode, String commercialCode, String serviceCode);
 }

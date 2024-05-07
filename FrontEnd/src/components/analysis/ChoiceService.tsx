343959e8087b582485fd97250a4aa96bd76a57af
이승현 <tmdgus1761@gmail.com>
2024-05-07 14:00:20 +0900

    [FE] feat: 업종 선택 컴포넌트 로직 작성 및 스타일 적용

--- FrontEnd/src/components/analysis/ChoiceService.tsx
+++ FrontEnd/src/components/analysis/ChoiceService.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react'
+import analysisStore from '@src/stores/analysisStore'
+import { SelectedServiceType } from '@src/types/AnalysisType'
+import ChoiceServiceType from '@src/components/analysis/ChoiceServiceType'
+import ChoiceServiceDetail from '@src/components/ChoiceServiceDetail'
 import * as c from '@src/components/styles/analysis/ChoiceServiceStyle'
 
 const ChoiceService = () => {
+  const serviceDataBody = analysisStore(state => state.serviceDataBody)
+  const [selectedType, setSelectedType] = useState('')
+  const [serviceList, setServiceList] = useState<SelectedServiceType[]>([])
+
+  const handleServiceTypeClick = (serviceType: string) => {
+    const filteredList = serviceDataBody.filter(
+      item => item.serviceType === serviceType,
+    )
+    setSelectedType(serviceType)
+    setServiceList(filteredList)
+  }
+
   return (
     <c.Container>
       <c.Title>분석하고 싶은 업종을 선택해주세요.</c.Title>
-      <div>
-        <div>여기가?</div>
-        <div>아마?</div>
-        <div>뭐가 오겠지</div>
-      </div>
+      <ChoiceServiceType
+        handleServiceTypeClick={handleServiceTypeClick}
+        selectedType={selectedType}
+      />
+      <ChoiceServiceDetail serviceList={serviceList} />
     </c.Container>
   )
 }

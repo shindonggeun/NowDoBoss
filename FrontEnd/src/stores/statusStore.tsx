9309098f43f6709d2d77b2babd48f66947c6fb4a
김온유 <gkc04075@gmail.com>
2024-05-07 10:41:52 +0900

    [FE] feat: 요일 목록 저장

---
+++ FrontEnd/src/stores/statusStore.tsx
@@ -0,0 +1,13 @@
+export type WeekDays = {
+  [key: string]: string
+}
+
+export const WeekData: WeekDays = {
+  monday: ' 월요일',
+  tuesday: ' 화요일',
+  wednesday: ' 수요일',
+  thursday: ' 목요일',
+  friday: ' 금요일',
+  saturday: ' 토요일',
+  sunday: ' 일요일',
+}

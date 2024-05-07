ba2b445b30e66f1da4a14fc2cb4d5e9b5a87f534
김온유 <gkc04075@gmail.com>
2024-05-07 11:23:23 +0900

    [FE] feat: 유동인구 상세 그래프 적용 (반응형 수정 필요)

--- FrontEnd/src/components/status/DetailPopulationComponent.tsx
+++ FrontEnd/src/components/status/DetailPopulationComponent.tsx
@@ -5,6 +5,8 @@ import BarChart2 from '@src/common/BarChart2'
 import PieChart from '@src/common/PieChart'
 import Radar2Chart from '@src/common/Radar2Chart'
 import BarChart3 from '@src/common/BarChart3'
+import { WeekData } from '@src/stores/statusStore'
+import ContainerBox from '@src/common/ContainerBox'
 
 interface DetailPopulationProps {
   props: DetailDataBody
@@ -49,70 +51,89 @@ const DetailPopulationComponent = ({ props }: DetailPopulationProps) => {
   const DayDataArray = Object.entries(DayData.data).map(([key, value]) => ({
     [key]: value,
   }))
+  const DayValues = Object.values(DayData)
+  const DayMinValue = Math.min(...DayValues)
 
   return (
-    <>
+    <div>
       {/* <h1>유동인구 페이지</h1> */}
-      <c.AnalysisTitle>분기별 평균 유동인구</c.AnalysisTitle>
-      <c.AnalysisSubTitle>
-        유동인구가 이전분기에 비해
-        <c.AnalysiEemphasis>{PeriodData.summary}</c.AnalysiEemphasis>
-        하고 있습니다.
-      </c.AnalysisSubTitle>
-      <BarChart2
-        labels={labels}
-        values={PeriodDataArray.map(item => Object.values(item)[0])}
-      />
+      <div>
+        <c.AnalysisTitle>분기별 평균 유동인구</c.AnalysisTitle>
+        <c.AnalysisSubTitle>
+          유동인구가 이전분기에 비해
+          <c.AnalysiEemphasis>{PeriodData.summary}</c.AnalysiEemphasis>
+          하고 있습니다.
+        </c.AnalysisSubTitle>
+        <BarChart2
+          labels={labels}
+          values={PeriodDataArray.map(item => Object.values(item)[0])}
+        />
+        <ContainerBox height={30} />
+      </div>
 
-      <c.AnalysisTitle>시간대별 유동인구</c.AnalysisTitle>
-      <c.AnalysisSubTitle>
-        유동인구가 가장 높은 시간대는
-        <c.AnalysiEemphasis>
-          {StartTime} ~ {EndTime}시
-        </c.AnalysiEemphasis>
-        입니다
-      </c.AnalysisSubTitle>
-      <AreaChart
-        labels={['00~06', '06~11', '11~17', '17~21', '21~24']}
-        values={TimeDataArray.map(item => Object.values(item)[0])}
-      />
+      <div>
+        <c.AnalysisTitle>시간대별 유동인구</c.AnalysisTitle>
+        <c.AnalysisSubTitle>
+          유동인구가 가장 높은 시간대는
+          <c.AnalysiEemphasis>
+            {StartTime} ~ {EndTime}시
+          </c.AnalysiEemphasis>
+          입니다
+        </c.AnalysisSubTitle>
+        <AreaChart
+          labels={['00~06', '06~11', '11~17', '17~21', '21~24']}
+          values={TimeDataArray.map(item => Object.values(item)[0])}
+        />
+        <ContainerBox height={30} />
+      </div>
 
-      <c.AnalysisTitle>성별 유동인구</c.AnalysisTitle>
-      <c.AnalysisSubTitle>
-        유동인구가 가장 많은 성별은
-        <c.AnalysiEemphasis>
-          {GenderData.summary === 'female' ? ' 여성' : ' 남성'}
-        </c.AnalysiEemphasis>
-        입니다
-      </c.AnalysisSubTitle>
-      <PieChart
-        labels={['남성', '여성']}
-        value={[GenderData.data.male, GenderData.data.female]}
-      />
+      <c.MixConatiner>
+        <c.MixInnerConatiner>
+          <c.AnalysisTitle>성별 유동인구</c.AnalysisTitle>
+          <c.AnalysisSubTitle>
+            유동인구가 가장 많은 성별은
+            <c.AnalysiEemphasis>
+              {GenderData.summary === 'female' ? ' 여성' : ' 남성'}
+            </c.AnalysiEemphasis>
+            입니다
+          </c.AnalysisSubTitle>
+          <PieChart
+            labels={['남성', '여성']}
+            value={[GenderData.data.male, GenderData.data.female]}
+          />
+          <ContainerBox height={30} />
+        </c.MixInnerConatiner>
 
-      <c.AnalysisTitle>연령별 유동인구</c.AnalysisTitle>
-      <c.AnalysisSubTitle>
-        유동인구가 가장 많은 연령대는
-        <c.AnalysiEemphasis>
-          {AgeData.summary.split('age')}대
-        </c.AnalysiEemphasis>
-        입니다
-      </c.AnalysisSubTitle>
-      <Radar2Chart
-        value={AgeDataArray.map(item => Object.values(item)[0] / 500000)}
-      />
+        <c.MixInnerConatiner>
+          <c.AnalysisTitle>연령별 유동인구</c.AnalysisTitle>
+          <c.AnalysisSubTitle>
+            유동인구가 가장 많은 연령대는
+            <c.AnalysiEemphasis>
+              {AgeData.summary.split('age')}대
+            </c.AnalysiEemphasis>
+            입니다
+          </c.AnalysisSubTitle>
+          <Radar2Chart
+            value={AgeDataArray.map(item => Object.values(item)[0] / 500000)}
+          />
+          <ContainerBox height={30} />
+        </c.MixInnerConatiner>
+      </c.MixConatiner>
 
-      <c.AnalysisTitle>요일별 유동인구</c.AnalysisTitle>
-      <c.AnalysisSubTitle>
-        유동인구가 가장 높은 요일은
-        <c.AnalysiEemphasis>{PeriodData.summary}</c.AnalysiEemphasis>
-        입니다
-      </c.AnalysisSubTitle>
-      <BarChart3
-        labels={labels}
-        values={DayDataArray.map(item => Object.values(item)[0])}
-      />
-    </>
+      <div>
+        <c.AnalysisTitle>요일별 유동인구</c.AnalysisTitle>
+        <c.AnalysisSubTitle>
+          유동인구가 가장 높은 요일은
+          <c.AnalysiEemphasis>{WeekData[DayData.summary]}</c.AnalysiEemphasis>
+          입니다
+        </c.AnalysisSubTitle>
+        <BarChart3
+          labels={['월', '화', '수', '목', '금', '토', '일']}
+          values={DayDataArray.map(item => Object.values(item)[0])}
+          minvalue={DayMinValue}
+        />
+      </div>
+    </div>
   )
 }
 

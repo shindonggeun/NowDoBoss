408c8db464dca5abe6e9bdad2980d29e0e477340
이승현 <tmdgus1761@gmail.com>
2024-05-08 13:05:59 +0900

    [FE] feat: 상권분석 연령 차트 prop 조건으로 변경

--- FrontEnd/src/components/analysis/sales/AgeChart.tsx
+++ FrontEnd/src/components/analysis/sales/AgeChart.tsx
@@ -2,11 +2,15 @@ import RadarChart from '@src/common/RadarChart'
 import * as s from '@src/components/styles/analysis/SalesAnalysisStyle'
 
 const AgeChart = () => {
+  const labels: string[] = ['10대', '20대', '30대', '40대', '50대', '60대 이상']
+  const value1 = [12, 13, 15, 30, 10, 20]
+  const value2 = [5, 20, 10, 15, 15, 35]
+
   return (
     <s.AgeChart>
       <s.ChartTitle>연령별 매출액</s.ChartTitle>
       <s.ChartSubTitle>60대 이상 남성 매출액이 가장 높아요.</s.ChartSubTitle>
-      <RadarChart />
+      <RadarChart labels={labels} value1={value1} value2={value2} />
     </s.AgeChart>
   )
 }

d7ce68d195f1b98325d8b684380f758d97edabdb
이승현 <tmdgus1761@gmail.com>
2024-05-08 10:47:30 +0900

    [FE] feat: 상권 분석 업종 선택 api 함수 작성

--- FrontEnd/src/api/analysisApi.tsx
+++ FrontEnd/src/api/analysisApi.tsx
@@ -1,5 +1,13 @@
 import { customAxios } from '@src/util/auth/customAxios'
 
+// 업종 선택
+export const getServiceData = async (commercialCode: string) => {
+  return customAxios
+    .get(`/commercial/service/${commercialCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
 // 유동인구
 export const getFlowPopulationData = async (commercialCode: string) => {
   return customAxios

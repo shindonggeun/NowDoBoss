b77ecfb28fd39fdffdbf7a84794368b0e1cd68ce
choiseongho <chltjdgh3@naver.com>
2024-05-08 15:02:03 +0900

    [FE] style: 채팅 화면 높이 설정

--- FrontEnd/src/components/styles/chatting/ChattingHeaderStyle.tsx
+++ FrontEnd/src/components/styles/chatting/ChattingHeaderStyle.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 8vh;
 `
 export const Header = styled.div`
   //background-color: #d9d9d9;

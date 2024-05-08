2f02f874a35b59a5dd14e04bd2d2c7517edda326
이승현 <tmdgus1761@gmail.com>
2024-05-08 10:43:11 +0900

    [FE] feat: 상권 분석 최상단 컨테이너로 페이지 내부 변경

--- FrontEnd/src/pages/AnalysisPage.tsx
+++ FrontEnd/src/pages/AnalysisPage.tsx
@@ -1,14 +1,7 @@
-import SelectContainer from '@src/containers/analysis/SelectContainer'
+import AnalysisContainer from '@src/containers/analysis/AnalysisContainer'
 
 const AnalysisPage = () => {
-  return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', overflowY: 'auto' }}
-    >
-      <SelectContainer />
-      {/* <ResultContainer /> */}
-    </div>
-  )
+  return <AnalysisContainer />
 }
 
 export default AnalysisPage

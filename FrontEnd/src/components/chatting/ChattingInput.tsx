7e1c59e2cefb55b376e97d11e04e6f830b663b6e
choiseongho <chltjdgh3@naver.com>
2024-05-08 15:01:22 +0900

    [FE] feat: 채팅 input 로직 수정

--- FrontEnd/src/components/chatting/ChattingInput.tsx
+++ FrontEnd/src/components/chatting/ChattingInput.tsx
@@ -5,6 +5,7 @@ import send_message from '@src/assets/send_message.svg'
 const ChattingInput = ({ onSend }: { onSend: (content: string) => void }) => {
   const [contentValue, setContentValue] = useState<string>('')
 
+  // 내용 제출 함수
   const handleSend = () => {
     if (contentValue.trim()) {
       onSend(contentValue)
@@ -12,15 +13,26 @@ const ChattingInput = ({ onSend }: { onSend: (content: string) => void }) => {
     }
   }
 
+  // textarea에서 enter 누르면 전송되는 로직
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault() // 기본 동작 방지
+      handleSend() // 제출 로직 실행
+    }
+  }
+
   return (
     <c.Container>
-      <c.Input
+      <c.Textarea
+        as="textarea"
         placeholder="내용을 입력해주세요."
-        defaultValue={contentValue}
+        value={contentValue}
         maxLength={499}
         onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
           setContentValue(e.target.value)
         }}
+        onKeyDown={handleKeyDown}
+        rows={5} // 높이를 설정해서 줄바꿈 가능하도록 설정
       />
       <c.SubmitButton $isActive={contentValue !== ''} onClick={handleSend}>
         <c.SubmitImg src={send_message} />

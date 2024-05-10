655bf0e863797407d9a583cc75e794e696813f79
이승현 <tmdgus1761@gmail.com>
2024-05-11 00:56:29 +0900

    [FE] feat: 프로필페이지 사이드 탭바 기본 경로 컴포넌트 작성

---
+++ FrontEnd/src/components/profile/SideBar.tsx
@@ -0,0 +1,15 @@
+import * as s from '@src/components/styles/profile/SideBarStyle'
+
+const SideBar = () => {
+  return (
+    <s.Container>
+      <div>북마크</div>
+      <div>개인 정보 설정</div>
+      <div>비밀번호 변경</div>
+      <div>로그아웃</div>
+      <div>회원탈퇴</div>
+    </s.Container>
+  )
+}
+
+export default SideBar
---
+++ FrontEnd/src/components/styles/profile/SideBarStyle.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin-top: 100px;
+  background-color: #cccccc;
+
+  @media (max-width: 576px) {
+    margin-top: 50px;
+  }
+`

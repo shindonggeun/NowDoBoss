01c6d3efc2e4650a26f8be83b1bbf5fedc4ca4b6
이승현 <tmdgus1761@gmail.com>
2024-05-11 00:52:13 +0900

    [FE] feat: 프로필페이지 중첩라우팅 작성

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -5,7 +5,10 @@ import MainPage from '@src/pages/MainPage'
 import SignUpPage from '@src/pages/SignUpPage'
 import LoginPage from '@src/pages/LoginPage'
 import SocialLoadingPage from '@src/pages/SocialLoadingPage'
-import MyPage from '@src/pages/MyPage'
+import ProfilePage from '@src/pages/ProfilePage'
+import EditProfilePage from '@src/pages/EditProfilePage'
+import WithdrawPage from '@src/pages/WithdrawPage'
+import BookmarksPage from '@src/pages/BookmarksPage'
 import CommunityPage from '@src/pages/CommunityPage'
 import CommunityRegisterPage from '@src/pages/CommunityRegisterPage'
 import CommunityDetailPage from '@src/pages/CommunityDetailPage'
@@ -34,13 +37,19 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
+          {/* 회원 */}
           <Route path="/register" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route
             path="/member/loading/:provider"
             element={<SocialLoadingPage />}
           />
-          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/profile/*" element={<ProfilePage />}>
+            <Route path="edit" element={<EditProfilePage />} />
+            <Route path="withdraw" element={<WithdrawPage />} />
+            <Route path="bookmarks" element={<BookmarksPage />} />
+          </Route>
+          {/* 상권 */}
           <Route path="/status" element={<StatusPage />} />
           <Route path="/analysis" element={<AnalysisPage />}>
             <Route path="simulation" element={<SimulationPage />} />
@@ -52,6 +61,7 @@ function App() {
           <Route path="/recommend" element={<RecommendPage />} />
           <Route path="/simulation" element={<SimulationPage />} />
           <Route path="/simulation/report" element={<SimulationReportPage />} />
+          {/* 커뮤니티 */}
           <Route path="/community/*" element={<CommunityPage />}>
             <Route path="list" element={<CommunityListPage />} />
             <Route path="register" element={<CommunityRegisterPage />} />

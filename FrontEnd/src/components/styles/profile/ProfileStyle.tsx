17fdfb2919346b8b4de911e64cdde26d3cb7cf76
이승현 <tmdgus1761@gmail.com>
2024-05-11 00:58:13 +0900

    [FE] feat: 프로필페이지 프로필 기본 경로 컴포넌트 작성 및 스타일 추가

---
+++ FrontEnd/src/components/styles/profile/ProfileStyle.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  height: auto;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ImgDiv = styled.div`
+  width: 20%;
+`
+export const Img = styled.img`
+  width: 70%;
+  height: auto;
+`
+
+export const InfoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`
+
+export const EmailDiv = styled.div`
+  display: flex;
+`
+
+export const Name = styled.div`
+  font-size: 1rem;
+  font-weight: 500;
+`
+
+export const Email = styled.div`
+  font-size: 0.8rem;
+  font-weight: 500;
+`
+
+export const Provider = styled.img``

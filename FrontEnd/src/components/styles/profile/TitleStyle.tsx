8d50c1c640b82bc27fa8909a36b958d46e2127da
이승현 <tmdgus1761@gmail.com>
2024-05-12 03:23:53 +0900

    [FE] style: 프로필 재사용 Title 컴포넌트 style 수정
    Former-commit-id: 4b2719f5ac982e52a19f19e5874cb46eeeaa5b12

--- FrontEnd/src/components/styles/profile/TitleStyle.tsx
+++ FrontEnd/src/components/styles/profile/TitleStyle.tsx
@@ -4,9 +4,4 @@ export const Container = styled.div`
   width: 100%;
   font-weight: 600;
   font-size: 22px;
-  margin-bottom: 50px;
-
-  @media (max-width: 768px) {
-    margin-bottom: 30px;
-  }
 `

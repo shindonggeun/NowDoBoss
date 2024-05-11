94dd93ee0308f484892f7ef7cfc8acafba81e587
이승현 <tmdgus1761@gmail.com>
2024-05-11 12:31:43 +0900

    [FE] feat: 프로필페이지 사이드 탭바 컴포넌트 style 적용

--- FrontEnd/src/components/profile/SideBar.tsx
+++ FrontEnd/src/components/profile/SideBar.tsx
@@ -3,11 +3,12 @@ import * as s from '@src/components/styles/profile/SideBarStyle'
 const SideBar = () => {
   return (
     <s.Container>
-      <div>북마크</div>
-      <div>개인 정보 설정</div>
-      <div>비밀번호 변경</div>
-      <div>로그아웃</div>
-      <div>회원탈퇴</div>
+      <s.MenuItem>북마크</s.MenuItem>
+      <s.Divider />
+      <s.MenuItem>개인 정보 설정</s.MenuItem>
+      <s.MenuItem>비밀번호 변경</s.MenuItem>
+      <s.MenuItem>로그아웃</s.MenuItem>
+      <s.MenuItem>회원탈퇴</s.MenuItem>
     </s.Container>
   )
 }
--- FrontEnd/src/components/styles/profile/SideBarStyle.tsx
+++ FrontEnd/src/components/styles/profile/SideBarStyle.tsx
@@ -1,10 +1,34 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  margin-top: 100px;
-  background-color: #cccccc;
+  display: flex;
+  flex-direction: column;
+  margin-top: 70px;
+  padding: 10px;
+  height: auto;
 
-  @media (max-width: 576px) {
+  @media (max-width: 768px) {
     margin-top: 50px;
   }
+`
+
+export const MenuItem = styled.div`
+  padding: 10px 0;
+  font-size: 1rem;
+  color: #333;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e7e7e7;
+  }
+
+  &:not(:first-child) {
+    margin-top: 5px; // 첫 번째 항목을 제외하고 상단 마진 추가
+  }
+`
+
+export const Divider = styled.div`
+  height: 1px;
+  background-color: #c2c2c2;
+  margin: 10px 0;
 `

d78e349d67ed00d7b39850ba529ac63494f47f7c
이승현 <tmdgus1761@gmail.com>
2024-05-12 04:55:33 +0900

    [FE] feat: 프로필 이미지 Firebase Storage 업로드 API 함수 생성
    Former-commit-id: dd7763f34184716b76f2bec208aa33cf69eba089

--- FrontEnd/src/api/profileApi.tsx
+++ FrontEnd/src/api/profileApi.tsx
@@ -1,9 +1,21 @@
 import { customAxios } from '@src/util/auth/customAxios'
 
-// 업종 선택
+// 회원정보 가져오기
 export const getMemberInfoData = async () => {
   return customAxios
     .get(`/member/get`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }
+
+// 프로필 이미지 업로드
+export const UploadProfileImage = async (data: FormData) => {
+  return customAxios
+    .post(`/firebase/upload`, data, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+//

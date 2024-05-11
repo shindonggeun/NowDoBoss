35a2b0e8a2c32838b72d589ea769ee9cf1f17b3b
이승현 <tmdgus1761@gmail.com>
2024-05-11 12:31:02 +0900

    [FE] feat: 프로필페이지 기본정보 컴포넌트 provider 추가

--- FrontEnd/src/components/profile/Profile.tsx
+++ FrontEnd/src/components/profile/Profile.tsx
@@ -3,14 +3,12 @@ import * as p from '@src/components/styles/profile/ProfileStyle'
 const Profile = () => {
   return (
     <p.Container>
-      <p.ImgDiv>
-        <p.Img src="/images/profile.png" alt="profile" />
-      </p.ImgDiv>
+      <p.Img src="/images/profile.png" alt="profile" />
       <p.InfoDiv>
         <p.Name>이승현</p.Name>
         <p.EmailDiv>
-          <p.Email>lsh1751@naver.com</p.Email>
-          <div>카</div>
+          <p.Email>tmdgus1761@gmail.com</p.Email>
+          <p.Provider src="src/assets/KakaoBtnSmall.png" alt="provider" />
         </p.EmailDiv>
       </p.InfoDiv>
     </p.Container>

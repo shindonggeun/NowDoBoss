e7fdb8bdb5aa940f5e8619c7fab68b40bb796044
이승현 <tmdgus1761@gmail.com>
2024-05-12 00:39:41 +0900

    [FE] feat: 프로필 컴포넌트 prop type 추가
    Former-commit-id: 81c0f9c996018583cd11caf8897d3249a7a5924d

---
+++ FrontEnd/src/types/ProfileType.ts
@@ -0,0 +1,9 @@
+import { MemberInfoType } from '@src/types/UserType'
+
+export type TitlePropsType = {
+  title: string
+}
+
+export type ProfilePropsType = {
+  MemberInfoData: MemberInfoType
+}
--- FrontEnd/src/types/ProfileType.tsx
+++
@@ -1,3 +0,0 @@
-export type TitlePropsType = {
-  title: string
-}

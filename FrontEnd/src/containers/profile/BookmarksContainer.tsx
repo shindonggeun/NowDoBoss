d1a1a37d5e360ee2f2bcb415aea9162754dc36b4
이승현 <tmdgus1761@gmail.com>
2024-05-12 03:26:28 +0900

    [FE] feat: 북마크 컨테이너 TabBar props 데이터 추가
    Former-commit-id: bbe1c124c43092451f2ab423ec181afc2d26ef31

--- FrontEnd/src/containers/profile/BookmarksContainer.tsx
+++ FrontEnd/src/containers/profile/BookmarksContainer.tsx
@@ -2,10 +2,17 @@ import Title from '@src/components/profile/Title'
 import TabBar from '@src/components/profile/TabBar'
 
 const BookmarksContainer = () => {
+  const tabs = [
+    { label: '전체', path: '/profile/bookmarks' },
+    { label: '상권분석', path: '/profile/bookmarks' },
+    { label: '상권추천', path: '/profile/bookmarks' },
+    { label: '창업시뮬레이션', path: '/profile/bookmarks' },
+  ]
+
   return (
     <div>
       <Title title="북마크" />
-      <TabBar />
+      <TabBar tabs={tabs} />
     </div>
   )
 }

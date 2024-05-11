8d0f0c020699fbf930e57d11e88a88f1c2e83652
이승현 <tmdgus1761@gmail.com>
2024-05-11 13:04:50 +0900

    [FE] feat: 프로필 사이드 탭 클릭 시 페이지 전환 및 클릭 스타일 추가

--- FrontEnd/src/components/profile/SideBar.tsx
+++ FrontEnd/src/components/profile/SideBar.tsx
@@ -1,14 +1,51 @@
+import { useNavigate, useLocation } from 'react-router-dom'
 import * as s from '@src/components/styles/profile/SideBarStyle'
 
 const SideBar = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  // 현재 경로와 메뉴 아이템의 경로를 비교하여 스타일을 결정하는 함수
+  const isActive = (path: string) => {
+    return location.pathname.includes(path)
+  }
+
   return (
     <s.Container>
-      <s.MenuItem>북마크</s.MenuItem>
+      <s.MenuItem
+        onClick={() => navigate('/profile/bookmarks')}
+        style={{
+          fontWeight: isActive('/profile/bookmarks') ? '600' : 'normal',
+        }}
+      >
+        북마크
+      </s.MenuItem>
       <s.Divider />
-      <s.MenuItem>개인 정보 설정</s.MenuItem>
-      <s.MenuItem>비밀번호 변경</s.MenuItem>
+      <s.MenuItem
+        onClick={() => navigate('/profile/edit')}
+        style={{
+          fontWeight: isActive('/profile/edit') ? '600' : 'normal',
+        }}
+      >
+        개인 정보 설정
+      </s.MenuItem>
+      <s.MenuItem
+        onClick={() => navigate('/profile/change-password')}
+        style={{
+          fontWeight: isActive('/profile/change-password') ? '600' : 'normal',
+        }}
+      >
+        비밀번호 변경
+      </s.MenuItem>
       <s.MenuItem>로그아웃</s.MenuItem>
-      <s.MenuItem>회원탈퇴</s.MenuItem>
+      <s.MenuItem
+        onClick={() => navigate('/profile/withdraw')}
+        style={{
+          fontWeight: isActive('/profile/withdraw') ? '600' : 'normal',
+        }}
+      >
+        회원탈퇴
+      </s.MenuItem>
     </s.Container>
   )
 }

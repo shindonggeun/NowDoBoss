73dff341e180a580f35df0055cad07a76b9fc127
이승현 <tmdgus1761@gmail.com>
2024-05-12 22:39:25 +0900

    [FE] fix: 재사용 TabBar 컴포넌트 현재 경로 감지 및 activeTab 업데이트 로직 추가
    Former-commit-id: b64d83a9b2562b7023b4ee9af0c0cbd172f80681

--- FrontEnd/src/components/profile/TabBar.tsx
+++ FrontEnd/src/components/profile/TabBar.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { TabBarPropsType } from '@src/types/ProfileType'
 import * as t from '@src/components/styles/profile/TabBarStyle'
 
 const TabBar = (props: TabBarPropsType) => {
   const { tabs } = props
   const navigate = useNavigate()
+  const location = useLocation()
   const [activeTab, setActiveTab] = useState(tabs[0].label)
+
+  // 경로 변경 감지 및 activeTab 업데이트
+  useEffect(() => {
+    const currentTab = tabs.find(tab => tab.path === location.pathname)
+    if (currentTab) {
+      setActiveTab(currentTab.label)
+    }
+  }, [location, tabs])
 
   const handleTabClick = (path: string) => {
     navigate(path) // 지정된 경로로 이동

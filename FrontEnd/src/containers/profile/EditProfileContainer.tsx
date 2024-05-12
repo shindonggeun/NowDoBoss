372bc404dfadf2fc08761a194e7c6fdfda98a12d
이승현 <tmdgus1761@gmail.com>
2024-05-12 21:13:40 +0900

    [FE] feat: 프로필 업데이트 컨테이너 API 연결 및 프로필 로컬스토리지에 업데이트
    Former-commit-id: 1d8b1e0c217d2891178cfa635229fac351226ff3

--- FrontEnd/src/containers/profile/EditProfileContainer.tsx
+++ FrontEnd/src/containers/profile/EditProfileContainer.tsx
@@ -1,15 +1,64 @@
-import React, { useRef, useState } from 'react'
-import userStore from '@src/stores/userStore'
-import { UploadProfileImage } from '@src/api/profileApi'
+import React, { useEffect, useRef, useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { UpdateMemberInfo, UploadProfileImage } from '@src/api/profileApi'
 import * as e from '@src/containers/profile/EditProfileContainerStyle'
 
 const EditProfileContainer = () => {
-  const memberInfo = userStore(state => state.memberInfo)
+  const queryClient = useQueryClient() // 캐싱 무효화를 위해 QueryClient 인스턴스를 가져옵니다.
+  const memberInfo = JSON.parse(localStorage.getItem('memberInfo') as string)
+  const [nickname, setNickname] = useState(memberInfo.nickname)
   const [profileImage, setProfileImage] = useState(
     memberInfo.profileImage || '/images/profile.png',
   )
   const fileInputRef = useRef<HTMLInputElement | null>(null)
 
+  const getProviderImage = (provider: string) => {
+    switch (provider) {
+      case 'GOOGLE':
+        return '/images/GoogleBtnSmall.png'
+      case 'KAKAO':
+        return '/images/KakaoBtnSmall.png'
+      case 'NAVER':
+        return '/images/NaverBtnSmall.png'
+      default:
+        return '' // provider가 없거나 다른 경우 기본 이미지 혹은 빈 문자열
+    }
+  }
+
+  // 프로필 이미지 업로드 API 호출
+  const { mutate: uploadProfileImage } = useMutation({
+    mutationKey: ['uploadProfileImage'],
+    mutationFn: UploadProfileImage,
+    onSuccess: res => {
+      if (res.dataHeader.successCode === 0) {
+        setProfileImage(res.dataBody)
+      }
+    },
+  })
+
+  // 프로필 업데이트 API 호출
+  const { mutate: updateMemberInfo } = useMutation({
+    mutationKey: ['updateMemberInfo'],
+    mutationFn: UpdateMemberInfo,
+    onSuccess: res => {
+      if (res.dataHeader.successCode === 0) {
+        // 캐싱 무효화하고 프로필 가져오기 API 재호출
+        queryClient.invalidateQueries({ queryKey: ['GetMemberInfoData'] })
+
+        // Local Storage 와 상태를 새로운 데이터로 업데이트
+        const updatedMemberInfo = { ...memberInfo, nickname, profileImage }
+        localStorage.setItem('memberInfo', JSON.stringify(updatedMemberInfo))
+
+        // 상태 업데이트로 컴포넌트에 반영
+        setNickname(updatedMemberInfo.nickname)
+        setProfileImage(updatedMemberInfo.profileImage)
+      } else {
+        alert(res.dataHeader.resultMessage || '업데이트 실패')
+      }
+    },
+  })
+
+  // 프로필 이미지가 바뀌었을때 함수
   const handleProfileImageChange = async (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
@@ -20,44 +69,77 @@ const EditProfileContainer = () => {
       formData.append('file', file)
       formData.append('fileName', file.name)
 
-      try {
-        const response = await UploadProfileImage(formData)
-        // 업로드된 이미지 URL로 상태 업데이트
-        setProfileImage(response.dataBody)
-        // 여기서 프로필 수정하는 데이터에 업로드된 이미지 URL 추가
-        // 예를 들어, 프로필 업데이트 함수 호출 등
-      } catch (error) {
-        console.error('프로필 이미지 업로드 실패:', error)
-      }
+      uploadProfileImage(formData)
     }
   }
 
+  useEffect(() => {
+    // profileImage가 초기 이미지가 아니고, 실제로 변경되었을 경우에만 실행
+    if (profileImage !== '/images/profile.png') {
+      const data = {
+        nickname,
+        profileImage,
+      }
+
+      updateMemberInfo(data) // profileImage가 변경될 때마다 회원 정보 업데이트 실행
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [profileImage])
+
+  // 프로필 업데이트 핸들러
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    console.log(profileImage)
-    // 여기서 프로필 수정 API 호출 가능
-    alert('프로필이 업데이트되었습니다!')
+    const data = {
+      nickname,
+      profileImage,
+    }
+
+    updateMemberInfo(data)
   }
 
+  // 프로필 이미지를 클릭했을떄 ref
   const handleClickImage = () => {
     fileInputRef.current?.click()
+  }
+
+  // 닉네임 변경 핸들러
+  const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(event.target.value)
   }
 
   return (
     <div>
       <e.Form onSubmit={handleSubmit}>
-        <div style={{ padding: '20px', textAlign: 'center' }}>
-          <input
-            type="file"
-            ref={fileInputRef}
-            onChange={handleProfileImageChange}
-            style={{ display: 'none' }}
-          />
-          <e.ProfileImageContainer
-            onClick={handleClickImage}
-            image={profileImage}
-          />
-        </div>
+        <e.InfoWrap>
+          <e.ProfileImageContainer>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleProfileImageChange}
+              style={{ display: 'none' }}
+            />
+            <e.ProfileImage onClick={handleClickImage} image={profileImage} />
+          </e.ProfileImageContainer>
+          <e.InfoDiv>
+            <e.Name>{memberInfo.nickname}</e.Name>
+            <e.EmailDiv>
+              <e.Email>{memberInfo.email}</e.Email>
+              {memberInfo.provider && (
+                <e.Provider
+                  src={getProviderImage(memberInfo.provider)}
+                  alt="provider"
+                />
+              )}
+            </e.EmailDiv>
+          </e.InfoDiv>
+        </e.InfoWrap>
+        <e.UpdateWrap>
+          <e.TitleWrap>
+            <e.UpdateTitle>닉네임 변경</e.UpdateTitle>
+            <e.NameInput value={nickname} onChange={handleNicknameChange} />
+          </e.TitleWrap>
+          <e.SubmitButton>수정</e.SubmitButton>
+        </e.UpdateWrap>
       </e.Form>
     </div>
   )

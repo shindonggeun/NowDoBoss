439b594943a457a5f534051452590d75fd61d2a3
이승현 <tmdgus1761@gmail.com>
2024-05-12 22:37:12 +0900

    [FE] feat: 비밀번호 변경 API 함수 생성
    Former-commit-id: 5a7b9b202f9492ceab12505796102d6d9441f93e

--- FrontEnd/src/api/profileApi.tsx
+++ FrontEnd/src/api/profileApi.tsx
@@ -1,5 +1,8 @@
 import { customAxios } from '@src/util/auth/customAxios'
-import { UpdateMemberInfoDataType } from '@src/types/ProfileType'
+import {
+  ChangeMemberPasswordDataType,
+  UpdateMemberInfoDataType,
+} from '@src/types/ProfileType'
 
 // 회원정보 가져오기
 export const getMemberInfoData = async () => {
@@ -23,6 +26,16 @@ export const UploadProfileImage = async (data: FormData) => {
 export const UpdateMemberInfo = async (data: UpdateMemberInfoDataType) => {
   return customAxios
     .patch(`/member/update`, data)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 비밀번호 변경
+export const changeMemberPassword = async (
+  data: ChangeMemberPasswordDataType,
+) => {
+  return customAxios
+    .patch(`/member/password/change`, data)
     .then(res => res.data)
     .catch(err => console.log(err))
 }

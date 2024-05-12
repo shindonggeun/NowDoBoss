9093bba4e60148136030bbdb5363496c79cb9a93
choiseongho <chltjdgh3@naver.com>
2024-05-12 18:45:33 +0900

    [FE] fix: 채팅 컨테이너 스타일 수정
    Former-commit-id: 08c2d3f8a6f55bb2cbd494831caf991934fa5d7b

--- FrontEnd/src/containers/chatting/ChattingContainerStyle.tsx
+++ FrontEnd/src/containers/chatting/ChattingContainerStyle.tsx
@@ -1,3 +1,25 @@
 import styled from 'styled-components'
 
+export const SideBar = styled.div`
+  position: fixed;
+  z-index: 10;
+  width: 250px;
+
+  @media only screen and (max-width: 992px) {
+    position: static; // 화면 너비가 992px 이하일 때는 고정 위치 해제
+    width: 100vw;
+  }
+`
+
+export const Context = styled.div`
+  width: 100%;
+`
+
+// detail style
+
+export const MainContentDiv = styled.div`
+  margin: 3% 0 3% 5%;
+  width: 80%;
+`
+
 export const Div = styled.div``

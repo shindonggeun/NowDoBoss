ce11d7e4ef5b6f7a3ca1ef01916a6a853425196c
choiseongho <chltjdgh3@naver.com>
2024-05-14 00:36:25 +0900

    [FE] feat: 회원 탈퇴 시 토큰 제거 api 연결
    Former-commit-id: 364d761f1b52d8f86ba111ec3b2b9d4c59376a27

--- FrontEnd/src/api/fcmApi.tsx
+++ FrontEnd/src/api/fcmApi.tsx
@@ -7,3 +7,11 @@ export const saveFcmToken = async (deviceToken: string) => {
     .then(res => res.data)
     .catch(err => console.log(err))
 }
+
+// fcm 토큰 삭제 delete api
+export const deleteFcmToken = async (deviceToken: string) => {
+  return customAxios
+    .delete(`/firebase/message/${deviceToken}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}

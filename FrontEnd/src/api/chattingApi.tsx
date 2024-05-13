d484e81c9127df65c103efdb3ce145c92d5d932e
choiseongho <chltjdgh3@naver.com>
2024-05-13 12:10:01 +0900

    [FE] fix: 내 채팅방 목록 조회 api 수정
    Former-commit-id: 9693b9f18847bc93fbbca3803edc98d9d13a370a

--- FrontEnd/src/api/chattingApi.tsx
+++ FrontEnd/src/api/chattingApi.tsx
@@ -28,7 +28,7 @@ export const enterChatRoom = async (chatRoomId: number) => {
 // navbar에 띄울 채팅방 목록 조회 get api
 export const fetchMyRooms = async (searchContent: string) => {
   return customAxios
-    .get(`/chat-rooms/my-rooms?${searchContent}`)
+    .get(`/chat-rooms/my-rooms?keyword=${searchContent}`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }

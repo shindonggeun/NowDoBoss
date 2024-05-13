000c510570a4c0d465b91ba8273b7891d868cce8
김온유 <gkc04075@gmail.com>
2024-05-13 11:08:17 +0900

    [FE] feat: 시뮬레이션 저장 연결
    Former-commit-id: 6c6ba48cf969114059c5e99660bffde34fcc2cfa

--- FrontEnd/src/components/simulation/report/ReportHeader.tsx
+++ FrontEnd/src/components/simulation/report/ReportHeader.tsx
@@ -5,15 +5,53 @@ import SaveIcon from '@src/assets/saveMark.svg'
 import CompareIcon from '@src/assets/compare.svg'
 import Xmark from '@src/assets/xmark_solid_nomal.svg'
 import { useState } from 'react'
+import useSimulationStore from '@src/stores/simulationStore'
+import useReportStore from '@src/stores/reportStore'
+import { useMutation } from '@tanstack/react-query'
+import { reportSave } from '@src/api/simulationApi'
+import { SimulationSaveType } from '@src/types/SimulationType'
 
 interface HeaderType {
   onClose: () => void
+  totalPrice: number
 }
-const ReportHeader = ({ onClose }: HeaderType) => {
+const ReportHeader = ({ onClose, totalPrice }: HeaderType) => {
+  const {
+    isFranchise,
+    brandName,
+    subCategoryName,
+    subCategoryCode,
+    bulidingSize,
+    floor,
+  } = useSimulationStore()
+  const { sigungu } = useReportStore()
   const [isSaved, setIsSaved] = useState<boolean>(false)
+
+  // 레포트 분석 저장
+  const { mutate: mutateSaveReport } = useMutation({
+    mutationFn: reportSave,
+    onSuccess: () => {
+      // console.log('저장 완료!')
+    },
+    onError: error => {
+      console.error(error)
+    },
+  })
 
   const onClickSave = () => {
     setIsSaved(!isSaved)
+    const saveReportData: SimulationSaveType = {
+      totalPrice,
+      isFranchisee: isFranchise!,
+      brandName,
+      gugun: sigungu,
+      serviceCode: subCategoryCode,
+      serviceCodeName: subCategoryName,
+      storeSize: bulidingSize,
+      floor,
+    }
+    console.log(saveReportData)
+    mutateSaveReport(saveReportData)
   }
 
   return (

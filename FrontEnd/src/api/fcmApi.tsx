ba628d86a225a1ef6f7b28ce7b7a68086b74bb06
choiseongho <chltjdgh3@naver.com>
2024-05-14 01:47:31 +0900

    [FE] feat: fcm api 작성
    Former-commit-id: 795bfe78ae623feaf756c76d56c1b9a999d4d914

--- FrontEnd/src/api/fcmApi.tsx
+++ FrontEnd/src/api/fcmApi.tsx
@@ -1,4 +1,9 @@
 import { customAxios } from '@src/util/auth/customAxios'
+import {
+  sendTokenDataType,
+  sendTopicDataType,
+  subscribeTopicDataType,
+} from '@src/types/FcmType'
 
 // fcm 토큰 저장 post api
 export const saveFcmToken = async (deviceToken: string) => {
@@ -12,6 +17,32 @@ export const saveFcmToken = async (deviceToken: string) => {
 export const deleteFcmToken = async (deviceToken: string) => {
   return customAxios
     .delete(`/firebase/message/${deviceToken}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 채팅방 생성 및 입장 시 topic 구독 post api
+export const subscribeTopic = async (
+  subscribeTopicData: subscribeTopicDataType,
+) => {
+  return customAxios
+    .post(`/firebase/message/subscribe`, subscribeTopicData)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 채팅방에서 대화 시 내용 topic에 쏘는 post api
+export const sendTopic = async (sendTopicData: sendTopicDataType) => {
+  return customAxios
+    .post(`/firebase/message/topic`, sendTopicData)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// token에 쏘아 1대1 알림 post api
+export const sendToken = async (sendTokenData: sendTokenDataType) => {
+  return customAxios
+    .post(`/firebase/message/token`, sendTokenData)
     .then(res => res.data)
     .catch(err => console.log(err))
 }

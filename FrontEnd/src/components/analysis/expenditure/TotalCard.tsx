9d70e1d21a82f95eed01b7f4d777c2141d1c45a1
이승현 <tmdgus1761@gmail.com>
2024-05-13 19:38:25 +0900

    [FE] feat: 총 지출금액 컴포넌트 로직 작성
    Former-commit-id: 1ee98d3217cf5d25b9c97a2bf80cecc9444944a7

--- FrontEnd/src/components/analysis/expenditure/TotalCard.tsx
+++ FrontEnd/src/components/analysis/expenditure/TotalCard.tsx
@@ -1,29 +1,51 @@
-import AreaChart from '@src/common/AreaChart'
 import analysisStore from '@src/stores/analysisStore'
+import { TotalExpenditureErrPropsType } from '@src/types/AnalysisType'
+import BarChartCompare2 from '@src/common/BarChartCompare2'
 import * as e from '@src/components/styles/analysis/ExpenditureAnalysisStyle'
 
-const TotalCard = () => {
-  const expenditureDataBody = analysisStore(state => state.expenditureDataBody)
-  const { annualQuarterIncomeInfos } = expenditureDataBody
+const TotalCard = (props: TotalExpenditureErrPropsType) => {
+  const { totalExpenditureErr } = props
+  const totalExpenditureDataBody = analysisStore(
+    state => state.totalExpenditureDataBody,
+  )
+  const {
+    districtTotalIncomeInfo,
+    administrationTotalIncomeInfo,
+    commercialTotalIncomeInfo,
+  } = totalExpenditureDataBody
 
-  const periodCodes = annualQuarterIncomeInfos.map(info => info.periodCode)
-  const values = annualQuarterIncomeInfos.map(info => info.totalPrice)
+  const labels: string[] = [
+    districtTotalIncomeInfo.districtCodeName,
+    administrationTotalIncomeInfo.administrationCodeName,
+    commercialTotalIncomeInfo.commercialCodeName,
+  ]
 
-  const labels = periodCodes.map(code => {
-    // 연도와 분기를 추출
-    const year = code.substring(0, 4)
-    const quarter = code.substring(4)
+  const values = [
+    districtTotalIncomeInfo.totalPrice,
+    administrationTotalIncomeInfo.totalPrice,
+    commercialTotalIncomeInfo.totalPrice,
+  ]
 
-    // '2023-1' 형식의 문자열로 변환
-    return `${year}-${quarter}`
-  })
+  const minValue = Math.min(...values)
 
   return (
     <e.TotalCard>
       <e.ChartTitle>총 지출금액</e.ChartTitle>
-      <e.ChartSubTitle>전분기 대비 지출금액이 00% 상승했어요.</e.ChartSubTitle>
-      <e.AddBox>전년 동분기 대비 지출금액은 00% 하락했어요.</e.AddBox>
-      <AreaChart labels={labels} values={values} />
+      {totalExpenditureErr ? (
+        <div>{totalExpenditureErr}</div>
+      ) : (
+        <>
+          <e.ChartSubTitle>자치구의 몇 %입니다.</e.ChartSubTitle>
+          <e.AddBox>행정동의 몇 %입니다.</e.AddBox>
+          <BarChartCompare2
+            labels={labels}
+            values={values}
+            datasetsLabel="지출금액(원)"
+            minvalue={minValue}
+            pluginUnit="원"
+          />
+        </>
+      )}
     </e.TotalCard>
   )
 }

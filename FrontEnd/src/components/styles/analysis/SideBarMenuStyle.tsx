9641a2b26bad7958020b2e9fda977f801583a327
이승현 <tmdgus1761@gmail.com>
2024-05-15 04:21:05 +0900

    [FE] feat: 상권분석 사이드바 생성 및 스타일 수정
    Former-commit-id: 1dad267220fffc175e060b98e9f5e5cfc3eca38d

--- FrontEnd/src/components/styles/analysis/SideBarMenuStyle.tsx
+++ FrontEnd/src/components/styles/analysis/SideBarMenuStyle.tsx
@@ -1,27 +1,31 @@
 import styled from 'styled-components'
 
-export const SidebarContainer = styled.div`
-  width: 85%;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-  background-color: #ffffff;
-  border: 1px solid #ddd;
-  padding: 20px;
-  box-sizing: border-box;
-  transition: transform 0.3s ease;
+export const Container = styled.div`
+  position: fixed;
+  left: 10%;
+  top: 25%;
+  z-index: 1;
+`
 
-  &:hover {
-    transform: translateY(-5px);
-  }
+export const Top = styled.button`
+  padding: 20px 50px;
+  background-color: #fff;
+  border: 1px solid rgb(202, 190, 169);
+  border-radius: 10px;
+  outline: none;
+  cursor: pointer;
 `
 
 export const MenuItem = styled.div`
-    font-weight: 500;
-    padding: 5px 0;
-    border-radius: 5px;
-    transition: background-color 0.3s ease;
+  color: #333;
+  font-weight: 400;
+  font-size: 1rem;
+  padding: 5px 0;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
 
-    &:hover {
-        background-color: #f0f0f0;
-        
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+    color: #2a65f0;
+  }
 `

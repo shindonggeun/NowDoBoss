57437302b4252155efdf832780e8974497a21ef7
김온유 <gkc04075@gmail.com>
2024-05-14 16:35:24 +0900

    [FE] fix: global style boxSize 주석처리
    Former-commit-id: bce5909b317024100db3ae371ec3d36ce0534025

--- FrontEnd/src/GlobalStyles.tsx
+++ FrontEnd/src/GlobalStyles.tsx
@@ -6,7 +6,7 @@ const GlobalStyles = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
+        //box-sizing: border-box;
     }
     
     @font-face {

3aa235fa5e953820e9bbf199b92748f354c49952
이승현 <tmdgus1761@gmail.com>
2024-05-14 09:41:28 +0900

    [FE] feat: 상권분석 지출내역 데이터 없는 경우 에러메세지 props 제거
    Former-commit-id: 104f564460d4eb1cd5656f16bf9e6a88878e3b39

--- FrontEnd/src/components/analysis/expenditure/TotalCard.tsx
+++ FrontEnd/src/components/analysis/expenditure/TotalCard.tsx
@@ -1,10 +1,8 @@
 import analysisStore from '@src/stores/analysisStore'
-import { TotalExpenditureErrPropsType } from '@src/types/AnalysisType'
 import BarChartCompare2 from '@src/common/BarChartCompare2'
 import * as e from '@src/components/styles/analysis/ExpenditureAnalysisStyle'
 
-const TotalCard = (props: TotalExpenditureErrPropsType) => {
-  const { totalExpenditureErr } = props
+const TotalCard = () => {
   const totalExpenditureDataBody = analysisStore(
     state => state.totalExpenditureDataBody,
   )
@@ -31,21 +29,15 @@ const TotalCard = (props: TotalExpenditureErrPropsType) => {
   return (
     <e.TotalCard>
       <e.ChartTitle>총 지출금액</e.ChartTitle>
-      {totalExpenditureErr ? (
-        <div>{totalExpenditureErr}</div>
-      ) : (
-        <>
-          <e.ChartSubTitle>자치구의 몇 %입니다.</e.ChartSubTitle>
-          <e.AddBox>행정동의 몇 %입니다.</e.AddBox>
-          <BarChartCompare2
-            labels={labels}
-            values={values}
-            datasetsLabel="지출금액(원)"
-            minvalue={minValue}
-            pluginUnit="원"
-          />
-        </>
-      )}
+      <e.ChartSubTitle>자치구의 몇 %입니다.</e.ChartSubTitle>
+      <e.AddBox>행정동의 몇 %입니다.</e.AddBox>
+      <BarChartCompare2
+        labels={labels}
+        values={values}
+        datasetsLabel="지출금액(원)"
+        minvalue={minValue}
+        pluginUnit="원"
+      />
     </e.TotalCard>
   )
 }

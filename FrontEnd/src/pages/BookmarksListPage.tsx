d8b062fd5a59c9fd08ed365c70ebe85052b8e8b4
이승현 <tmdgus1761@gmail.com>
2024-05-14 11:53:04 +0900

    [FE] feat: 프로필 북마크 전체 리스트 페이지 및 컨테이너 생성
    Former-commit-id: 5cabacdba294dc0100b3aba5d7201b29a7b67821

---
+++ FrontEnd/src/pages/BookmarksListPage.tsx
@@ -0,0 +1,7 @@
+import BookmarksListContainer from '@src/containers/profile/BookmarksListContainer'
+
+const BookmarksListPage = () => {
+  return <BookmarksListContainer />
+}
+
+export default BookmarksListPage

d38920573cc2867bd0e58551035a04bd860359ef
이승현 <tmdgus1761@gmail.com>
2024-05-16 03:17:15 +0900

    [FE] feat: 상권분석 재사용 카테고리 제목 카드 년도/분기 드롭다운 생성 및 스타일 적용
    Former-commit-id: a0fc43c3b10a1ad2fb49de6896e006faca0e7d89

--- FrontEnd/src/components/analysis/CategoryTitleCard.tsx
+++ FrontEnd/src/components/analysis/CategoryTitleCard.tsx
@@ -1,12 +1,65 @@
+import React, { useState } from 'react'
 import { CategoryTitleCardProps } from '@src/types/AnalysisType'
 import * as c from '@src/components/styles/analysis/CategoryTitleCardStyle'
 
 const CategoryTitleCard = (props: CategoryTitleCardProps) => {
-  const { src, title } = props
+  const { src, title, setPeriodCode } = props
+  const [selectedYear, setSelectedYear] = useState('2023')
+  const [selectedQuarter, setSelectedQuarter] = useState('3')
+  // console.log(selectedYear, selectedQuarter)
+
+  let years
+  if (title === '집객시설') {
+    years = ['2020', '2021', '2022', '2023']
+  } else {
+    years = ['2019', '2020', '2021', '2022', '2023']
+  }
+
+  let quarters
+  if (selectedYear === '2023') {
+    quarters = ['1', '2', '3']
+  } else if (title === '상주인구' && selectedYear === '2019') {
+    quarters = ['4']
+  } else if (title === '집객시설' && selectedYear === '2020') {
+    quarters = ['4']
+  } else {
+    quarters = ['1', '2', '3', '4']
+  }
+
+  // 년도 선택 핸들러
+  const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newYear = e.target.value
+    setSelectedYear(newYear) // 년도 상태 업데이트
+    setPeriodCode(`${newYear}${selectedQuarter}`)
+  }
+
+  // 분기 선택 핸들러
+  const handleQuarterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newQuarter = e.target.value
+    setSelectedQuarter(newQuarter) // 분기 상태 업데이트
+    setPeriodCode(`${selectedYear}${newQuarter}`)
+  }
+
   return (
     <c.CardContainer>
       <c.IconImg src={src} alt={title} />
       <c.Title>{title}</c.Title>
+      <c.DropdownContainer>
+        <c.Dropdown onChange={handleYearChange} value={selectedYear}>
+          {years.map((year, index) => (
+            <option key={index} value={year}>
+              {year}년
+            </option>
+          ))}
+        </c.Dropdown>
+        <c.Dropdown onChange={handleQuarterChange} value={selectedQuarter}>
+          {quarters.map((quarter, index) => (
+            <option key={index} value={quarter}>
+              {quarter}분기
+            </option>
+          ))}
+        </c.Dropdown>
+      </c.DropdownContainer>
     </c.CardContainer>
   )
 }
--- FrontEnd/src/components/styles/analysis/CategoryTitleCardStyle.tsx
+++ FrontEnd/src/components/styles/analysis/CategoryTitleCardStyle.tsx
@@ -28,8 +28,6 @@ export const CardContainer = styled.div`
   box-sizing: border-box;
   padding: 10px 20px;
   background: #ffffff;
-  //border: 1px solid #d8d8d8;
-  //box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
   border: 2px solid #2a65f0; /* 파란색 테두리 */
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 파란색 계열의 그림자 */
   border-radius: 8px;
@@ -48,4 +46,40 @@ export const Title = styled.div`
 
   ${media('tablet')(`font-size: 1rem `)}
   ${media('mobileM')(`font-size: 0.9rem `)}
+`
+
+export const DropdownContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+
+  select {
+    margin: 0 5px; // select 요소의 마진
+    padding: 5px 10px; // select 요소의 패딩
+    border: 1px solid #ccc; // select 요소의 경계선 색상
+    border-radius: 5px; // select 요소의 경계선 둥글기
+    background-color: white; // 배경색
+    cursor: pointer; // 마우스 커서 모양을 손가락 모양으로
+  }
+
+  select:focus {
+    outline: none; // 포커스 시 외곽선 제거
+    border-color: #007bff; // 포커스 시 경계선 색상 변경
+  }
+`
+
+export const Dropdown = styled.select`
+  padding: 5px 10px;
+  margin-left: auto;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`
+
+export const Button = styled.div`
+  padding: 5px 10px;
+  color: #333;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  border: 1px solid #ccc;
 `

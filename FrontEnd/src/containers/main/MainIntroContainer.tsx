81d8e0d477ad4b4b5261bf7b9ce9dc455c689c3a
choiseongho <chltjdgh3@naver.com>
2024-05-15 18:19:29 +0900

    [FE] feat: 메인 페이지 첫 소개 틀 생성
    Former-commit-id: 70fe50f314f9cd5859231f8529c4d58c2060ce68

---
+++ FrontEnd/src/containers/main/MainIntroContainer.tsx
@@ -0,0 +1,29 @@
+import * as i from '@src/containers/main/MainIntroContainerStyle'
+
+const MainIntroContainer = () => {
+  return (
+    <i.Container>
+      <i.Main>
+        <i.MainContent>
+          서울시 상권을 한눈에!
+          <br />
+          상권분석의 A부터 Z 까지 나도보스와 함께하세요.
+        </i.MainContent>
+        <i.MainButton>분석하러가기 버튼 / 튜토리얼 버튼 </i.MainButton>
+      </i.Main>
+      <i.Sub>
+        <i.SubContent>
+          모든 상권 정보를 손끝에서 파악하고 최적의 입지를 발견하세요.
+          <br />
+          지금까지 없었던 직관적이고 혁신적인 상권 분석 서비스,
+          <br />
+          저희와 함께라면 당신의 창업 준비 과정이 변화될 것입니다.
+          <br />
+          상권 추천부터 창업 시뮬레이션까지, 예비 창업자의 든든한 동반자가 되어
+          드립니다.
+        </i.SubContent>
+      </i.Sub>
+    </i.Container>
+  )
+}
+export default MainIntroContainer
---
+++ FrontEnd/src/containers/main/MainIntroContainerStyle.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  @media only screen and (max-width: 992px) {
+    //
+  }
+`
+
+export const Main = styled.div`
+  width: 100vw;
+  height: calc(100vh - 70px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #dbe7ff;
+`
+export const MainContent = styled.div`
+  font-weight: 700;
+  font-size: 2rem;
+`
+export const MainButton = styled.div``
+export const Sub = styled.div`
+  width: 100vw;
+  height: calc(100vh - 70px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #cbdeff;
+`
+export const SubContent = styled.div`
+  font-weight: 700;
+  font-size: 1.6rem;
+  text-align: center;
+  line-height: 200%;
+`

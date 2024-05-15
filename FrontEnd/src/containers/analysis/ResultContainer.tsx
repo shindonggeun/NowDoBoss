942e6676ad65220354d4e6048c83c387f43174e1
이승현 <tmdgus1761@gmail.com>
2024-05-15 17:06:49 +0900

    [FE] feat: 상권분석 결과 컨테이너 분리
    Former-commit-id: 717f9cf9b13ae9a5158843740284115e6a876147

--- FrontEnd/src/containers/analysis/ResultContainer.tsx
+++ FrontEnd/src/containers/analysis/ResultContainer.tsx
@@ -1,9 +1,4 @@
-import { forwardRef, Ref, useEffect, useRef, useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
-import { postAnalysisBookmarks } from '@src/api/analysisApi'
-import selectPlaceStore from '@src/stores/selectPlaceStore'
-import analysisStore from '@src/stores/analysisStore'
-import { AnalysisBookmarksDataType } from '@src/types/AnalysisType'
+import { useEffect, useRef, useState } from 'react'
 import FlowPopulationAnalysisContainer from '@src/containers/analysis/FlowPopulationAnalysisContainer'
 import FacilitiesAnalysis from '@src/components/analysis/facilities/FacilitiesAnalysis'
 import StoreCountAnalysisContainer from '@src/containers/analysis/StoreCountAnalysisContainer'
@@ -11,16 +6,10 @@ import SalesAnalysisContainer from '@src/containers/analysis/SalesAnalysisContai
 import ResidentPopulationAnalysisContainer from '@src/containers/analysis/ResidentPopulationAnalysisContainer'
 import ExpenditureAnalysisContainer from '@src/containers/analysis/ExpenditureAnalysisContainer'
 import SideBarMenu from '@src/components/analysis/SideBarMenu'
-import ResultIntro from '@src/components/analysis/ResultIntro'
 import ScrollToTopButton from '@src/common/ScrolllToTopButton'
 import * as a from '@src/containers/analysis/ResultContainerStyle'
 
-const ResultContainer = forwardRef((_, ref: Ref<HTMLDivElement>) => {
-  const selectedGoo = selectPlaceStore(state => state.selectedGoo)
-  const selectedDong = selectPlaceStore(state => state.selectedDong)
-  const selectedCommercial = selectPlaceStore(state => state.selectedCommercial)
-  const selectedService = analysisStore(state => state.selectedService)
-
+const ResultContainer = () => {
   // 카테고리별 컴포넌트로 이동하기 위한 ref
   const flowRef = useRef<HTMLDivElement>(null)
   const facilitiesRef = useRef<HTMLDivElement>(null)
@@ -62,38 +51,8 @@ const ResultContainer = forwardRef((_, ref: Ref<HTMLDivElement>) => {
     }
   }, [])
 
-  const { mutate: PostAnalysisBookmarks } = useMutation({
-    mutationKey: ['PostAnalysisBookmarks'],
-    mutationFn: postAnalysisBookmarks,
-    onSuccess: res => console.log(res),
-  })
-
-  const handlePostAnalysisBookmarks = () => {
-    console.log('okay!')
-    const data: AnalysisBookmarksDataType = {
-      districtCode: String(selectedGoo.code),
-      districtCodeName: selectedGoo.name,
-      administrationCode: String(selectedDong.code),
-      administrationCodeName: selectedDong.name,
-      commercialCode: String(selectedCommercial.code),
-      commercialCodeName: selectedCommercial.name,
-      serviceType: selectedService.serviceType,
-      serviceCode: selectedService.serviceCode,
-      serviceCodeName: selectedService.serviceCodeName,
-    }
-
-    PostAnalysisBookmarks(data)
-  }
-
   return (
-    <a.Container ref={ref}>
-      <a.ImgDiv>
-        <img src="/images/Buildings.png" alt="buildings" />
-      </a.ImgDiv>
-      <a.ResultWrap>
-        <a.IntroTitle>분석 리포트</a.IntroTitle>
-      </a.ResultWrap>
-      <ResultIntro handlePostAnalysisBookmarks={handlePostAnalysisBookmarks} />
+    <a.Container>
       {showButton && <SideBarMenu moveTo={moveTo} />}
       <a.MainDiv>
         <FlowPopulationAnalysisContainer ref={flowRef} />
@@ -106,7 +65,5 @@ const ResultContainer = forwardRef((_, ref: Ref<HTMLDivElement>) => {
       <ScrollToTopButton />
     </a.Container>
   )
-})
-
-ResultContainer.displayName = 'ResultContainer'
+}
 export default ResultContainer
---
+++ FrontEnd/src/containers/analysis/ResultIntroContainer.tsx
@@ -0,0 +1,53 @@
+import { forwardRef, Ref } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import selectPlaceStore from '@src/stores/selectPlaceStore'
+import analysisStore from '@src/stores/analysisStore'
+import { postAnalysisBookmarks } from '@src/api/analysisApi'
+import { AnalysisBookmarksDataType } from '@src/types/AnalysisType'
+import ResultIntro from '@src/components/analysis/ResultIntro'
+import * as r from '@src/containers/analysis/ResultInroContainerStyle'
+
+const ResultIntroContainer = forwardRef((_, ref: Ref<HTMLDivElement>) => {
+  const selectedGoo = selectPlaceStore(state => state.selectedGoo)
+  const selectedDong = selectPlaceStore(state => state.selectedDong)
+  const selectedCommercial = selectPlaceStore(state => state.selectedCommercial)
+  const selectedService = analysisStore(state => state.selectedService)
+
+  const { mutate: PostAnalysisBookmarks } = useMutation({
+    mutationKey: ['PostAnalysisBookmarks'],
+    mutationFn: postAnalysisBookmarks,
+    onSuccess: res => console.log(res),
+  })
+
+  const handlePostAnalysisBookmarks = () => {
+    console.log('okay!')
+    const data: AnalysisBookmarksDataType = {
+      districtCode: String(selectedGoo.code),
+      districtCodeName: selectedGoo.name,
+      administrationCode: String(selectedDong.code),
+      administrationCodeName: selectedDong.name,
+      commercialCode: String(selectedCommercial.code),
+      commercialCodeName: selectedCommercial.name,
+      serviceType: selectedService.serviceType,
+      serviceCode: selectedService.serviceCode,
+      serviceCodeName: selectedService.serviceCodeName,
+    }
+
+    PostAnalysisBookmarks(data)
+  }
+
+  return (
+    <r.Container ref={ref}>
+      <r.ImgDiv>
+        <img src="/images/Buildings.png" alt="buildings" />
+      </r.ImgDiv>
+      <r.ResultWrap>
+        <r.IntroTitle>분석 리포트</r.IntroTitle>
+      </r.ResultWrap>
+      <ResultIntro handlePostAnalysisBookmarks={handlePostAnalysisBookmarks} />
+    </r.Container>
+  )
+})
+
+ResultIntroContainer.displayName = 'ResultIntroContainer'
+export default ResultIntroContainer

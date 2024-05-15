e5c5311453641a9fe84104dac21190e814797a92
김온유 <gkc04075@gmail.com>
2024-05-16 08:36:29 +0900

    [FE] feat: 메인페이지 카드 구현
    Former-commit-id: 45bff168d0c1926966eab08455ef8c6cb177640d

--- FrontEnd/src/containers/main/MainStatusContainer.tsx
+++ FrontEnd/src/containers/main/MainStatusContainer.tsx
@@ -1,6 +1,19 @@
 import * as m from '@src/containers/main/MainContainerStyle'
+import * as c from '@src/containers/main/MainStausContainerStyle'
 
 const MainStatusContainer = () => {
+  // 동기화 함수
+  // function syncScroll() {
+  //   const cardList = document.querySelector('.cardList')
+  //   const scrollTop = window.scrollY || document.documentElement.scrollTop
+  //   const scrollAmount = scrollTop
+  //
+  //   // 가로 스크롤 컨테이너의 스크롤 위치를 조정합니다.
+  //   cardList.scrollLeft = scrollAmount
+  // }
+  //
+  // window.addEventListener('scroll', syncScroll)
+
   return (
     <m.Container>
       <m.Content>
@@ -13,9 +26,17 @@ const MainStatusContainer = () => {
             지금 바로 추천받아 보세요 <br />
           </m.TextContent>
         </m.Text>
-        <m.CardList>
-          <m.Card>카드</m.Card>
-        </m.CardList>
+        <c.CardList>
+          <div>
+            <c.Card>
+              <c.CardContainer>내용</c.CardContainer>
+            </c.Card>
+            <c.Card>dd</c.Card>
+            <c.Card>dd</c.Card>
+            <c.Card>dd</c.Card>
+            <c.Card>dd</c.Card>
+          </div>
+        </c.CardList>
       </m.Content>
     </m.Container>
   )
---
+++ FrontEnd/src/containers/main/MainStausContainerStyle.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const CardList = styled.div`
+  width: 100%;
+  margin: 8% 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  ::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera */
+  }
+`
+
+export const Card = styled.div`
+  display: inline-block;
+  width: 350px;
+  height: 400px;
+  border-radius: 20px;
+  color: #ecf0f5;
+  box-shadow: 0 0 40px 5px;
+  padding: 10px;
+  margin: 20px 20px;
+`
+
+export const CardContainer = styled.div`
+  color: #222;
+`

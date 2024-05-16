4d3c2165799170fab1b04500c5a5f0b7397fd924
김온유 <gkc04075@gmail.com>
2024-05-17 02:51:01 +0900

    [FE] feat: 메인페이지 가로 무한스크롤, 카드 추가
    Former-commit-id: cede96dee4b4e0e24152b99820d1c68b4b37bed5

--- FrontEnd/src/containers/main/MainStausContainerStyle.tsx
+++ FrontEnd/src/containers/main/MainStausContainerStyle.tsx
@@ -1,12 +1,8 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const CardList = styled.div`
   width: 90%;
   margin: 8% 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
 
   overflow-x: auto;
   overflow-y: hidden;
@@ -18,17 +14,169 @@ export const CardList = styled.div`
   }
 `
 
-export const Card = styled.div`
-  display: inline-block;
-  width: 350px;
-  height: 400px;
-  border-radius: 20px;
-  color: #ecf0f5;
-  box-shadow: 0 0 40px 5px;
-  padding: 10px;
-  margin: 20px 20px;
+export const SlideWrapper = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-wrap: nowrap;
+  padding: 30px 0;
 `
 
-export const CardContainer = styled.div`
+export const SlideList = styled.div`
+  width: 350px;
+  height: 400px;
+  cursor: pointer;
+  z-index: 2;
+  transition: 0.3s;
+  transform: scale(1);
+
+  border-radius: 20px;
+  color: rgba(186, 209, 255, 0.4);
+  box-shadow: 0 0 30px 10px;
+  margin: 20px 20px;
+
+  &:hover {
+    transform: scale(1.1);
+    //&::after {
+    //  content: '';
+    //  position: absolute;
+    //  top: 0;
+    //  left: 0;
+    //  bottom: 0;
+    //  right: 0;
+    //  width: 100%;
+    //  height: 100%;
+    //}
+  }
+`
+
+const infiniteAnimation1 = keyframes`
+    0% {
+        transform: translateX(0%);
+    }
+    50% {
+        transform: translateX(-100%);
+    }
+    50.1% {
+        transform: translateX(100%);
+    }
+    100% {
+        transform: translateX(0%);
+    }
+`
+
+export const SlideOriginal = styled.div<{ $animate: boolean }>`
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  position: relative;
+  animation: 50s linear infinite normal none running ${infiniteAnimation1};
+  animation-play-state: ${props => (props.$animate ? 'running' : 'paused')};
+
+  //&::before {
+  //  content: '';
+  //  display: block;
+  //  width: 100%;
+  //  height: 1px;
+  //  position: absolute;
+  //  top: 50%;
+  //  left: 50%;
+  //  transform: translate(-50%, -50%);
+  //  background-color: #bbb;
+  //  z-index: 1;
+  //}
+`
+
+const infiniteAnimation2 = keyframes`
+    0% {
+        transform: translateX(0%);
+    }
+    100% {
+        transform: translateX(-200%);
+    }
+`
+
+export const SlideClone = styled.div<{ $animate: boolean }>`
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  position: relative;
+  animation: 50s linear infinite normal none running ${infiniteAnimation2};
+  animation-play-state: ${props => (props.$animate ? 'running' : 'paused')};
+
+  //&::before {
+  //  content: '';
+  //  display: block;
+  //  width: 100%;
+  //  height: 1px;
+  //  position: absolute;
+  //  top: 50%;
+  //  left: 50%;
+  //  transform: translate(-50%, -50%);
+  //  background-color: #bbb;
+  //  z-index: 1;
+  //}
+`
+
+export const Card = styled.div`
   color: #222;
+  padding: 40px 30px;
+`
+
+export const HeaderText = styled.div`
+  color: #1f67fd;
+  font-size: 15px;
+`
+
+export const TitleText = styled.div`
+  color: #404040;
+  font-weight: 600;
+  font-size: 18px;
+  padding: 4px 0;
+`
+
+export const SubTitleText = styled.div`
+  color: #606d85;
+  font-size: 15px;
+`
+
+// Main Card2
+export const TopTenContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 6px 0;
+`
+
+export const TopTenRank = styled.div`
+  width: 10px;
+  color: #606d85;
+`
+
+export const TopTenInfo = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 15px;
+  font-weight: 600;
+`
+
+export const TopTenInfoName = styled.div`
+  color: #404040;
+`
+
+export const TopTenInfoRate = styled.div<{ $isUp: boolean }>`
+  color: ${props => (props.$isUp ? '#FF0000' : '#0066FF')};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Icon = styled.img<{ $width: number }>`
+  width: ${props => `${props.$width}px`};
+`
+
+export const AlignCenter = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `

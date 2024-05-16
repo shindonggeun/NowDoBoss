392752f51b6c01f2720819fec147af71bd57213a
이승현 <tmdgus1761@gmail.com>
2024-05-16 09:12:30 +0900

    [FE] fix: 집객시설 기간코드 적용
    Former-commit-id: b6f46f111838338128901d6d719e87bde83ac939

--- FrontEnd/src/components/analysis/facilities/FacilitiesAnalysis.tsx
+++ FrontEnd/src/components/analysis/facilities/FacilitiesAnalysis.tsx
@@ -1,17 +1,24 @@
-import { forwardRef, Ref } from 'react'
+import { forwardRef, Ref, useState } from 'react'
 import CategoryTitleCard from '@src/components/analysis/CategoryTitleCard'
 import TransChart from '@src/components/analysis/facilities/TransChart'
 import EduChart from '@src/components/analysis/facilities/EduChart'
 import * as f from '@src/components/styles/analysis/FacilitiesAnalysisStyle'
 
 const FacilitiesAnalysis = forwardRef((_, ref: Ref<HTMLDivElement>) => {
+  const [periodCode, setPeriodCode] = useState('20233') // 분기 코드
+
   return (
     <div ref={ref}>
-      <CategoryTitleCard src="/images/facilities.png" title="집객시설" />
+      <CategoryTitleCard
+        src="/images/facilities.png"
+        title="집객시설"
+        setPeriodCode={setPeriodCode}
+      />
       <f.FirstLowContainer>
         <TransChart />
         <EduChart />
       </f.FirstLowContainer>
+      <div>{periodCode}</div>
     </div>
   )
 })

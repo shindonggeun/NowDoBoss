c3871c440fae92b5c36d2e2eba57626b02b3a099
이승현 <tmdgus1761@gmail.com>
2024-05-17 16:48:51 +0900

    [FE] fix: 로그아웃 시, 회원정보 상태관리 제거
    Former-commit-id: d6f62b22fc40ad2b806ec17799385b0b5e7f5b86

--- FrontEnd/src/containers/User/LogoutContainer.tsx
+++ FrontEnd/src/containers/User/LogoutContainer.tsx
@@ -1,11 +1,9 @@
 import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
-import userStore from '@src/stores/userStore'
 import { logoutUser } from '@src/api/userApi'
 
 const LogoutContainer = () => {
-  const setMemberInfo = userStore(state => state.setMemberInfo)
   const [, , removeCookie] = useCookies(['accessToken'])
   const navigate = useNavigate()
 
@@ -20,17 +18,6 @@ const LogoutContainer = () => {
       // 로컬 스토리지에서 memberInfo 및 로그인 여부 삭제
       localStorage.removeItem('memberInfo')
       localStorage.removeItem('isLogIn')
-
-      // 회원정보 상태관리 초기화
-      setMemberInfo({
-        id: null,
-        name: '',
-        nickname: '',
-        email: '',
-        profileImage: null,
-        provider: null,
-        role: '',
-      })
 
       // 메인페이지로 리다이렉트
       navigate('/')

6d10ab56d0cd845299ed2d3da1c04f4a2367079f
김광연 <kwang9705@naver.com>
2024-05-17 13:50:00 +0900

    [BE] refactor: 자치구 상세 api 나누기
    Former-commit-id: 3eac1704cbb05fde5780cc4a0b41369bdb0a9749

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/district/controller/DistrictController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/district/controller/DistrictController.java
@@ -1,8 +1,11 @@
 package com.ssafy.backend.domain.district.controller;
 
-import com.ssafy.backend.domain.district.dto.response.DistrictAreaResponse;
-import com.ssafy.backend.domain.district.dto.response.DistrictDetailResponse;
-import com.ssafy.backend.domain.district.dto.response.DistrictTopTenResponse;
+import com.ssafy.backend.domain.administration.dto.info.ClosedStoreAdministrationTopFiveInfo;
+import com.ssafy.backend.domain.administration.dto.info.OpenedStoreAdministrationTopFiveInfo;
+import com.ssafy.backend.domain.administration.dto.info.SalesAdministrationTopFiveInfo;
+import com.ssafy.backend.domain.district.dto.info.SalesDistrictMonthSalesTopFiveInfo;
+import com.ssafy.backend.domain.district.dto.info.StoreDistrictTotalTopEightInfo;
+import com.ssafy.backend.domain.district.dto.response.*;
 import com.ssafy.backend.domain.district.service.DistrictService;
 import com.ssafy.backend.global.common.dto.Message;
 import io.swagger.v3.oas.annotations.Operation;
@@ -39,6 +42,86 @@ public class DistrictController {
     public ResponseEntity<Message<DistrictDetailResponse>> getDistrictDetail(@PathVariable String districtCode) {
         System.out.println("해당 자치구 상세 분석 가져오기!");
         return ResponseEntity.ok().body(Message.success(districtService.getDistrictDetail(districtCode)));
+    }
+
+    @Operation(
+            summary = "특정 자치구 유동인구 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/footTraffic/{districtCode}")
+    public ResponseEntity<Message<FootTrafficDistrictDetailResponse>> getDistrictFootTrafficDetail(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictFootTrafficDetail(districtCode)));
+    }
+
+    @Operation(
+            summary = "특정 자치구 변화 지표 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/changeIndicator/{districtCode}")
+    public ResponseEntity<Message<ChangeIndicatorDistrictResponse>> getDistrictChangeDetail(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictChangeIndicatorDetail(districtCode)));
+    }
+
+    @Operation(
+            summary = "특정 자치구 점포 수 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/store/total/{districtCode}")
+    public ResponseEntity<Message<List<StoreDistrictTotalTopEightInfo>>> getDistrictTotalStoreDetail(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictTotalStoreDetail(districtCode)));
+    }
+
+    @Operation(
+            summary = "특정 자치구 개업률 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/store/opened/{districtCode}")
+    public ResponseEntity<Message<List<OpenedStoreAdministrationTopFiveInfo>>> getDistrictOpenedStoreDetail(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictOpenedStoreDetail(districtCode)));
+    }
+
+    @Operation(
+            summary = "특정 자치구 폐업률 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/store/closed/{districtCode}")
+    public ResponseEntity<Message<List<ClosedStoreAdministrationTopFiveInfo>>> getDistrictStoreDetail(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictClosedStoreDetail(districtCode)));
+    }
+
+//    @Operation(
+//            summary = "특정 자치구 매출 상세 분석",
+//            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+//    )
+//    @GetMapping("/detail/sales/{districtCode}")
+//    public ResponseEntity<Message<SalesDistrictDetailResponse>> getDistrictSalesDetail(@PathVariable String districtCode) {
+//        System.out.println("해당 자치구 상세 분석 가져오기!");
+//        return ResponseEntity.ok().body(Message.success(districtService.getDistrictSalesDetail(districtCode)));
+//    }
+
+    @Operation(
+            summary = "특정 자치구 업종별 매출 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/sales/service/{districtCode}")
+    public ResponseEntity<Message<List<SalesDistrictMonthSalesTopFiveInfo>>> getDistrictSalesDetailByServiceCode(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictSalesDetailByServiceCode(districtCode)));
+    }
+
+    @Operation(
+            summary = "특정 자치구 해당 행정동 매출 상세 분석",
+            description = "상권변화지표 상세, 유동인구 상세, 점포 상세 분석을 제공하는 기능입니다."
+    )
+    @GetMapping("/detail/sales/administration/{districtCode}")
+    public ResponseEntity<Message<List<SalesAdministrationTopFiveInfo>>> getDistrictSalesDetailByAdministrationCode(@PathVariable String districtCode) {
+        System.out.println("해당 자치구 상세 분석 가져오기!");
+        return ResponseEntity.ok().body(Message.success(districtService.getDistrictSalesDetailByAdministrationCode(districtCode)));
     }
 
     @Operation(

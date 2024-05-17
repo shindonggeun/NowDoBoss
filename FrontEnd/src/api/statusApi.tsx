81b9cd1e2d49fcf30dfc57fcf65bb1e0fa88b48d
김온유 <gkc04075@gmail.com>
2024-05-17 23:06:03 +0900

    [FE] feat: 구별 상권현황 api 각 컴포넌트별로 분할
    Former-commit-id: b927ca6592cc61813f58848b2b949a510fae3028

--- FrontEnd/src/api/statusApi.tsx
+++ FrontEnd/src/api/statusApi.tsx
@@ -15,3 +15,60 @@ export const fetchStatusDetail = async (districtCode: number) => {
     .then(res => res.data)
     .catch(err => console.log(err))
 }
+
+// [NOTE] 상권현황 상세 데이터 분리
+// 자치구 유동인구 상세 분석
+export const fetchStatusFootTraffic = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/footTraffic/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 자치구 상권 변화지표 상세 분석
+export const fetchStatusChangeIndicator = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/changeIndicator/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 자치구 상권 점포수 상세 분석
+export const fetchStatusStore = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/store/total/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 자치구 상권 개업률 상세 분석
+export const fetchStatusOpen = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/store/opened/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 자치구 상권 폐업률 상세 분석
+export const fetchStatusClose = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/store/closed/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 자치구 상권 업종별 매출 상세 분석
+export const fetchStatusService = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/sales/service/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 자치구 상권 행정동별 매출 상세 분석
+export const fetchStatusSale = async (districtCode: number) => {
+  return customAxios
+    .get(`/district/detail/sales/administration/${districtCode}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}

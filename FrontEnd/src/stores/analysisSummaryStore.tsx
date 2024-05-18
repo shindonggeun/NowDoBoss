1fcc48844a79fe94d1841055fc09ddaed5ed1e97
이승현 <tmdgus1761@gmail.com>
2024-05-18 18:03:27 +0900

    [FE] feat: 상권분석 요약 상태관리 스토어 생성 및 유동인구,매출,점포 관련 상태관리 추가
    Former-commit-id: 5ae6f125fa3d3c6d32aff0758f5b4c6e0e991f1e

---
+++ FrontEnd/src/stores/analysisSummaryStore.tsx
@@ -0,0 +1,76 @@
+import { create } from 'zustand'
+import {
+  FlowSummaryType,
+  ResidentSummaryType,
+  SalesSummaryType,
+  StoreSummaryType,
+  UseAnalysisSummaryStoreType,
+} from '@src/types/AnalysisSummaryType'
+
+const initialFlowSummary: FlowSummaryType = {
+  daily: '',
+  age: '',
+  gender: '',
+  time: '',
+  timeInfo: '',
+  maxWeek: '',
+  minWeek: '',
+  ratio: '',
+}
+
+const initialSalesSummary: SalesSummaryType = {
+  total: '',
+  monthly: '',
+  average: '',
+  gender: '',
+  age: '',
+  week: '',
+  time: '',
+}
+
+const initialStoreSummary: StoreSummaryType = {
+  tip: '',
+}
+
+const initialResidentSummary: ResidentSummaryType = {
+  gender: '',
+  age: '',
+}
+
+const useAnalysisSummaryStore = create<UseAnalysisSummaryStoreType>(set => ({
+  flowSummary: initialFlowSummary, // 유동인구
+  salesSummary: initialSalesSummary, // 매출분석
+  storeSummary: initialStoreSummary, // 점포분석
+  residentSummary: initialResidentSummary, // 상주인구
+
+  setFlowSummary: (
+    key: keyof FlowSummaryType,
+    value: FlowSummaryType[keyof FlowSummaryType],
+  ) =>
+    set(state => ({
+      flowSummary: { ...state.flowSummary, [key]: value },
+    })),
+  setSalesSummary: (
+    key: keyof SalesSummaryType,
+    value: SalesSummaryType[keyof SalesSummaryType],
+  ) =>
+    set(state => ({
+      salesSummary: { ...state.salesSummary, [key]: value },
+    })),
+  setStoreSummary: (
+    key: keyof StoreSummaryType,
+    value: StoreSummaryType[keyof StoreSummaryType],
+  ) =>
+    set(state => ({
+      storeSummary: { ...state.storeSummary, [key]: value },
+    })),
+  setResidentSummary: (
+    key: keyof ResidentSummaryType,
+    value: ResidentSummaryType[keyof ResidentSummaryType],
+  ) =>
+    set(state => ({
+      residentSummary: { ...state.residentSummary, [key]: value },
+    })),
+}))
+
+export default useAnalysisSummaryStore

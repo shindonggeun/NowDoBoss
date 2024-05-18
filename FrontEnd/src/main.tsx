6d01f827cbe8231fa595f1edff53c7908bce044b
이승현 <tmdgus1761@gmail.com>
2024-05-18 16:50:35 +0900

    [FE] feat: BrowserRouter 최상위 컴포넌트로 이동
    Former-commit-id: cf2865d2ba677072d337da626bd9a762924b93a2

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom' // 전체 애플리케이션의 라우팅 컨텍스트를 최상위에서 관리하기 위함
 import App from './App'
 // import './index.css'
 
@@ -9,7 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )

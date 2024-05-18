5755f98ecc294e1a526707cce6529d40f36c07f1
choiseongho <chltjdgh3@naver.com>
2024-05-19 02:50:29 +0900

    [FE] feat: 상권 추천 저장 목록 tyle 생성
    Former-commit-id: 984b53e58f1e0a6ee8ef68d542f9fcc51673a1bf

--- FrontEnd/src/types/GlobalType.ts
+++ FrontEnd/src/types/GlobalType.ts
@@ -1,3 +1,23 @@
 export type NavigateType = {
   url: string
 }
+
+export type PromiseSaveListType = {
+  dataHeader: {
+    successCode: 0
+    resultCode: null
+    resultMessage: null
+  }
+  dataBody: {
+    data: {
+      userId: number
+      commercialCode: string
+      commercialCodeName: string
+      districtCode: string
+      districtCodeName: string
+      administrationCode: string
+      administrationCodeName: string
+      createdAt: string
+    }[]
+  }
+}

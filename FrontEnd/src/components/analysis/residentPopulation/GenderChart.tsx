c41728211f34051035587c6c3c67c7193ace7c41
이승현 <tmdgus1761@gmail.com>
2024-05-18 18:14:35 +0900

    [FE] feat: 상주인구 성별 관련 요약 상태 업데이트 로직 추가
    Former-commit-id: aefbf189d1bf81d1e5ac8b550c171ef10edc9af5

--- FrontEnd/src/components/analysis/residentPopulation/GenderChart.tsx
+++ FrontEnd/src/components/analysis/residentPopulation/GenderChart.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react'
 import analysisStore from '@src/stores/analysisStore'
+import useAnalysisSummaryStore from '@src/stores/analysisSummaryStore'
 import DoughnutChart from '@src/common/DoughnutChart'
 import * as r from '@src/components/styles/analysis/ResidentPopulationAnalysisStyle'
 
 const GenderChart = () => {
-  const { residentPopulationDataBody } = analysisStore()
+  const residentPopulationDataBody = analysisStore(
+    state => state.residentPopulationDataBody,
+  )
+  const setResidentSummary = useAnalysisSummaryStore(
+    state => state.setResidentSummary,
+  )
 
   const { malePercentage } = residentPopulationDataBody
   const { femalePercentage } = residentPopulationDataBody
@@ -22,12 +29,17 @@ const GenderChart = () => {
   const maxLabel = labels[value.indexOf(maxValue)]
   const minLabel = labels[value.indexOf(minValue)]
 
+  // 요약 상태 업데이트
+  useEffect(() => {
+    setResidentSummary('gender', maxLabel)
+  }, [residentPopulationDataBody, maxLabel, setResidentSummary])
+
   return (
     <r.GenderChart>
       <r.ChartTitle>남/녀 상주인구</r.ChartTitle>
       <r.ChartSubTitle>
         {minLabel}의 상주인구가 {maxLabel}보다 약{' '}
-        <r.HighlightText>{ratio}배</r.HighlightText> 더 많아요.
+        <r.HighlightText>{ratio}배</r.HighlightText>더 많아요.
       </r.ChartSubTitle>
       <DoughnutChart labels={labels} value={value} textCenter={textCenter} />
     </r.GenderChart>

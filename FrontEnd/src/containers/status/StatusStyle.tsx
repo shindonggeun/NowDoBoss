af644d1d4af0ab10544c844f19c69439774cdce5
choiseongho <chltjdgh3@naver.com>
2024-05-18 23:39:37 +0900

    [FE] feat: 구별 현황 반응형 적용
    Former-commit-id: d1a00ec0400ef105608c8a0e77ab76279fdc0caf

--- FrontEnd/src/containers/status/StatusStyle.tsx
+++ FrontEnd/src/containers/status/StatusStyle.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components'
 
 export const StatusContainer = styled.div`
+  position: relative;
   display: flex;
   flex-direction: row;
-  // 너비 어떻게??
-  //width: window.innerWidth;
   width: 100vw;
   height: calc(100vh - 68px);
   overflow: hidden;
@@ -13,34 +12,116 @@ export const StatusContainer = styled.div`
   @media (max-width: 768px) {
     flex-direction: column;
   }
+  @media only screen and (max-width: 550px) {
+    flex-direction: column;
+  }
+`
+
+export const LeftDiv = styled.div`
+  width: 40vw;
+  height: calc(100vh - 68px);
+  position: absolute;
+  z-index: 1;
+
+  @media only screen and (max-width: 920px) {
+    width: 35vw;
+  }
+  @media only screen and (max-width: 550px) {
+    font-size: 0.9rem;
+    top: auto;
+    bottom: 0;
+    width: 105%;
+    height: 45%;
+  }
 `
 
 export const Sidebar = styled.div`
   width: 380px;
+  //min-width: 320px;
   height: 475px;
+  margin: 10px 10px;
   box-shadow: 10px 10px 15px -5px rgba(0, 0, 0, 0.2);
   border: 1px solid #dce5f2;
   border-radius: 5px;
-  margin: 10px 10px;
   background-color: white;
 
-  @media (max-width: 768px) {
-    display: none;
+  @media only screen and (max-width: 1035px) {
+    //display: none;
+    width: 80%;
+  }
+  @media only screen and (max-width: 945px) {
+    //display: none;
+    width: 75%;
+    height: 445px;
+  }
+  @media only screen and (max-width: 920px) {
+    //display: none;
+    width: 90%;
+    height: 445px;
+  }
+  @media only screen and (max-width: 820px) {
+    //display: none;
+    width: 90%;
+    height: 420px;
+  }
+  @media only screen and (width < 550px) {
+    box-shadow: none;
   }
 `
 
-export const EmptyContainer = styled.div`
-  width: 400px;
+export const PoligonMap = styled.div`
+  margin-left: 30vw;
+  width: 70vw;
+
+  @media only screen and (max-width: 1130px) {
+    margin-left: 35vw;
+    width: 65vw;
+  }
+  @media only screen and (max-width: 980px) {
+    margin-left: calc(100vw * 80%);
+    width: calc(100vw - (100vw * 80%));
+  }
+  @media only screen and (max-width: 945px) {
+    margin-left: calc(100vw * 75%);
+    width: calc(100vw - (100vw * 75%));
+  }
+  @media only screen and (max-width: 900px) {
+    margin-left: calc(100vw * 75%);
+    width: calc(100vw - (100vw * 75%));
+  }
+  @media only screen and (max-width: 550px) {
+    top: 0;
+    margin-left: 0;
+    width: 100vw;
+    height: 50vh;
+  }
+  @media only screen and (max-width: 460px) {
+  }
 `
 
 export const Banner = styled.div`
-  margin: 5px -10px;
+  margin: 5px 0;
 
-  @media only screen and (max-width: 992px) {
+  @media only screen and (max-width: 1035px) {
     display: none;
   }
 `
 
-export const Content = styled.div`
-  flex: 1;
+export const Div = styled.div``
+
+export const Report = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 0;
+
+  @media only screen and (max-width: 680px) {
+    font-size: 0.9rem;
+    top: auto;
+    bottom: 0;
+    width: 100%;
+    max-height: 60vh;
+    border-bottom-right-radius: 0;
+    border-top-left-radius: 20px;
+    box-shadow: none;
+  }
 `

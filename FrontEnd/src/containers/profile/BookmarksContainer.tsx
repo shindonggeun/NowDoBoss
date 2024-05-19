aa4f6cff7592b826cb4241ba0e04bd56302ff25e
김온유 <gkc04075@gmail.com>
2024-05-19 21:29:08 +0900

    [FE] refactor: 전체 페이지 삭제
    Former-commit-id: 890e6933f87f5b54e48127034f86c9a96b1e7fc8

--- FrontEnd/src/containers/profile/BookmarksContainer.tsx
+++ FrontEnd/src/containers/profile/BookmarksContainer.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 const BookmarksContainer = () => {
   const tabs = [
-    { label: '전체', path: '/profile/bookmarks' },
+    // { label: '전체', path: '/profile/bookmarks' },
     { label: '상권분석', path: '/profile/bookmarks/analysis' },
     { label: '상권추천', path: '/profile/bookmarks/recommend' },
     { label: '창업시뮬레이션', path: '/profile/bookmarks/simulation' },

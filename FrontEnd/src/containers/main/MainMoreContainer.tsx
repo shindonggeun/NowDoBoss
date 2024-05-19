b8ce2037ce374623183577cbfc80af2473f85015
김온유 <gkc04075@gmail.com>
2024-05-19 18:25:18 +0900

    [FE] feat: 메인페이지 서비스 페이지 추가
    Former-commit-id: 9bbddfef0629547173b6388ebaf8e22810315c2a

--- FrontEnd/src/containers/main/MainMoreContainer.tsx
+++ FrontEnd/src/containers/main/MainMoreContainer.tsx
@@ -1,23 +1,36 @@
 import * as m from '@src/containers/main/MainContainerStyle'
+import MoreCard1 from '@src/components/main/service/MoreCard1'
+import MoreCard2 from '@src/components/main/service/MoreCard2'
+import MoreCard3 from '@src/components/main/service/MoreCard3'
 
 const MainMoreContainer = () => {
   return (
-    <m.Container>
-      <m.Content>
-        <m.Text>
+    <m.EctContainer>
+      <m.EctContent>
+        <m.EctText>
           <m.BlueText>More Service</m.BlueText>
-          <m.Title>추가 서비스</m.Title>
-          <m.TextContent>
-            관심 분야가 같은 사람들과 함께 <br />
-            고민을 나누고 소통할 수 있는 <br />
-            커뮤니티 및 채팅 <br />
-          </m.TextContent>
-        </m.Text>
-        <m.CardList>
-          <m.Card>임시</m.Card>
-        </m.CardList>
-      </m.Content>
-    </m.Container>
+          <m.Title>NowDoBoss만의 든든한 서비스</m.Title>
+          <m.EctTextContent>
+            관심 분야가 같은 사람들과 함께 고민을 나누고 소통할 수 있는 커뮤니티
+            및 채팅, <br /> 상권분석과 업종추천에 대한 분석 리포트와 결과
+            공유까지! <br />
+            지금 바로 함께 하세요.
+            <br />
+          </m.EctTextContent>
+        </m.EctText>
+      </m.EctContent>
+      <m.EctCardContainer>
+        <m.EctCard $isup={false}>
+          <MoreCard1 />
+        </m.EctCard>
+        <m.EctCard $isup>
+          <MoreCard2 />
+        </m.EctCard>
+        <m.EctCard $isup={false}>
+          <MoreCard3 />
+        </m.EctCard>
+      </m.EctCardContainer>
+    </m.EctContainer>
   )
 }
 export default MainMoreContainer

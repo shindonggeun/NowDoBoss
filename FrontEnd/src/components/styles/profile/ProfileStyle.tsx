64771a4cf6021ff4ea7f78375d533a81035b2b83
김온유 <gkc04075@gmail.com>
2024-05-19 21:29:36 +0900

    [FE] feat: 창업시뮬레이션 저장목록 리스트카드 추가
    Former-commit-id: b889ecdbbeb75d850bab937a0f9f6a097a846a06

--- FrontEnd/src/components/styles/profile/ProfileStyle.tsx
+++ FrontEnd/src/components/styles/profile/ProfileStyle.tsx
@@ -59,3 +59,91 @@ export const Provider = styled.img`
   height: 16px;
   border-radius: 100%;
 `
+
+// 북마크 카드
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+`
+
+export const Card = styled.div<{ $width: number }>`
+  width: ${props => `${props.$width}px`};
+  padding: 1rem;
+  cursor: pointer;
+  border-radius: 0.75rem;
+  background-color: white;
+  border: 1px solid #e2ebfd;
+  position: relative;
+  //background: #f1f1f3;
+  //background-color: #f0f5ff;
+  //box-shadow: 0 0 5px 2px rgba(240, 245, 255, 1);
+`
+
+export const CardTitle = styled.div`
+  padding: 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #3c3852;
+  margin: 5px 0;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  //&:hover {
+  //  color: #236cff;
+  //  text-decoration: underline;
+  //}
+`
+
+export const CardTitleEmphasis = styled.span`
+  padding: 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #236cff;
+`
+
+export const CardContent = styled.p<{ $marginTop: number }>`
+  color: #3c3852;
+  font-size: 0.86rem;
+  margin-top: ${props => `${props.$marginTop}px`};
+`
+
+export const CardDate = styled.div<{ $marginTop: number }>`
+  color: #6e6b80;
+  font-size: 0.8rem;
+  margin-top: ${props => `${props.$marginTop}px`};
+`
+
+export const CardArrow = styled.div`
+  position: absolute;
+  background: #5d93fb;
+  padding: 0.4rem;
+  border-top-left-radius: 0.75rem;
+  border-bottom-right-radius: 0.75rem;
+  bottom: 0;
+  right: 0;
+  transition: 0.2s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background: #236cff;
+
+    svg {
+      transform: translateX(3px);
+    }
+  }
+`
+
+export const ArrowSVG = styled.svg`
+  fill: none;
+  height: 20px;
+  width: 23px;
+  path {
+    fill: #fff;
+  }
+`

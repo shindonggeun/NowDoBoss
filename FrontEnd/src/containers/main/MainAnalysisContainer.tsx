b574e2e5cb186bc12bd660e2f50a98f9d7cfd2c6
이승현 <tmdgus1761@gmail.com>
2024-05-20 00:27:29 +0900

    [FE] feat: 상권분석 메인페이지 작성
    Former-commit-id: 8724ed342599fa5dc4d4b5c3f1aa5568d60f7ba6

--- FrontEnd/src/containers/main/MainAnalysisContainer.tsx
+++ FrontEnd/src/containers/main/MainAnalysisContainer.tsx
@@ -2,74 +2,59 @@ import * as a from '@src/containers/main/MainAnalysisContainerStyle'
 import MainCard1 from '@src/components/main/analysis/MainCard1'
 import MainCard2 from '@src/components/main/analysis/MainCard2'
 import MainCard3 from '@src/components/main/analysis/MainCard3'
+import MainCard4 from '@src/components/main/analysis/MainCard4'
 
 const MainAnalysisContainer = () => {
   return (
     <a.Container>
       <a.Main>
-        <a.InnerBox>
-          <a.TextDiv>
-            <a.TextWrap>
-              <a.BlueText>Market Analysis Report</a.BlueText>
-              <a.MainText>상권분석 리포트</a.MainText>
-              <a.SubText>
-                인구분석부터 매출정보까지,
-                <br />
-                핵심 정보를 한눈에!
-                <br />
-                지금 바로 당신의 비즈니스를 위한
-                <br />
-                최적의 상권을 발견하세요.
-                <br />
-              </a.SubText>
-              <a.TextStopBtn />
-            </a.TextWrap>
-          </a.TextDiv>
-          <a.ImgBox>
-            <a.Fix>
-              <a.Rel>
-                <a.BoxWrap>
-                  <a.CardWrap>
-                    <a.OriginalCardDiv>
-                      <MainCard1 />
-                      <MainCard2 />
-                      <MainCard3 />
-                    </a.OriginalCardDiv>
-                    <a.CloneCardDiv>
-                      <MainCard1 />
-                      <MainCard2 />
-                      <MainCard3 />
-                    </a.CloneCardDiv>
-                  </a.CardWrap>
-                  <a.CardWrap>
-                    <a.DescOriginalCardDiv>
-                      <MainCard1 />
-                      <MainCard2 />
-                      <MainCard3 />
-                    </a.DescOriginalCardDiv>
-                    <a.DescCloneCardDiv>
-                      <MainCard1 />
-                      <MainCard2 />
-                      <MainCard3 />
-                    </a.DescCloneCardDiv>
-                  </a.CardWrap>
-                  <a.CardWrap>
-                    <a.OriginalCardDiv>
-                      <MainCard1 />
-                      <MainCard2 />
-                      <MainCard3 />
-                    </a.OriginalCardDiv>
-                    <a.CloneCardDiv>
-                      <MainCard1 />
-                      <MainCard2 />
-                      <MainCard3 />
-                    </a.CloneCardDiv>
-                  </a.CardWrap>
-                </a.BoxWrap>
-              </a.Rel>
-            </a.Fix>
-          </a.ImgBox>
-        </a.InnerBox>
+        <a.TextDiv>
+          <a.TextWrap>
+            <a.BlueText>Market Analysis Report</a.BlueText>
+            <a.MainText>상권분석 리포트</a.MainText>
+            <a.SubText>
+              인구분석부터 매출정보까지,
+              <br />
+              핵심 정보를 한눈에!
+              <br />
+              지금 바로 당신의 비즈니스를 위한
+              <br />
+              최적의 상권을 발견하세요.
+              <br />
+            </a.SubText>
+          </a.TextWrap>
+        </a.TextDiv>
+        <a.ImgBox>
+          <a.CardWrap>
+            <a.OriginalCardDiv>
+              <MainCard1 />
+              <MainCard2 />
+              <MainCard4 />
+              <MainCard3 />
+            </a.OriginalCardDiv>
+            <a.CloneCardDiv>
+              <MainCard1 />
+              <MainCard2 />
+              <MainCard4 />
+              <MainCard3 />
+            </a.CloneCardDiv>
+          </a.CardWrap>
+
+          <a.CardWrap>
+            <a.DescOriginalCardDiv>
+              <MainCard3 />
+              <MainCard4 />
+              <MainCard1 />
+              <MainCard2 />
+            </a.DescOriginalCardDiv>
+            <a.DescCloneCardDiv>
+              <MainCard3 />
+              <MainCard4 />
+              <MainCard1 />
+              <MainCard2 />
+            </a.DescCloneCardDiv>
+          </a.CardWrap>
+        </a.ImgBox>
       </a.Main>
       <a.Sub>
         <a.SubInnerBox>
@@ -92,27 +77,6 @@ const MainAnalysisContainer = () => {
           </a.ImgDiv>
         </a.SubInnerBox>
       </a.Sub>
-      {/* <a.SubSub> */}
-      {/*  <a.SubInnerBox> */}
-      {/*    <a.ImgDiv> */}
-      {/*      <a.MapImgDiv> */}
-      {/*        <img src="/images/main_recommend_goo.png" alt="map" /> */}
-      {/*      </a.MapImgDiv> */}
-      {/*    </a.ImgDiv> */}
-      {/*    <a.ContentDiv> */}
-      {/*      <div> */}
-      {/*        <h2> */}
-      {/*          상권의 동향을 <br /> */}
-      {/*          한눈에 파악하세요. <br /> */}
-      {/*        </h2> */}
-      {/*        <p> */}
-      {/*          차트 분석을 통해 시장 트렌드를 정확히 이해하고, <br /> */}
-      {/*          경쟁 우위에 있는 위치를 발견해보세요. */}
-      {/*        </p> */}
-      {/*      </div> */}
-      {/*    </a.ContentDiv> */}
-      {/*  </a.SubInnerBox> */}
-      {/* </a.SubSub> */}
     </a.Container>
   )
 }

ff26421a82c99b3df80c33d03cd9f5bb7b5b6554
김온유 <gkc04075@gmail.com>
2024-05-19 18:25:02 +0900

    [FE] feat: 메인페이지 서비스 스타일 추가
    Former-commit-id: 662be214fd127dc302b9755890bf8ba510d5ba74

--- FrontEnd/src/containers/main/MainContainerStyle.tsx
+++ FrontEnd/src/containers/main/MainContainerStyle.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const MainContainer = styled.div`
   scroll-behavior: smooth;
@@ -150,4 +150,128 @@ export const Recommend = styled.div`
     transform: translateY(0);
     align-items: center;
   }
+`
+
+// moreService
+export const EctContainer = styled.div`
+  width: calc(100vw - 5px);
+  height: calc(100vh);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+  padding-top: 80px;
+  background-color: #f0f5ff;
+  @media only screen and (max-width: 992px) {
+    //
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`
+
+// 내용이 들어갈 div
+export const EctContent = styled.div`
+  height: 100%;
+  margin: 0 10%;
+  //background-color: #d9d9d9;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (max-width: 992px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`
+
+// 글로 소개하는 부분
+export const EctText = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 40px;
+`
+
+// 설명
+export const EctTextContent = styled.div`
+  color: #606d85;
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: 160%;
+  margin-bottom: 40px;
+  text-align: center;
+`
+
+export const EctCardContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  background-color: #f0f5ff;
+  padding: 50px 40px 100px 40px;
+  gap: 50px;
+
+  @media only screen and (max-width: 992px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+const bounce = keyframes`
+  0% { transform: translateY(0); }
+  50% { transform: translateY(-10px); }
+  100% { transform: translateY(0); }
+`
+
+export const EctCard = styled.div<{ $isup: boolean }>`
+  width: 23%;
+  min-width: 300px;
+  height: 400px;
+  background-color: white;
+  margin-top: ${props => (props.$isup ? 0 : `60px`)};
+  border-radius: 20px;
+  padding: 10px;
+  box-shadow: 0 0 30px 5px #d2def8;
+  animation: ${bounce} 2s infinite;
+
+  @media only screen and (max-width: 992px) {
+    width: 70%;
+    margin: 20px 0;
+  }
+`
+
+export const EctCardContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 30px;
+`
+
+export const EctImgIcon = styled.img`
+  width: 150px;
+`
+
+export const CardTitle = styled.div`
+  color: #191f28;
+  margin: 10px 0;
+  font-size: 1.5rem;
+  font-weight: 700;
+  word-break: keep-all;
+  line-height: 140%;
+`
+
+export const CardSubTitle = styled.div`
+  width: 80%;
+  color: #606d85;
+  font-size: 17px;
+  text-align: center;
+  margin-top: 10px;
+  font-weight: 500;
 `
